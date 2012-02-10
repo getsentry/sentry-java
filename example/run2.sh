6c0f6ea1edbf8816cb394b90b708a21032607972
Ken Cochrane <kcochrane@Ken-Cochranes-MacBook-Pro.local>
2012-02-10 12:04:03 -0500

    bumped version to 0.2

--- example/run2.sh
+++ example/run2.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -classpath ../raven-java-0.1.jar net.kencochrane.sentry.SentryExample log4j_configuration.txt
+java -classpath ../raven-java-0.2.jar net.kencochrane.sentry.SentryExample log4j_configuration.txt
