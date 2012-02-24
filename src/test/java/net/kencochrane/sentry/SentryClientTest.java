fb1d88ad919a4bc3eac8bf11ab70c3d7cd6f9209
David Cramer <dcramer@gmail.com>
2012-02-24 02:20:24 -0800

    Added captureMessage and captureException interfaces to bring API in line with other clients

--- src/test/java/net/kencochrane/sentry/SentryClientTest.java
+++ src/test/java/net/kencochrane/sentry/SentryClientTest.java
@@ -2,11 +2,25 @@ package net.kencochrane.sentry;
 
 import mockit.Mocked;
 import mockit.Expectations;
+import mockit.Verifications;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class SentryClientTest {
+    public void triggerRuntimeException() {
+        try {
+            triggerNullPointer();
+        } catch (Exception e) {
+            throw new RuntimeException("Error triggering null pointer", e);
+        }
+    }
+
+    public String triggerNullPointer() {
+        String c = null;
+        return c.toLowerCase();
+    }
+
     @Test(expected=RuntimeException.class)
     public void testMissingDSN() {
         RavenClient client = new RavenClient();
@@ -31,4 +45,30 @@ public class SentryClientTest {
         RavenClient client = new RavenClient();
         assertEquals(client.getSentryDSN(), "http://public:secret@example.com/path/1");
    }
+
+    @Test
+    public void testCaptureExceptionWithOnlyThrowable() {
+        RavenClient client = new RavenClient("http://public:secret@example.com/path/1");
+
+        new Expectations()
+        {
+
+            @Mocked("getRandomUUID") RavenUtils mockedRavenUtils;
+
+            {
+                RavenUtils.getRandomUUID(); returns("1234567890");
+            }
+
+        // TODO: this should be mocked, somehow
+        //     RavenClient.sendMessage(); minTimes = 1; maxTimes = 1;
+        };
+
+
+        try {
+            triggerRuntimeException();
+        } catch (RuntimeException e) {
+            String ident = client.captureException(e);
+            assertEquals(ident, "1234567890");
+        }
+    }
 }
