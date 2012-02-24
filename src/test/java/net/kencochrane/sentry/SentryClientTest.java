bf3a6099abda47e54dc6d932d2cb50c41c0dd19a
David Cramer <dcramer@gmail.com>
2012-02-24 01:41:29 -0800

    Added a test for the added RuntimeException when Raven is not configured

--- src/test/java/net/kencochrane/sentry/SentryClientTest.java
+++ src/test/java/net/kencochrane/sentry/SentryClientTest.java
@@ -7,11 +7,16 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class SentryClientTest {
+    @Test(expected=RuntimeException.class)
+    public void testMissingDSN() {
+        RavenClient client = new RavenClient();
+    }
+
     @Test
     public void testConfigureFromDSN() {
         RavenClient client = new RavenClient("http://public:secret@example.com/path/1");
         assertEquals(client.getSentryDSN(), "http://public:secret@example.com/path/1");
-   }
+    }
 
     @Test
     public void testConfigureFromEnvironment() {
