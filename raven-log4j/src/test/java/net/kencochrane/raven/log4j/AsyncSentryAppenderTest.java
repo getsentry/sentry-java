155bae8287fc8b9f987f7d3e16bd7030cbfeb771
Kevin Wetzels <kevin@roam.be>
2012-07-07 12:21:18 +0200

    Remove raven-integration-tests as a module
    Otherwise the raven-integration-tests tests would be run on each build
    at the project level. Because those tests require a particular Sentry
    setup, this is something that would become a serious PITA eventually.

---
+++ raven-log4j/src/test/java/net/kencochrane/raven/log4j/AsyncSentryAppenderTest.java
@@ -0,0 +1,11 @@
+package net.kencochrane.raven.log4j;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kevin
+ * Date: 07/07/12
+ * Time: 11:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class AsyncSentryAppenderTest {
+}
