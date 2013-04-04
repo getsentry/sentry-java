183debd9fff6ad8f61b092588a04ef57d8f3ea44
Colin Hebert <colin.hebert@it.ox.ac.uk>
2013-04-04 11:53:34 +0100

    Improve the documentation of EventBuilder

--- raven/src/main/java/net/kencochrane/raven/event/EventBuilder.java
+++ raven/src/main/java/net/kencochrane/raven/event/EventBuilder.java
@@ -35,22 +35,44 @@ public class EventBuilder {
     private final LoggedEvent event;
     private boolean alreadyBuilt = false;
 
+    /**
+     * Creates a new EventBuilder to prepare a new {@link LoggedEvent}.
+     * <p>
+     * Automatically generates the id of the new event.
+     * </p>
+     */
     public EventBuilder() {
         this(UUID.randomUUID());
     }
 
+    /**
+     * Creates a new EventBuilder to prepare a new {@link LoggedEvent}.
+     *
+     * @param eventId unique identifier for the new event.
+     */
     public EventBuilder(UUID eventId) {
         this.event = new LoggedEvent(eventId);
     }
 
-    private static String calculateChecksum(String message) {
+    /**
+     * Calculates a checksum for a given string.
+     *
+     * @param string string from which a checksum should be obtained
+     * @return a checksum allowing two events with the same properties to be grouped later.
+     */
+    private static String calculateChecksum(String string) {
         // TODO: Large strings will be poorly handled
-        byte[] bytes = message.getBytes();
+        byte[] bytes = string.getBytes();
         Checksum checksum = new CRC32();
         checksum.update(bytes, 0, bytes.length);
         return String.valueOf(checksum.getValue());
     }
 
+    /**
+     * Obtains the current hostname.
+     *
+     * @return the current hostname, or {@link #DEFAULT_HOSTNAME} if it couldn't be determined.
+     */
     private static String getHostname() {
         try {
             // TODO: Cache this info
@@ -60,6 +82,12 @@ public class EventBuilder {
         }
     }
 
+    /**
+     * Sets default values for each field that hasn't been provided manually.
+     *
+     * @param event currently handled event.
+     */
+    //TODO: Shouldn't this be removed to rely on sentry default settings?
     private static void autoSetMissingValues(LoggedEvent event) {
         // Ensure that an actual message is set
         if (event.getMessage() == null)
@@ -84,11 +112,13 @@ public class EventBuilder {
         // Ensure that a hostname is set
         if (event.getServerName() == null)
             event.setServerName(getHostname());
-
-        // TODO: As the server can figure that itself, wouldn't it be better to assume that if no checksum is given
-        // the server will be in charge of generating it?
     }
 
+    /**
+     * Ensures that every field in the {@code LoggedEvent} are immutable to avoid confusion later.
+     *
+     * @param event event to make immutable.
+     */
     private static void makeImmutable(LoggedEvent event) {
         // Make the tags unmodifiable
         Map<String, Set<String>> unmodifiablesTags = new HashMap<String, Set<String>>(event.getTags().size());
@@ -101,9 +131,16 @@ public class EventBuilder {
         event.setSentryInterfaces(Collections.unmodifiableMap(event.getSentryInterfaces()));
     }
 
+    /**
+     * Determines the culprit value for an event based on a {@code Throwable}.
+     *
+     * @param throwable throwable caught, responsible of the event.
+     * @return the name of the method/class responsible for the event, based on the {@code Throwable}.
+     */
     private static String determineCulprit(Throwable throwable) {
         Throwable currentThrowable = throwable;
         String culprit = null;
+        // Attempts to go through each cause, in case the last ones do not provide a stacktrace.
         while (currentThrowable != null) {
             StackTraceElement[] elements = currentThrowable.getStackTrace();
             if (elements.length > 0) {
@@ -115,6 +152,18 @@ public class EventBuilder {
         return culprit;
     }
 
+    /**
+     * Adds a tag to an event.
+     * <p>
+     * Multiple calls to {@code addTag} allow to have more that one value for a single tag.<br />
+     * This allows to set a tag value in different contexts.
+     * </p>
+     *
+     * @param tagKey   name of the tag.
+     * @param tagValue value of the tag.
+     * @return the current {@code EventBuilder} for chained calls.
+     */
+    //TODO: Check that the tag system works indeed this way.
     public EventBuilder addTag(String tagKey, String tagValue) {
         Set<String> tagValues = event.getTags().get(tagKey);
         if (tagValues == null) {
@@ -126,28 +175,50 @@ public class EventBuilder {
         return this;
     }
 
+    /**
+     * Sets the culprit in the event based on a {@code Throwable}.
+     *
+     * @param throwable throwable responsible of the event.
+     * @return the current {@code EventBuilder} for chained calls.
+     */
     public EventBuilder setCulprit(Throwable throwable) {
         return setCulprit(determineCulprit(throwable));
     }
 
+    /**
+     * Sets the culprit in the event.
+     *
+     * @param culprit culprit.
+     * @return the current {@code EventBuilder} for chained calls.
+     */
     public EventBuilder setCulprit(String culprit) {
         event.setCulprit(culprit);
         return this;
     }
 
+    /**
+     * Adds a {@link SentryInterface} to the event.
+     * <p>
+     * If a {@code SentryInterface} with the same interface name has already been added, the new one will replace
+     * the old one.
+     * </p>
+     *
+     * @param sentryInterface sentry interface to add to the event.
+     * @return the current {@code EventBuilder} for chained calls.
+     */
     public EventBuilder addSentryInterface(SentryInterface sentryInterface) {
         event.getSentryInterfaces().put(sentryInterface.getInterfaceName(), sentryInterface);
         return this;
     }
 
     /**
-     * Manually set the checksum for the current event.
+     * Sets the checksum for the current event.
      * <p>
      * It's recommended to rely instead on the checksum system provided by Sentry.
      * </p>
      *
-     * @param checksum
-     * @return
+     * @param checksum checksum for the event.
+     * @return the current {@code EventBuilder} for chained calls.
      */
     public EventBuilder setChecksum(String checksum) {
         event.setChecksum(checksum);
@@ -155,16 +226,17 @@ public class EventBuilder {
     }
 
     /**
+     * Generates a checksum from a given content and set it to the current event.
      *
-     * @param contentToCheckSum
-     * @return
+     * @param contentToChecksum content to checksum.
+     * @return the current {@code EventBuilder} for chained calls.
      */
-    public EventBuilder generateChecksum(String contentToCheckSum) {
-        return setChecksum(calculateChecksum(contentToCheckSum));
+    public EventBuilder generateChecksum(String contentToChecksum) {
+        return setChecksum(calculateChecksum(contentToChecksum));
     }
 
     /**
-     * Finalise the {@link LoggedEvent} and returns it.
+     * Finalises the {@link LoggedEvent} and returns it.
      * <p>
      * This operations will automatically set the missing values and make the mutable values immutable.
      * </p>
