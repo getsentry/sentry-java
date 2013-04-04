0a752d76abcb63a56022fa2c855bd5058e7f192d
Colin Hebert <colin.hebert@it.ox.ac.uk>
2013-04-04 17:05:46 +0100

    Add documentation to the Connection interface

--- raven/src/main/java/net/kencochrane/raven/connection/Connection.java
+++ raven/src/main/java/net/kencochrane/raven/connection/Connection.java
@@ -2,6 +2,14 @@ package net.kencochrane.raven.connection;
 
 import net.kencochrane.raven.event.LoggedEvent;
 
+/**
+ * Connection to a Sentry server, allowing to send captured events.
+ */
 public interface Connection {
+    /**
+     * Sends an event to the sentry server.
+     *
+     * @param event captured event to add in Sentry.
+     */
     void send(LoggedEvent event);
 }
