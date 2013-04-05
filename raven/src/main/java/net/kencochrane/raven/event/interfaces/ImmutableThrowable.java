42678ec617d94ff359fe7173276393fcf18bdfc4
Colin Hebert <colin.hebert@it.ox.ac.uk>
2013-04-05 13:02:28 +0100

    Add a class to make Throwable immutable

---
+++ raven/src/main/java/net/kencochrane/raven/event/interfaces/ImmutableThrowable.java
@@ -0,0 +1,77 @@
+package net.kencochrane.raven.event.interfaces;
+
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+/**
+ * Wrapper for {@code Throwable} to make it immutable.
+ */
+public class ImmutableThrowable extends Throwable {
+    private final Throwable actualThrowable;
+
+    /**
+     * Creates an immutable wrapper for the given throwable.
+     *
+     * @param actualThrowable exception wrapped.
+     */
+    public ImmutableThrowable(Throwable actualThrowable) {
+        this.actualThrowable = actualThrowable;
+    }
+
+    @Override
+    public String getMessage() {
+        return actualThrowable.getMessage();
+    }
+
+    @Override
+    public String getLocalizedMessage() {
+        return actualThrowable.getLocalizedMessage();
+    }
+
+    @Override
+    public Throwable getCause() {
+        return actualThrowable.getCause();
+    }
+
+    @Override
+    public Throwable initCause(Throwable cause) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String toString() {
+        return actualThrowable.toString();
+    }
+
+    @Override
+    public void printStackTrace() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void printStackTrace(PrintStream s) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void printStackTrace(PrintWriter s) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Throwable fillInStackTrace() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public StackTraceElement[] getStackTrace() {
+        StackTraceElement[] stackTrace = actualThrowable.getStackTrace();
+        return Arrays.copyOf(stackTrace, stackTrace.length);
+    }
+
+    @Override
+    public void setStackTrace(StackTraceElement[] stackTrace) {
+        throw new UnsupportedOperationException();
+    }
+}
