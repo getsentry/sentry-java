d21e94dfd74bec05232292c90855d259059f0d19
Colin Hebert <hebert.colin@gmail.com>
2013-04-06 18:11:44 +0100

    Use a DaemonThreadFactory for AsyncConnection

--- raven/src/main/java/net/kencochrane/raven/connection/AsyncConnection.java
+++ raven/src/main/java/net/kencochrane/raven/connection/AsyncConnection.java
@@ -4,6 +4,10 @@ import net.kencochrane.raven.event.LoggedEvent;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Asynchronous usage of a connection.
@@ -13,12 +17,13 @@ import java.util.concurrent.Executors;
  * </p>
  */
 public class AsyncConnection implements Connection {
+    private static final Logger logger = Logger.getLogger(AsyncConnection.class.getCanonicalName());
     /**
      * Connection used to actually send the events.
      */
     private final Connection actualConnection;
     // TODO: Allow the user to change to a custom executor?
-    private final ExecutorService executorService = Executors.newCachedThreadPool();
+    private final ExecutorService executorService = Executors.newCachedThreadPool(new DaemonThreadFactory());
 
     /**
      * Create a connection which will rely on an executor to send events.
@@ -35,6 +40,28 @@ public class AsyncConnection implements Connection {
         executorService.execute(new LoggedEventSubmitter(event));
     }
 
+    private static final class DaemonThreadFactory implements ThreadFactory {
+        private static final AtomicInteger poolNumber = new AtomicInteger(1);
+        private final ThreadGroup group;
+        private final AtomicInteger threadNumber = new AtomicInteger(1);
+        private final String namePrefix;
+
+        private DaemonThreadFactory() {
+            SecurityManager s = System.getSecurityManager();
+            group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+            namePrefix = "pool-" + poolNumber.getAndIncrement() + "-thread-";
+        }
+
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
+            if (!t.isDaemon())
+                t.setDaemon(true);
+            if (t.getPriority() != Thread.NORM_PRIORITY)
+                t.setPriority(Thread.NORM_PRIORITY);
+            return t;
+        }
+    }
+
     /**
      * Simple runnable using the {@link #send(net.kencochrane.raven.event.LoggedEvent)} method of the
      * {@link #actualConnection}.
@@ -48,7 +75,11 @@ public class AsyncConnection implements Connection {
 
         @Override
         public void run() {
-            actualConnection.send(event);
+            try {
+                actualConnection.send(event);
+            } catch (Exception e) {
+                logger.log(Level.SEVERE, "An exception occurred while sending the event to Sentry.", e);
+            }
         }
     }
 }
