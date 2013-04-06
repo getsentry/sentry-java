3a1d4c4436f57486ed9a59e5b7b701fdb0161468
Colin Hebert <hebert.colin@gmail.com>
2013-04-06 19:39:58 +0100

    Create raven appenders for logback and log4j

---
+++ raven-log4j/src/main/java/net/kencochrane/raven/log4j/SentryAppender.java
@@ -0,0 +1,65 @@
+package net.kencochrane.raven.log4j;
+
+import net.kencochrane.raven.Raven;
+import net.kencochrane.raven.event.EventBuilder;
+import net.kencochrane.raven.event.LoggedEvent;
+import net.kencochrane.raven.event.interfaces.ExceptionInterface;
+import net.kencochrane.raven.event.interfaces.StackTraceInterface;
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Level;
+import org.apache.log4j.spi.LoggingEvent;
+
+import java.util.Date;
+
+public class SentryAppender extends AppenderSkeleton {
+    private final Raven raven;
+
+    public SentryAppender() {
+        this(new Raven());
+    }
+
+    public SentryAppender(Raven raven) {
+        this.raven = raven;
+    }
+
+    private static LoggedEvent.Level formatLevel(LoggingEvent loggingEvent) {
+        if (loggingEvent.getLevel().isGreaterOrEqual(Level.FATAL)) {
+            return LoggedEvent.Level.FATAL;
+        } else if (loggingEvent.getLevel().isGreaterOrEqual(Level.ERROR)) {
+            return LoggedEvent.Level.ERROR;
+        } else if (loggingEvent.getLevel().isGreaterOrEqual(Level.WARN)) {
+            return LoggedEvent.Level.WARNING;
+        } else if (loggingEvent.getLevel().isGreaterOrEqual(Level.INFO)) {
+            return LoggedEvent.Level.INFO;
+        } else if (loggingEvent.getLevel().isGreaterOrEqual(Level.ALL)) {
+            return LoggedEvent.Level.DEBUG;
+        } else return null;
+    }
+
+    @Override
+    protected void append(LoggingEvent loggingEvent) {
+        EventBuilder eventBuilder = new EventBuilder()
+                .setTimestamp(new Date(loggingEvent.getTimeStamp()))
+                .setMessage(loggingEvent.getRenderedMessage())
+                .setLogger(loggingEvent.getLoggerName())
+                .setLevel(formatLevel(loggingEvent))
+                .setCulprit(loggingEvent.getLoggerName());
+
+        if (loggingEvent.getThrowableInformation() != null) {
+            Throwable throwable = loggingEvent.getThrowableInformation().getThrowable();
+            eventBuilder.addSentryInterface(new ExceptionInterface(throwable))
+                    .addSentryInterface(new StackTraceInterface(throwable));
+        }
+
+        raven.sendEvent(eventBuilder);
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public boolean requiresLayout() {
+        return false;
+    }
+}
---
+++ raven-logback/src/main/java/net/kencochrane/raven/logback/SentryAppender.java
@@ -0,0 +1,68 @@
+package net.kencochrane.raven.logback;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.ThrowableProxy;
+import ch.qos.logback.core.AppenderBase;
+import net.kencochrane.raven.Raven;
+import net.kencochrane.raven.event.EventBuilder;
+import net.kencochrane.raven.event.LoggedEvent;
+import net.kencochrane.raven.event.interfaces.ExceptionInterface;
+import net.kencochrane.raven.event.interfaces.MessageInterface;
+import net.kencochrane.raven.event.interfaces.StackTraceInterface;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class SentryAppender extends AppenderBase<ILoggingEvent> {
+    private final Raven raven;
+
+    public SentryAppender() {
+        this(new Raven());
+    }
+
+    public SentryAppender(Raven raven) {
+        this.raven = raven;
+    }
+
+    private static List<String> formatArguments(Object[] argumentArray) {
+        List<String> arguments = new ArrayList<String>(argumentArray.length);
+        for (Object argument : argumentArray) {
+            arguments.add(argument.toString());
+        }
+        return arguments;
+    }
+
+    private static LoggedEvent.Level formatLevel(ILoggingEvent iLoggingEvent) {
+        if (iLoggingEvent.getLevel().isGreaterOrEqual(Level.ERROR)) {
+            return LoggedEvent.Level.ERROR;
+        } else if (iLoggingEvent.getLevel().isGreaterOrEqual(Level.WARN)) {
+            return LoggedEvent.Level.WARNING;
+        } else if (iLoggingEvent.getLevel().isGreaterOrEqual(Level.INFO)) {
+            return LoggedEvent.Level.INFO;
+        } else if (iLoggingEvent.getLevel().isGreaterOrEqual(Level.ALL)) {
+            return LoggedEvent.Level.DEBUG;
+        } else return null;
+    }
+
+    @Override
+    protected void append(ILoggingEvent iLoggingEvent) {
+        EventBuilder eventBuilder = new EventBuilder()
+                .setTimestamp(new Date(iLoggingEvent.getTimeStamp()))
+                .setMessage(iLoggingEvent.getFormattedMessage())
+                .setLogger(iLoggingEvent.getLoggerName())
+                .setLevel(formatLevel(iLoggingEvent))
+                .setCulprit(iLoggingEvent.getLoggerName());
+
+        if (iLoggingEvent.getThrowableProxy() != null) {
+            Throwable throwable = ((ThrowableProxy) iLoggingEvent.getThrowableProxy()).getThrowable();
+            eventBuilder.addSentryInterface(new ExceptionInterface(throwable))
+                    .addSentryInterface(new StackTraceInterface(throwable));
+        } else if (iLoggingEvent.getArgumentArray() != null) {
+            eventBuilder.addSentryInterface(new MessageInterface(iLoggingEvent.getMessage(),
+                    formatArguments(iLoggingEvent.getArgumentArray())));
+        }
+        raven.sendEvent(eventBuilder);
+    }
+}
