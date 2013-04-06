2c2da5b7b0bfae85d61e73142460ba9f0950a38f
Colin Hebert <hebert.colin@gmail.com>
2013-04-06 18:14:13 +0100

    Add details on the StackTraceInterface

--- raven/src/main/java/net/kencochrane/raven/event/interfaces/StackTraceInterface.java
+++ raven/src/main/java/net/kencochrane/raven/event/interfaces/StackTraceInterface.java
@@ -4,6 +4,9 @@ public class StackTraceInterface implements SentryInterface {
     public static final String STACKTRACE_INTERFACE = "sentry.interfaces.Stacktrace";
     private final ImmutableThrowable throwable;
 
+    //TODO: Base this interface on a unique stacktrace (rather than an entire exception)
+    //This should be done when the exception system in Sentry will be improved to support chained exception
+    //For now, a fake stacktrace (containing the parent exceptions and their stacktraces) will be used.
     public StackTraceInterface(Throwable throwable) {
         this.throwable = new ImmutableThrowable(throwable);
     }
