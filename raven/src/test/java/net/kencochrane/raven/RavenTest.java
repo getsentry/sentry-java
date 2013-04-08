976242fe2e0a5c898f4bdc5be3132421854efbc4
Colin Hebert <colin.hebert@it.ox.ac.uk>
2013-04-08 18:24:24 +0100

    Reorganise imports

--- raven/src/test/java/net/kencochrane/raven/RavenTest.java
+++ raven/src/test/java/net/kencochrane/raven/RavenTest.java
@@ -3,7 +3,6 @@ package net.kencochrane.raven;
 import net.kencochrane.raven.connection.Connection;
 import net.kencochrane.raven.event.EventBuilder;
 import net.kencochrane.raven.event.LoggedEvent;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -13,9 +12,7 @@ import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class RavenTest {
     @Mock
