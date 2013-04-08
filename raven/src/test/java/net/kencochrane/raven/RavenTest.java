6d88744a1b1026150e4bf58522bd3dfbb1b6c985
Colin Hebert <hebert.colin@gmail.com>
2013-04-08 08:47:02 +0100

    Fix raven unit test
    sendEvent(EventBuilder) now returns the UUID of the generated event
    create a mock to avoid a NPE and test the returned UUID

--- raven/src/test/java/net/kencochrane/raven/RavenTest.java
+++ raven/src/test/java/net/kencochrane/raven/RavenTest.java
@@ -3,14 +3,19 @@ package net.kencochrane.raven;
 import net.kencochrane.raven.connection.Connection;
 import net.kencochrane.raven.event.EventBuilder;
 import net.kencochrane.raven.event.LoggedEvent;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class RavenTest {
     @Mock
@@ -34,9 +39,14 @@ public class RavenTest {
     @Test
     public void testSendEventBuilder() {
         EventBuilder eventBuilder = mock(EventBuilder.class);
-        raven.sendEvent(eventBuilder);
+        LoggedEvent loggedEvent = mock(LoggedEvent.class);
+        UUID mockUuid = UUID.randomUUID();
+        when(eventBuilder.build()).thenReturn(loggedEvent);
+        when(loggedEvent.getId()).thenReturn(mockUuid);
+        UUID uuid = raven.sendEvent(eventBuilder);
 
         verify(eventBuilder).build();
         verify(mockConnection).send(any(LoggedEvent.class));
+        assertEquals(mockUuid, uuid);
     }
 }
