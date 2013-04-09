cf752f01ef49fe6f26789ad3220863f1bd37a44f
Colin Hebert <colin.hebert@it.ox.ac.uk>
2013-04-09 18:26:56 +0100

    Set a maximum size for the Message interface

--- raven/src/main/java/net/kencochrane/raven/marshaller/json/MessageInterfaceBinding.java
+++ raven/src/main/java/net/kencochrane/raven/marshaller/json/MessageInterfaceBinding.java
@@ -6,13 +6,31 @@ import net.kencochrane.raven.event.interfaces.MessageInterface;
 import java.io.IOException;
 
 public class MessageInterfaceBinding implements InterfaceBinding<MessageInterface> {
+    /**
+     * Maximum length for a message.
+     */
+    public static final int MAX_MESSAGE_LENGTH = 1000;
     private static final String MESSAGE_PARAMETER = "message";
     private static final String PARAMS_PARAMETER = "params";
+
+    /**
+     * Formats a message, ensuring that the maximum length {@link #MAX_MESSAGE_LENGTH} isn't reached.
+     *
+     * @param message message to format.
+     * @return formatted message (shortened if necessary).
+     */
+    private String formatMessage(String message) {
+        if (message == null)
+            return null;
+        else if (message.length() > MAX_MESSAGE_LENGTH)
+            return message.substring(0, MAX_MESSAGE_LENGTH);
+        else return message;
+    }
 
     @Override
     public void writeInterface(JsonGenerator generator, MessageInterface messageInterface) throws IOException {
         generator.writeStartObject();
-        generator.writeStringField(MESSAGE_PARAMETER, messageInterface.getMessage());
+        generator.writeStringField(MESSAGE_PARAMETER, formatMessage(messageInterface.getMessage()));
         generator.writeArrayFieldStart(PARAMS_PARAMETER);
         for (String parameter : messageInterface.getParams()) {
             generator.writeString(parameter);
