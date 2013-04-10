e55d3d7218bf31a687c643123841e0df1d182311
Colin Hebert <colin.hebert@it.ox.ac.uk>
2013-04-10 14:04:29 +0100

    Create Helpers to run with the EventBuilder

---
+++ raven/src/main/java/net/kencochrane/raven/event/helper/EventBuilderHelper.java
@@ -0,0 +1,7 @@
+package net.kencochrane.raven.event.helper;
+
+import net.kencochrane.raven.event.EventBuilder;
+
+public interface EventBuilderHelper {
+    void helpBuildingEvent(EventBuilder eventBuilder);
+}
