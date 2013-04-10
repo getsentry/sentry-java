a761affd561d34671520656a55d760cb7ac73580
Colin Hebert <colin.hebert@it.ox.ac.uk>
2013-04-10 14:10:36 +0100

    Add a builder helper to extract HttpRequests
    Use a ServletRequestListener to capture an HttpServletRequest and a
    BuilderHelper to extract the ThreadLocal content when building the
    event.

---
+++ raven/src/main/java/net/kencochrane/raven/event/helper/HttpEventBuilderHelper.java
@@ -0,0 +1,17 @@
+package net.kencochrane.raven.event.helper;
+
+import net.kencochrane.raven.event.EventBuilder;
+import net.kencochrane.raven.event.interfaces.HttpInterface;
+import net.kencochrane.raven.servlet.RavenServletRequestListener;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class HttpEventBuilderHelper implements EventBuilderHelper {
+    @Override
+    public void helpBuildingEvent(EventBuilder eventBuilder) {
+        HttpServletRequest servletRequest = RavenServletRequestListener.getServletRequest();
+        if (servletRequest != null) {
+            eventBuilder.addSentryInterface(new HttpInterface(servletRequest));
+        }
+    }
+}
---
+++ raven/src/main/java/net/kencochrane/raven/servlet/RavenServletRequestListener.java
@@ -0,0 +1,24 @@
+package net.kencochrane.raven.servlet;
+
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
+import javax.servlet.http.HttpServletRequest;
+
+public class RavenServletRequestListener implements ServletRequestListener {
+    private static final ThreadLocal<HttpServletRequest> THREAD_REQUEST = new ThreadLocal<HttpServletRequest>();
+
+    public static HttpServletRequest getServletRequest() {
+        return THREAD_REQUEST.get();
+    }
+
+    @Override
+    public void requestDestroyed(ServletRequestEvent servletRequestEvent) {
+        THREAD_REQUEST.remove();
+    }
+
+    @Override
+    public void requestInitialized(ServletRequestEvent servletRequestEvent) {
+        if (servletRequestEvent instanceof HttpServletRequest)
+            THREAD_REQUEST.set((HttpServletRequest) servletRequestEvent.getServletRequest());
+    }
+}
