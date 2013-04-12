3bcbd5a8680d28c7bab094e090fc720cf8b2f7a3
Colin Hebert <colin.hebert@it.ox.ac.uk>
2013-04-12 20:25:24 +0100

    Add a validation class to test Auth headers

---
+++ sentry-stub/src/main/java/net/kencochrane/raven/sentrystub/auth/AuthValidator.java
@@ -0,0 +1,74 @@
+package net.kencochrane.raven.sentrystub.auth;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AuthValidator {
+    private static final Collection<String> SENTRY_PROTOCOL_VERSIONS = Arrays.asList("3");
+    private static final String SENTRY_VERSION_PARAMETER = "Sentry sentry_version";
+    private static final String PUBLIC_KEY_PARAMETER = "sentry_key";
+    private static final String SECRET_KEY_PARAMETER = "sentry_secret";
+    private static final String SENTRY_CLIENT_PARAMETER = "sentry_client";
+    private final Map<String, String> publicKeySecretKey = new HashMap<String, String>();
+    private final Map<String, String> publicKeyProjectId = new HashMap<String, String>();
+
+    public void addUser(String publicKey, String secretKey, String projectId) {
+        if (publicKeySecretKey.containsKey(publicKey) || publicKeyProjectId.containsKey(publicKey)) {
+            throw new IllegalArgumentException("There is already a user " + publicKey);
+        }
+
+        publicKeySecretKey.put(publicKey, secretKey);
+        publicKeyProjectId.put(publicKey, projectId);
+    }
+
+    public void validateSentryAuth(Map<String, String> authParameters, String projectId) {
+        InvalidAuthException invalidAuthException = new InvalidAuthException("The auth parameters weren't valid");
+
+        validateVersion(authParameters.get(SENTRY_VERSION_PARAMETER), invalidAuthException);
+        validateKeys(authParameters.get(PUBLIC_KEY_PARAMETER), authParameters.get(SECRET_KEY_PARAMETER),
+                invalidAuthException);
+        validateProject(authParameters.get(PUBLIC_KEY_PARAMETER), projectId, invalidAuthException);
+        validateClient(authParameters.get(SENTRY_CLIENT_PARAMETER), invalidAuthException);
+
+        if (!invalidAuthException.isEmpty())
+            throw invalidAuthException;
+    }
+
+    private void validateVersion(String authSentryVersion, InvalidAuthException invalidAuthException) {
+        if (authSentryVersion == null || !SENTRY_PROTOCOL_VERSIONS.contains(authSentryVersion))
+            invalidAuthException.addDetailedMessage("The version '" + authSentryVersion + "' isn't valid, " +
+                    "only '" + SENTRY_PROTOCOL_VERSIONS + "' are accepted.");
+    }
+
+    private void validateKeys(String publicKey, String secretKey,
+                              InvalidAuthException invalidAuthException) {
+        if (publicKey == null)
+            invalidAuthException.addDetailedMessage("No public key provided");
+        else if (!publicKeySecretKey.containsKey(publicKey))
+            invalidAuthException.addDetailedMessage("The public key '" + publicKey + "' isn't associated " +
+                    "with a secret key.");
+
+        if (secretKey == null)
+            invalidAuthException.addDetailedMessage("No secret key provided");
+
+        if (secretKey != null && publicKey != null && !publicKeySecretKey.get(publicKey).equals(secretKey))
+            invalidAuthException.addDetailedMessage("The secret key '" + secretKey + "' " +
+                    "isn't valid for '" + publicKey + "'");
+    }
+
+    private void validateProject(String publicKey, String projectId, InvalidAuthException invalidAuthException) {
+        if (projectId == null)
+            invalidAuthException.addDetailedMessage("No project ID provided");
+
+        if (publicKey != null && !publicKeyProjectId.get(publicKey).equals(projectId))
+            invalidAuthException.addDetailedMessage("The project '" + projectId + "' " +
+                    "can't be accessed by ' " + publicKey + " '");
+    }
+
+    private void validateClient(String client, InvalidAuthException invalidAuthException) {
+        if (client == null)
+            invalidAuthException.addDetailedMessage("The client name is mandatory.");
+    }
+}
---
+++ sentry-stub/src/main/java/net/kencochrane/raven/sentrystub/auth/InvalidAuthException.java
@@ -0,0 +1,36 @@
+package net.kencochrane.raven.sentrystub.auth;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class InvalidAuthException extends RuntimeException {
+    private final Collection<String> detailedMessages = new LinkedList<String>();
+
+    public InvalidAuthException() {
+    }
+
+    public InvalidAuthException(String message) {
+        super(message);
+    }
+
+    public InvalidAuthException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidAuthException(Throwable cause) {
+        super(cause);
+    }
+
+    public Collection<String> getDetailedMessages() {
+        return Collections.unmodifiableCollection(detailedMessages);
+    }
+
+    public void addDetailedMessage(String message) {
+        detailedMessages.add(message);
+    }
+
+    public boolean isEmpty(){
+        return detailedMessages.isEmpty();
+    }
+}
