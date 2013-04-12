4cb114e51b2b8b847c36063ec0e84c775e6be1e0
Colin Hebert <hebert.colin@gmail.com>
2013-04-12 23:44:42 +0100

    Automatically parse Events retreived from HTTP

--- sentry-stub/src/main/java/net/kencochrane/raven/sentrystub/SentryHttpServlet.java
+++ sentry-stub/src/main/java/net/kencochrane/raven/sentrystub/SentryHttpServlet.java
@@ -1,20 +1,28 @@
 package net.kencochrane.raven.sentrystub;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.kencochrane.raven.sentrystub.event.Event;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
 
 @WebServlet(name = "SentryHttpServlet", displayName = "SentryHttpServlet", urlPatterns = "/api/*")
 public class SentryHttpServlet extends HttpServlet {
+    private JsonDecoder jsonDecoder = new JsonDecoder();
+    private ObjectMapper om = new ObjectMapper();
+
     public SentryHttpServlet() {
         super();
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("test");
+        InputStream jsonStream = jsonDecoder.decapsulateContent(req.getInputStream());
+        Event e = om.readValue(jsonStream, Event.class);
     }
 }
