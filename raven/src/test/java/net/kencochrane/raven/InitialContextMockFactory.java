68b8b308904e4f40a005911b95f0aba8ed227c62
Colin Hebert <hebert.colin@gmail.com>
2013-04-15 21:52:20 +0100

    Add documentation to InitialContextMockFactory

--- raven/src/test/java/net/kencochrane/raven/InitialContextMockFactory.java
+++ raven/src/test/java/net/kencochrane/raven/InitialContextMockFactory.java
@@ -5,6 +5,11 @@ import javax.naming.NamingException;
 import javax.naming.spi.InitialContextFactory;
 import java.util.Hashtable;
 
+/**
+ * JNDI initial context factory allowing to create custom mocked contexts.
+ *
+ * @see DsnTest#setUp()
+ */
 public class InitialContextMockFactory implements InitialContextFactory {
     public static Context context;
 
