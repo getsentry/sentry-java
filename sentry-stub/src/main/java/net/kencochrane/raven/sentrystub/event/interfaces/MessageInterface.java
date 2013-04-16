b800522c17c4153a78dd930ff6a4210fc803b3fc
Colin Hebert <colin.hebert@it.ox.ac.uk>
2013-04-16 12:06:45 +0100

    Give elements to parse the Message interface

--- sentry-stub/src/main/java/net/kencochrane/raven/sentrystub/event/interfaces/MessageInterface.java
+++ sentry-stub/src/main/java/net/kencochrane/raven/sentrystub/event/interfaces/MessageInterface.java
@@ -1,8 +1,12 @@
 package net.kencochrane.raven.sentrystub.event.interfaces;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public class MessageInterface {
+    @JsonProperty(value = "message")
     private String message;
+    @JsonProperty(value = "params")
     private List<String> params;
 }
