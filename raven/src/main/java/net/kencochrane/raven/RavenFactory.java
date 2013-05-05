71d561a811dbb018a0385048c779a715ba634f5f
Colin Hebert <hebert.colin@gmail.com>
2013-05-05 16:58:22 +0200

    Do not obtain a Raven instance safely
    Obtaining a raven instance should fail if the DSN isn't valid or something goes
    wrong, it's the user's responsibility to handle that.

--- raven/src/main/java/net/kencochrane/raven/RavenFactory.java
+++ raven/src/main/java/net/kencochrane/raven/RavenFactory.java
@@ -11,12 +11,11 @@ import java.util.logging.Logger;
  * </p>
  */
 public abstract class RavenFactory {
-    private static final Logger logger = Logger.getLogger(RavenFactory.class.getCanonicalName());
     private static final ServiceLoader<RavenFactory> RAVEN_FACTORIES = ServiceLoader.load(RavenFactory.class);
 
     public static Raven ravenInstance(Dsn dsn) {
         for (RavenFactory ravenFactory : RAVEN_FACTORIES) {
-            Raven raven = getRavenSafely(dsn, ravenFactory);
+            Raven raven = ravenFactory.createRavenInstance(dsn);
             if (raven != null) {
                 return raven;
             }
@@ -33,24 +32,13 @@ public abstract class RavenFactory {
             if (!ravenFactoryName.equals(ravenFactory.getClass().getCanonicalName()))
                 continue;
 
-            Raven raven = getRavenSafely(dsn, ravenFactory);
+            Raven raven = ravenFactory.createRavenInstance(dsn);
             if (raven != null) {
                 return raven;
             }
         }
 
         throw new IllegalStateException("Couldn't create a raven instance for '" + dsn + "'");
-    }
-
-    private static Raven getRavenSafely(Dsn dsn, RavenFactory ravenFactory) {
-        Raven raven = null;
-        try {
-            raven = ravenFactory.createRavenInstance(dsn);
-        } catch (Exception e) {
-            logger.log(Level.WARNING, "An exception occurred during the creation of a Raven instance with "
-                    + "'" + ravenFactory + "' using the DSN '" + dsn + "'", e);
-        }
-        return raven;
     }
 
     public abstract Raven createRavenInstance(Dsn dsn);
