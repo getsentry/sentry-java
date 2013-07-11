a152e3d0797f955c167c58fe3d7e6f8b292abadf
Colin Hebert <colin.hebert@it.ox.ac.uk>
2013-07-11 17:00:07 +0100

    Add test to check that thread name is added to the event

--- raven-log4j/src/test/java/net/kencochrane/raven/log4j/SentryAppenderTest.java
+++ raven-log4j/src/test/java/net/kencochrane/raven/log4j/SentryAppenderTest.java
@@ -4,10 +4,16 @@ import net.kencochrane.raven.AbstractLoggerTest;
 import net.kencochrane.raven.event.Event;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.mockito.Mockito.*;
 
 public class SentryAppenderTest extends AbstractLoggerTest {
     private Logger logger;
@@ -64,5 +70,16 @@ public class SentryAppenderTest extends AbstractLoggerTest {
     @Override
     public void testLogParametrisedMessage() throws Exception {
         // Parametrised messages aren't supported
+    }
+
+    @Test
+    public void testThreadNameAddedToExtra() {
+        ArgumentCaptor<Event> eventCaptor = ArgumentCaptor.forClass(Event.class);
+
+        logger.info("testMessage");
+
+        verify(mockRaven).sendEvent(eventCaptor.capture());
+        assertThat(eventCaptor.getValue().getExtra(),
+                Matchers.<String, Object>hasEntry(SentryAppender.THREAD_NAME, Thread.currentThread().getName()));
     }
 }
