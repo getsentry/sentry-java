bd054463dbc5ce2cd80de126690f92b7785cd62d
Colin Hebert <colin.hebert@it.ox.ac.uk>
2013-07-12 13:52:40 +0100

    Document InterfaceBinding

--- raven/src/main/java/net/kencochrane/raven/marshaller/json/InterfaceBinding.java
+++ raven/src/main/java/net/kencochrane/raven/marshaller/json/InterfaceBinding.java
@@ -5,6 +5,18 @@ import net.kencochrane.raven.event.interfaces.SentryInterface;
 
 import java.io.IOException;
 
+/**
+ * An interface binding allows to encore a {@link SentryInterface} of a specific type into a JSON stream.
+ *
+ * @param <T> type of {@link SentryInterface} supported by the binder.
+ */
 public interface InterfaceBinding<T extends SentryInterface> {
+    /**
+     * Encodes the content of a sentry interface into a JSON stream.
+     *
+     * @param generator       JSON generator allowing to write JSON content.
+     * @param sentryInterface interface to encode.
+     * @throws IOException thrown in case of failure during the generation of JSON content.
+     */
     void writeInterface(JsonGenerator generator, T sentryInterface) throws IOException;
 }
