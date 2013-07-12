26e4fd1c29d900b3dd66af3dd23113352ac449ee
Colin Hebert <colin.hebert@it.ox.ac.uk>
2013-07-12 13:50:11 +0100

    Document InvalidDsnException

--- raven/src/main/java/net/kencochrane/raven/dsn/InvalidDsnException.java
+++ raven/src/main/java/net/kencochrane/raven/dsn/InvalidDsnException.java
@@ -1,5 +1,12 @@
 package net.kencochrane.raven.dsn;
 
+/**
+ * Exception thrown whenever the given {@link Dsn} as been detected as invalid.
+ * <p>
+ * The invalidity of the DSN can either be due on the content of the DSN (invalid or missing parameters) or the sentry
+ * server issuing an authentication error.
+ * </p>
+ */
 public class InvalidDsnException extends RuntimeException {
     public InvalidDsnException() {
     }
