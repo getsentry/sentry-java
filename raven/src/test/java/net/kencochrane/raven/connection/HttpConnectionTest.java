2b6d0b55d9804c583c0fa1c7d4d18eb023b3533c
Colin Hebert <hebert.colin@gmail.com>
2013-09-04 11:24:53 +1000

    Use JMockit with HttpConnectionTest

--- raven/src/test/java/net/kencochrane/raven/connection/HttpConnectionTest.java
+++ raven/src/test/java/net/kencochrane/raven/connection/HttpConnectionTest.java
@@ -1,147 +1,164 @@
 package net.kencochrane.raven.connection;
 
+import mockit.Expectations;
+import mockit.Injectable;
+import mockit.NonStrictExpectations;
+import mockit.Verifications;
 import net.kencochrane.raven.Raven;
-import net.kencochrane.raven.dsn.Dsn;
 import net.kencochrane.raven.event.Event;
-import net.kencochrane.raven.event.EventBuilder;
 import net.kencochrane.raven.marshaller.Marshaller;
-import org.hamcrest.CustomTypeSafeMatcher;
-import org.hamcrest.Matcher;
-import org.mockito.Answers;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
 import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLStreamHandler;
 import java.util.UUID;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.core.Is.is;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.*;
+import static org.hamcrest.Matchers.is;
 
 public class HttpConnectionTest {
+    private final String publicKey = UUID.randomUUID().toString();
+    private final String secretKey = UUID.randomUUID().toString();
     private HttpConnection httpConnection;
-    private String publicKey = UUID.randomUUID().toString();
-    private String secretKey = UUID.randomUUID().toString();
-    @Mock(answer = Answers.RETURNS_MOCKS)
+    @Injectable
     private HttpsURLConnection mockUrlConnection;
-    @Mock
+    @Injectable
     private Marshaller mockMarshaller;
 
     @BeforeMethod
     public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        URLStreamHandler stubUrlHandler = new URLStreamHandler() {
-            @Override
-            protected URLConnection openConnection(URL u) throws IOException {
-                return mockUrlConnection;
+        new NonStrictExpectations() {
+            @Injectable
+            private URL mockUrl;
+            @Injectable
+            private OutputStream mockOutputStream;
+            @Injectable
+            private InputStream mockInputStream;
+
+            {
+                mockUrl.openConnection();
+                result = mockUrlConnection;
+                mockUrlConnection.getOutputStream();
+                result = mockOutputStream;
+                mockUrlConnection.getInputStream();
+                result = mockInputStream;
+
+                httpConnection = new HttpConnection(mockUrl, publicKey, secretKey);
+                httpConnection.setMarshaller(mockMarshaller);
             }
         };
-
-        URL mockUrl = new URL(null, "http://", stubUrlHandler);
-        httpConnection = new HttpConnection(mockUrl, publicKey, secretKey);
-        httpConnection.setMarshaller(mockMarshaller);
     }
 
     @Test
-    public void testTimeout() throws Exception {
-        int timeout = 12;
+    public void testTimeout(@Injectable final Event mockEvent) throws Exception {
+        final int timeout = 12;
         httpConnection.setTimeout(timeout);
-        httpConnection.send(new EventBuilder().build());
 
-        verify(mockUrlConnection).setConnectTimeout(timeout);
+        httpConnection.send(mockEvent);
+
+        new Verifications() {{
+            mockUrlConnection.setConnectTimeout(timeout);
+        }};
     }
 
     @Test
-    public void testByPassSecurity() throws Exception {
-        ArgumentCaptor<HostnameVerifier> hostnameVerifierCaptor = ArgumentCaptor.forClass(HostnameVerifier.class);
-        httpConnection.send(new EventBuilder().build());
-        verify(mockUrlConnection, never()).setHostnameVerifier(any(HostnameVerifier.class));
+    public void testByPassSecurityDefaultsToFalse(@Injectable final Event mockEvent) throws Exception {
+        httpConnection.send(mockEvent);
 
-        reset(mockUrlConnection);
+        new Verifications() {{
+            mockUrlConnection.setHostnameVerifier((HostnameVerifier) any);
+            times = 0;
+        }};
+    }
+
+    @Test
+    public void testByPassSecurity(@Injectable final Event mockEvent) throws Exception {
         httpConnection.setBypassSecurity(true);
-        httpConnection.send(new EventBuilder().build());
-        verify(mockUrlConnection).setHostnameVerifier(hostnameVerifierCaptor.capture());
-        assertThat(hostnameVerifierCaptor.getValue().verify(null, null), is(true));
 
-        reset(mockUrlConnection);
+        httpConnection.send(mockEvent);
+
+        new Verifications() {
+            @Injectable
+            private String mockString;
+            @Injectable
+            private SSLSession mockSslSession;
+
+            {
+                HostnameVerifier hostnameVerifier;
+                mockUrlConnection.setHostnameVerifier(hostnameVerifier = withCapture());
+                assertThat(hostnameVerifier.verify(mockString, mockSslSession), is(true));
+            }
+        };
+    }
+
+    @Test
+    public void testDontByPassSecurity(@Injectable final Event mockEvent) throws Exception {
         httpConnection.setBypassSecurity(false);
-        httpConnection.send(new EventBuilder().build());
-        verify(mockUrlConnection, never()).setHostnameVerifier(any(HostnameVerifier.class));
+
+        httpConnection.send(mockEvent);
+
+        new Verifications() {{
+            mockUrlConnection.setHostnameVerifier((HostnameVerifier) any);
+            times = 0;
+        }};
     }
 
     @Test
-    public void testContentMarshalled() throws Exception {
-        Event event = new EventBuilder().build();
+    public void testContentMarshalled(@Injectable final Event mockEvent) throws Exception {
+        httpConnection.send(mockEvent);
 
-        httpConnection.send(event);
-
-        verify(mockMarshaller).marshall(eq(event), any(OutputStream.class));
+        new Verifications() {{
+            mockMarshaller.marshall(mockEvent, (OutputStream) any);
+        }};
     }
 
     @Test
-    public void testAuthHeaderSent() throws Exception {
-        httpConnection.send(new EventBuilder().build());
+    public void testAuthHeaderSent(@Injectable final Event mockEvent) throws Exception {
+        httpConnection.send(mockEvent);
 
-        verify(mockUrlConnection).setRequestProperty("User-Agent", Raven.NAME);
-        String expectedAuthRequest = "Sentry sentry_version=4,"
-                + "sentry_client=" + Raven.NAME + ","
-                + "sentry_key=" + publicKey + ","
-                + "sentry_secret=" + secretKey;
-        verify(mockUrlConnection).setRequestProperty("X-Sentry-Auth", expectedAuthRequest);
+
+        new Verifications() {{
+            mockUrlConnection.setRequestProperty("User-Agent", Raven.NAME);
+
+            String expectedAuthRequest = "Sentry sentry_version=4,"
+                    + "sentry_client=" + Raven.NAME + ","
+                    + "sentry_key=" + publicKey + ","
+                    + "sentry_secret=" + secretKey;
+            mockUrlConnection.setRequestProperty("X-Sentry-Auth", expectedAuthRequest);
+        }};
     }
 
-    @Test(enabled = false)
-    /**
-     * TODO: HTTP errors are thrown exceptions caught in the abstract class and not logged directly.
-     */
-    public void testHttpErrorLogged() throws Exception {
+    @Test(expectedExceptions = {ConnectionException.class})
+    public void testHttpErrorThrowsAnException(@Injectable final Event mockEvent) throws Exception {
         final String httpErrorMessage = UUID.randomUUID().toString();
-        ArgumentCaptor<LogRecord> logRecordCaptor = ArgumentCaptor.forClass(LogRecord.class);
-        Handler handler = mock(Handler.class);
-        Logger.getLogger(HttpConnection.class.getCanonicalName()).addHandler(handler);
-        when(mockUrlConnection.getOutputStream()).thenThrow(new IOException());
-        when(mockUrlConnection.getErrorStream()).thenReturn(new ByteArrayInputStream(httpErrorMessage.getBytes()));
+        new NonStrictExpectations() {{
+            mockUrlConnection.getOutputStream();
+            result = new IOException();
+            mockUrlConnection.getErrorStream();
+            result = new ByteArrayInputStream(httpErrorMessage.getBytes());
+        }};
 
-        httpConnection.send(new EventBuilder().build());
-
-        verify(handler).publish(logRecordCaptor.capture());
-        Matcher<Iterable<? super LogRecord>> matcher = hasItem(
-                new CustomTypeSafeMatcher<LogRecord>("Looks for message '" + httpErrorMessage + "'") {
-                    @Override
-                    protected boolean matchesSafely(LogRecord logRecord) {
-                        return httpErrorMessage.equals(logRecord.getMessage());
-                    }
-                });
-        assertThat(logRecordCaptor.getAllValues(), matcher);
-
-        Logger.getLogger(HttpConnection.class.getCanonicalName()).removeHandler(handler);
+        httpConnection.doSend(mockEvent);
     }
 
     @Test
-    public void testApiUrlCreation() throws Exception {
-        Dsn dsn = mock(Dsn.class);
-        String projectId = UUID.randomUUID().toString();
-        String uri = "http://host/sentry/";
-        when(dsn.getUri()).thenReturn(new URI(uri));
-        when(dsn.getProjectId()).thenReturn(projectId);
+    public void testApiUrlCreation(@Injectable final URI sentryUri) throws Exception {
+        final String uri = "http://host/sentry/";
+        final String projectId = UUID.randomUUID().toString();
+        new Expectations() {{
+            sentryUri.toString();
+            result = "http://host/sentry/";
+        }};
 
-        URL sentryApiUrl = HttpConnection.getSentryApiUrl(dsn.getUri(), dsn.getProjectId());
+        URL sentryApiUrl = HttpConnection.getSentryApiUrl(sentryUri, projectId);
 
         assertThat(sentryApiUrl.toString(), is(uri + "api/" + projectId + "/store/"));
     }
