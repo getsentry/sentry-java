14f8ae57abc0b4c252a2503bf795ef44df31dbf8
Colin Hebert <hebert.colin@gmail.com>
2013-11-10 12:12:58 +1100

    Ensure that timestamp is cloned when obtained from event

--- raven/src/test/java/net/kencochrane/raven/event/EventTest.java
+++ raven/src/test/java/net/kencochrane/raven/event/EventTest.java
@@ -1,10 +1,34 @@
 package net.kencochrane.raven.event;
 
+import mockit.Injectable;
+import mockit.NonStrictExpectations;
 import org.testng.annotations.Test;
+
+import java.util.Date;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.sameInstance;
 
 public class EventTest {
     @Test(expectedExceptions = IllegalArgumentException.class)
     public void ensureEventIdCantBeNull() throws Exception {
         final Event event = new Event(null);
+    }
+
+    @Test
+    public void returnsCloneOfTimestamp(@Injectable final Date mockTimestamp,
+                                        @Injectable final Date mockCloneTimestamp)
+            throws Exception {
+        new NonStrictExpectations() {{
+            mockTimestamp.clone();
+            result = mockCloneTimestamp;
+        }};
+        final Event event = new Event(UUID.randomUUID());
+
+        event.setTimestamp(mockTimestamp);
+
+        assertThat(event.getTimestamp(), is(sameInstance(mockCloneTimestamp)));
     }
 }
