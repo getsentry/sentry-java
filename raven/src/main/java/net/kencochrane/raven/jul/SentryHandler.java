1fd8edac5a58e89c61e27c511855b1cb0a763e44
Colin Hebert <hebert.colin@gmail.com>
2013-11-24 23:20:31 +1100

    Add tags from the JUL configuration to Events

--- raven/src/main/java/net/kencochrane/raven/jul/SentryHandler.java
+++ raven/src/main/java/net/kencochrane/raven/jul/SentryHandler.java
@@ -1,5 +1,6 @@
 package net.kencochrane.raven.jul;
 
+import com.google.common.base.Splitter;
 import net.kencochrane.raven.Raven;
 import net.kencochrane.raven.RavenFactory;
 import net.kencochrane.raven.dsn.Dsn;
@@ -11,9 +12,7 @@ import net.kencochrane.raven.event.interfaces.MessageInterface;
 
 import java.io.IOException;
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.logging.*;
 
 /**
@@ -44,6 +43,10 @@ public class SentryHandler extends Handler {
      * </p>
      */
     protected String ravenFactory;
+    /**
+     * Tags to add to every event.
+     */
+    protected Map<String, String> tags = Collections.emptyMap();
     private final boolean propagateClose;
 
     /**
@@ -116,6 +119,9 @@ public class SentryHandler extends Handler {
         LogManager manager = LogManager.getLogManager();
         dsn = manager.getProperty(SentryHandler.class.getName() + ".dsn");
         ravenFactory = manager.getProperty(SentryHandler.class.getName() + ".ravenFactory");
+        String tagsProperty = manager.getProperty(SentryHandler.class.getName() + ".tags");
+        if (tagsProperty != null)
+            tags = Splitter.on(",").withKeyValueSeparator(":").split(tagsProperty);
     }
 
     @Override
@@ -186,6 +192,10 @@ public class SentryHandler extends Handler {
             eventBuilder.setCulprit(fakeFrame);
         } else {
             eventBuilder.setCulprit(record.getLoggerName());
+        }
+
+        for (Map.Entry<String, String> tagEntry : tags.entrySet()) {
+            eventBuilder.addTag(tagEntry.getKey(), tagEntry.getValue());
         }
 
         eventBuilder.addExtra(THREAD_ID, record.getThreadID());
