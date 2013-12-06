5167b630acabc83227bd54fc8ef937aa6c230737
Colin Hebert <hebert.colin@gmail.com>
2013-12-07 09:34:46 +1100

    Ensure that DefaultRavenFactory is provided by ServiceLoader

---
+++ raven/src/test/java/net/kencochrane/raven/DefaultRavenFactoryTest.java
@@ -0,0 +1,18 @@
+package net.kencochrane.raven;
+
+import org.testng.annotations.Test;
+
+import java.util.ServiceLoader;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.instanceOf;
+
+public class DefaultRavenFactoryTest {
+    @Test
+    public void checkServiceLoaderProvidesFactory(){
+        ServiceLoader<RavenFactory> ravenFactories = ServiceLoader.load(RavenFactory.class);
+
+        assertThat(ravenFactories, contains(instanceOf(DefaultRavenFactory.class)));
+    }
+}
