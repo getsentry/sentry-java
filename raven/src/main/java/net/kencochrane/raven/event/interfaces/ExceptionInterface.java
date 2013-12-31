ca73b1f2182315e9b286b2e6fec8db4af447567f
Sebastian Gröbler <s.groebler@googlemail.com>
2013-12-31 17:31:06 +0100

    Remove the ImmutableThrowable usage from ExceptionInterface

--- raven/src/main/java/net/kencochrane/raven/event/interfaces/ExceptionInterface.java
+++ raven/src/main/java/net/kencochrane/raven/event/interfaces/ExceptionInterface.java
@@ -1,6 +1,12 @@
 package net.kencochrane.raven.event.interfaces;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * The Exception interface for Sentry allowing to add an Exception details to an event.
@@ -10,15 +16,27 @@ public class ExceptionInterface implements SentryInterface {
      * Name of the exception interface in Sentry.
      */
     public static final String EXCEPTION_INTERFACE = "sentry.interfaces.Exception";
-    private final ImmutableThrowable throwable;
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionInterface.class);
+
+    private final Deque<ExceptionWithStackTrace> exceptions;
 
     /**
-     * Creates a an Exception element for an {@link net.kencochrane.raven.event.Event}.
+     * Creates a new instance from the given {@code throwable}.
      *
-     * @param throwable Exception from the JVM to send to Sentry.
+     * @param throwable the {@link Throwable} to build this instance from
      */
-    public ExceptionInterface(Throwable throwable) {
-        this.throwable = new ImmutableThrowable(throwable);
+    public ExceptionInterface(final Throwable throwable) {
+        this(extractExceptionQueue(throwable));
+    }
+
+    /**
+     * Creates a new instance from the given {@code exceptions}.
+     *
+     * @param exceptions a {@link Deque} of {@link ExceptionWithStackTrace} to build this instance from
+     */
+    public ExceptionInterface(final Deque<ExceptionWithStackTrace> exceptions) {
+        this.exceptions = exceptions;
     }
 
     @Override
@@ -26,11 +44,47 @@ public class ExceptionInterface implements SentryInterface {
         return EXCEPTION_INTERFACE;
     }
 
-    public ImmutableThrowable getThrowable() {
-        return throwable;
+    public Deque<ExceptionWithStackTrace> getExceptions()  {
+        return exceptions;
     }
 
-    public Deque<ExceptionWithStackTrace> getExceptions()  {
-        return null; // TODO replace this
+    private static Deque<ExceptionWithStackTrace> extractExceptionQueue(Throwable throwable) {
+        Deque<ExceptionWithStackTrace> exceptions = new ArrayDeque<ExceptionWithStackTrace>();
+        Set<Throwable> circularityDetector = new HashSet<Throwable>();
+        StackTraceElement[] enclosingStackTrace = new StackTraceElement[0];
+
+        //Stack the exceptions to send them in the reverse order
+        while (throwable != null) {
+            if (!circularityDetector.add(throwable)) {
+                logger.warn("Exiting a circular exception!");
+                break;
+            }
+
+            StackTraceInterface stackTrace = new StackTraceInterface(throwable.getStackTrace(), enclosingStackTrace);
+            exceptions.push(createExceptionWithStackTraceFrom(throwable, stackTrace));
+            enclosingStackTrace = throwable.getStackTrace();
+            throwable = throwable.getCause();
+        }
+
+        return exceptions;
+    }
+
+    private static ExceptionWithStackTrace createExceptionWithStackTraceFrom(final Throwable throwable,
+                                                                      final StackTraceInterface stackTrace) {
+        final String exceptionMessage = throwable.getMessage();
+        final String exceptionClassName = throwable.getClass().getSimpleName();
+        final String exceptionPackageName = extractPackageName(throwable);
+        return new ExceptionWithStackTrace(exceptionMessage, exceptionClassName, exceptionPackageName, stackTrace);
+    }
+
+    private static String extractPackageName(final Throwable throwable) {
+
+        final Package exceptionPackage = throwable.getClass().getPackage();
+
+        if (exceptionPackage != null) {
+            return exceptionPackage.getName();
+        }
+
+        return null;
     }
 }
