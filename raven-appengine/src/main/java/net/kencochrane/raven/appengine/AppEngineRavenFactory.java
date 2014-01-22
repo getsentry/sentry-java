e530ae075e685c27cdc7bf6e6a0258ddc06780fb
Colin Hebert <hebert.colin@gmail.com>
2014-01-23 06:01:39 +1100

    Use the connectionId provided by the user in the AppEngineRavenFactory

--- raven-appengine/src/main/java/net/kencochrane/raven/appengine/AppEngineRavenFactory.java
+++ raven-appengine/src/main/java/net/kencochrane/raven/appengine/AppEngineRavenFactory.java
@@ -13,6 +13,19 @@ public class AppEngineRavenFactory extends DefaultRavenFactory {
      * Option for the queue name used in Google App Engine of threads assigned for the connection.
      */
     public static final String QUEUE_NAME = "raven.async.gaequeuename";
+    /**
+     * Option to define the identifier of the async connection across every instance of the application.
+     * <p>
+     * It is important to set a different connection identifier for each opened connection to keep the uniqueness
+     * of connection ID.
+     * </p>
+     * <p>
+     * If the connection identifier is not specified, the system will define a connection identifier itself, but its
+     * uniqueness within an instance isn't guaranteed.
+     * </p>
+     * {@see AppEngineAsyncConnection}
+     */
+    public static final String CONNECTION_IDENTIFIER = "raven.async.gae.connectionid";
 
     /**
      * Encapsulates an already existing connection in an {@link AppEngineAsyncConnection} and get the async options
@@ -24,7 +37,14 @@ public class AppEngineRavenFactory extends DefaultRavenFactory {
      */
     @Override
     protected Connection createAsyncConnection(Dsn dsn, Connection connection) {
-        AppEngineAsyncConnection asyncConnection = new AppEngineAsyncConnection(connection);
+        String connectionIdentifier;
+        if (dsn.getOptions().containsKey(QUEUE_NAME)) {
+            connectionIdentifier = dsn.getOptions().get(CONNECTION_IDENTIFIER);
+        } else {
+            connectionIdentifier = AppEngineRavenFactory.class.getCanonicalName() + dsn;
+        }
+
+        AppEngineAsyncConnection asyncConnection = new AppEngineAsyncConnection(connectionIdentifier, connection);
 
         if (dsn.getOptions().containsKey(QUEUE_NAME)) {
             asyncConnection.setQueue(dsn.getOptions().get(QUEUE_NAME));
