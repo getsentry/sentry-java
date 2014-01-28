d9ab08a2e12ba5fdcc48c64267f1186c854794a2
Colin Hebert <hebert.colin@gmail.com>
2014-01-29 08:16:38 +1100

    Fix documentation

--- raven-appengine/src/main/java/net/kencochrane/raven/appengine/event/helper/AppEngineEventBuilderHelper.java
+++ raven-appengine/src/main/java/net/kencochrane/raven/appengine/event/helper/AppEngineEventBuilderHelper.java
@@ -9,13 +9,16 @@ import net.kencochrane.raven.event.helper.EventBuilderHelper;
  */
 public class AppEngineEventBuilderHelper implements EventBuilderHelper {
     /**
-     * Property used internally by GAE to define
+     * Property used internally by GAE to define the hostname.
+     *
+     * @see <a href="https://developers.google.com/appengine/docs/java/appidentity/">GAE: App Identity Java API</a>
      */
     private static final String CURRENT_VERSION_HOSTNAME_PROPERTY = "com.google.appengine.runtime.default_version_hostname";
 
     @Override
     public void helpBuildingEvent(EventBuilder eventBuilder) {
         ApiProxy.Environment env = ApiProxy.getCurrentEnvironment();
+        // Set the hostname to the actual application hostname
         eventBuilder.setServerName((String) env.getAttributes().get(CURRENT_VERSION_HOSTNAME_PROPERTY));
     }
 }
