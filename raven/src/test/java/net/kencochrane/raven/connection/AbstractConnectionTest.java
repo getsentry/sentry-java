67adca7c746f522ceec17e2cf4ac8f75c61be94e
Colin Hebert <hebert.colin@gmail.com>
2014-03-02 10:42:25 +1100

    Use Tested rather than creating dummy implementations

--- raven/src/test/java/net/kencochrane/raven/connection/AbstractConnectionTest.java
+++ raven/src/test/java/net/kencochrane/raven/connection/AbstractConnectionTest.java
@@ -1,8 +1,9 @@
 package net.kencochrane.raven.connection;
 
 import mockit.Injectable;
-import mockit.Mock;
-import mockit.MockUp;
+import mockit.NonStrictExpectations;
+import mockit.Tested;
+import mockit.Verifications;
 import net.kencochrane.raven.Raven;
 import net.kencochrane.raven.event.Event;
 import org.mockito.MockitoAnnotations;
@@ -10,7 +11,6 @@ import org.mockito.Spy;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
 import java.util.concurrent.locks.ReentrantLock;
 
 import static mockit.Deencapsulation.setField;
@@ -19,8 +19,11 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.verify;
 
 public class AbstractConnectionTest {
+    @Injectable
     private final String publicKey = "9bcf4a8c-f353-4f25-9dda-76a873fff905";
+    @Injectable
     private final String secretKey = "56a9d05e-9032-4fdd-8f67-867d526422f9";
+    @Tested
     private AbstractConnection abstractConnection;
     //Spying with mockito as jMockit doesn't support mocks of ReentrantLock
     @Spy
@@ -30,8 +33,6 @@ public class AbstractConnectionTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         setField(Raven.class, "NAME", "Raven-Java/Test");
-        abstractConnection = new DummyAbstractConnection(publicKey, secretKey);
-        setField(abstractConnection, "lock", reentrantLock);
     }
 
     @Test
@@ -46,43 +47,26 @@ public class AbstractConnectionTest {
 
     @Test
     public void testSuccessfulSendCallsDoSend(@Injectable final Event mockEvent) throws Exception {
-        new MockUp<DummyAbstractConnection>() {
-            @SuppressWarnings("unused")
-            @Mock(invocations = 1)
-            protected void doSend(Event event) throws ConnectionException {
-            }
-        };
+        setField(abstractConnection, "lock", reentrantLock);
 
         abstractConnection.send(mockEvent);
+
+        new Verifications() {{
+            abstractConnection.doSend(mockEvent);
+        }};
     }
 
     @Test
     public void testExceptionOnSendStartLockDown(@Injectable final Event mockEvent) throws Exception {
-        new MockUp<DummyAbstractConnection>() {
-            @SuppressWarnings("unused")
-            @Mock
-            protected void doSend(Event event) throws ConnectionException {
-                throw new ConnectionException();
-            }
-        };
+        setField(abstractConnection, "lock", reentrantLock);
+        new NonStrictExpectations() {{
+            abstractConnection.doSend((Event) any);
+            result = new ConnectionException();
+        }};
 
         abstractConnection.send(mockEvent);
 
         verify(reentrantLock).tryLock();
         verify(reentrantLock).unlock();
-    }
-
-    private static final class DummyAbstractConnection extends AbstractConnection {
-        public DummyAbstractConnection(String publicKey, String secretKey) {
-            super(publicKey, secretKey);
-        }
-
-        @Override
-        protected void doSend(Event event) throws ConnectionException {
-        }
-
-        @Override
-        public void close() throws IOException {
-        }
     }
 }
