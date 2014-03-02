2bda90e2335d0cb6a6ce082e2e469ef024d987bf
Colin Hebert <hebert.colin@gmail.com>
2014-03-02 18:38:23 +1100

    Ensure that the tested element is reset after each run

--- raven/src/test/java/net/kencochrane/raven/connection/AbstractConnectionTest.java
+++ raven/src/test/java/net/kencochrane/raven/connection/AbstractConnectionTest.java
@@ -33,6 +33,8 @@ public class AbstractConnectionTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         setField(Raven.class, "NAME", "Raven-Java/Test");
+        // Reset tested
+        abstractConnection = null;
     }
 
     @Test
