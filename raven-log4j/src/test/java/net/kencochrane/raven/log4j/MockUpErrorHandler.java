9f631ab244d7f8d18e8835d782c8a6f9add99d57
Colin Hebert <hebert.colin@gmail.com>
2014-03-15 11:33:04 +1100

    Log ErrorHandler messages with a logger
    Also disable the logs during the unit tests

--- raven-log4j/src/test/java/net/kencochrane/raven/log4j/MockUpErrorHandler.java
+++ raven-log4j/src/test/java/net/kencochrane/raven/log4j/MockUpErrorHandler.java
@@ -4,15 +4,17 @@ import mockit.Mock;
 import mockit.MockUp;
 import org.apache.log4j.spi.ErrorHandler;
 import org.apache.log4j.spi.LoggingEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MockUpErrorHandler extends MockUp<ErrorHandler> {
+    private static final Logger logger = LoggerFactory.getLogger("ErrorHandler");
     private int errorCount = 0;
 
     @Mock
     public void error(String message) {
         errorCount++;
-        System.err.println("[RAVEN] ErrorHandler - " + message);
-        System.err.flush();
+        logger.error(message);
     }
 
     @Mock
