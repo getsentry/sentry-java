5cc2ac492b7d7b78a4689df24d873d59ca4af9d0
Colin Hebert <hebert.colin@gmail.com>
2014-06-28 14:11:12 +1000

    Improve tests
     - Do not mock ServiceLoader entirely, just an injectable.
     - Inject the custom ServiceLoader in the RavenFactory class directly
     - Create a new Iterator every time the Iterator() method is called
     - Unregister manually registered factories
     - Do not register the factories in the expectations

--- raven/src/test/java/net/kencochrane/raven/RavenFactoryTest.java
+++ raven/src/test/java/net/kencochrane/raven/RavenFactoryTest.java
@@ -1,34 +1,44 @@
 package net.kencochrane.raven;
 
 import com.google.common.collect.Iterators;
+import mockit.Delegate;
 import mockit.Injectable;
-import mockit.Mocked;
 import mockit.NonStrictExpectations;
 import mockit.Tested;
 import net.kencochrane.raven.dsn.Dsn;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.ServiceLoader;
 
+import static mockit.Deencapsulation.setField;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RavenFactoryTest {
     @Tested
     private RavenFactory ravenFactory;
-    @Mocked
+    @Injectable
     private ServiceLoader mockServiceLoader;
 
     @BeforeMethod
     public void setUp() throws Exception {
+        setField(RavenFactory.class, "AUTO_REGISTERED_FACTORIES", mockServiceLoader);
+
         new NonStrictExpectations() {{
-            ServiceLoader.load(RavenFactory.class);
-            result = mockServiceLoader;
             mockServiceLoader.iterator();
             result = Iterators.emptyIterator();
-
         }};
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        // Reset the registered factories
+        setField(RavenFactory.class, "MANUALLY_REGISTERED_FACTORIES", new HashSet<>());
+        setField(RavenFactory.class, "AUTO_REGISTERED_FACTORIES", ServiceLoader.load(RavenFactory.class));
     }
 
     @Test
@@ -36,7 +46,11 @@ public class RavenFactoryTest {
                                                   @Injectable final Dsn mockDsn) throws Exception {
         new NonStrictExpectations() {{
             mockServiceLoader.iterator();
-            result = Iterators.singletonIterator(ravenFactory);
+            result = new Delegate<Iterator<RavenFactory>>() {
+                public Iterator<RavenFactory> iterator() {
+                    return Iterators.singletonIterator(ravenFactory);
+                }
+            };
             ravenFactory.createRavenInstance(mockDsn);
             result = mockRaven;
         }};
@@ -49,8 +63,8 @@ public class RavenFactoryTest {
     @Test
     public void testGetFactoriesManuallyAdded(@Injectable final Raven mockRaven,
                                               @Injectable final Dsn mockDsn) throws Exception {
+        RavenFactory.registerFactory(ravenFactory);
         new NonStrictExpectations() {{
-            RavenFactory.registerFactory(ravenFactory);
             ravenFactory.createRavenInstance(mockDsn);
             result = mockRaven;
         }};
@@ -62,10 +76,10 @@ public class RavenFactoryTest {
 
     @Test
     public void testRavenInstanceForFactoryNameSucceedsIfFactoryFound(@Injectable final Raven mockRaven,
-                                                                     @Injectable final Dsn mockDsn) throws Exception {
+                                                                      @Injectable final Dsn mockDsn) throws Exception {
         String factoryName = ravenFactory.getClass().getName();
+        RavenFactory.registerFactory(ravenFactory);
         new NonStrictExpectations() {{
-            RavenFactory.registerFactory(ravenFactory);
             ravenFactory.createRavenInstance(mockDsn);
             result = mockRaven;
         }};
@@ -77,10 +91,10 @@ public class RavenFactoryTest {
 
     @Test(expectedExceptions = IllegalStateException.class)
     public void testRavenInstanceForFactoryNameFailsIfNoFactoryFound(@Injectable final Raven mockRaven,
-                                            @Injectable final Dsn mockDsn) throws Exception {
+                                                                     @Injectable final Dsn mockDsn) throws Exception {
         String factoryName = "invalidName";
+        RavenFactory.registerFactory(ravenFactory);
         new NonStrictExpectations() {{
-            RavenFactory.registerFactory(ravenFactory);
             ravenFactory.createRavenInstance(mockDsn);
             result = mockRaven;
         }};
