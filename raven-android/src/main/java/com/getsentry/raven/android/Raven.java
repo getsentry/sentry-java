8bbcb8cb52c911aeb01d37f9f3da89f8a5b8c0da
Brett Hoerner <brett@bretthoerner.com>
2016-07-29 14:03:51 -0500

    Add docs.

--- raven-android/src/main/java/com/getsentry/raven/android/Raven.java
+++ raven-android/src/main/java/com/getsentry/raven/android/Raven.java
@@ -11,65 +11,122 @@ import android.util.Log;
 import com.getsentry.raven.DefaultRavenFactory;
 import com.getsentry.raven.dsn.Dsn;
 
-public class Raven {
+/**
+ * Android specific class to interface with Raven. Supplements the default Java classes
+ * with Android specific state and features.
+ */
+public final class Raven {
 
+    /**
+     * Logger tag.
+     */
     public static final String TAG = Raven.class.getName();
 
+    /**
+     * Android application context.
+     */
     private static volatile Context context;
 
-    public static void init(Context context) {
-        init(context, null);
+    /**
+     * Hide constructor.
+     */
+    private Raven() {
+
     }
 
-    public static void init(Context context, String dsnStr) {
-        context = context.getApplicationContext();
+    /**
+     * Initialize Raven using a DSN set in the AndroidManifest.
+     *
+     * @param ctx Android application ctx
+     */
+    public static void init(Context ctx) {
+        ctx = ctx.getApplicationContext();
+        String dsn = "";
 
-        if (!checkPermission(context, Manifest.permission.INTERNET)) {
+        // attempt to get DSN from AndroidManifest
+        ApplicationInfo appInfo = null;
+        try {
+            PackageManager packageManager = ctx.getPackageManager();
+            appInfo = packageManager.getApplicationInfo(ctx.getPackageName(), PackageManager.GET_META_DATA);
+            dsn = appInfo.metaData.getString("com.getsentry.raven.android.DSN");
+        } catch (PackageManager.NameNotFoundException e) {
+            // skip
+        }
+
+        if (TextUtils.isEmpty(dsn)) {
+            throw new NullPointerException("Raven DSN is not set, you must provide it via"
+                + "the constructor or AndroidManifest.");
+        }
+
+        init(ctx, new Dsn(dsn));
+    }
+
+    /**
+     * Initialize Raven using a string DSN.
+     *
+     * @param ctx Android application ctx
+     * @param dsn Sentry DSN string
+     */
+    public static void init(Context ctx, String dsn) {
+        init(ctx, new Dsn(dsn));
+    }
+
+    /**
+     * Initialize Raven using a DSN object. This is the 'main' initializer that other methods
+     * eventually call.
+     *
+     * @param ctx Android application ctx
+     * @param dsn Sentry DSN object
+     */
+    public static void init(Context ctx, Dsn dsn) {
+        context = ctx.getApplicationContext();
+
+        if (!checkPermission(ctx, Manifest.permission.INTERNET)) {
             Log.e(TAG, Manifest.permission.INTERNET + " is required to connect to the Sentry server,"
                 + " please add it to your AndroidManifest.xml");
         }
 
-        if (TextUtils.isEmpty(dsnStr)) {
-            // attempt to get DSN from AndroidManifest
-            ApplicationInfo appInfo = null;
-            try {
-                PackageManager packageManager = context.getPackageManager();
-                appInfo = packageManager.getApplicationInfo(context.getPackageName(), PackageManager.GET_META_DATA);
-                dsnStr = appInfo.metaData.getString("com.getsentry.raven.android.DSN");
-            } catch (PackageManager.NameNotFoundException e) {
-                // skip
-            }
-        }
-
-        if (TextUtils.isEmpty(dsnStr)) {
-            throw new NullPointerException("Raven DSN is not set, you must provide it via constructor or AndroidManifest.");
-        }
-
-        Log.d(TAG, "raven init with context='" + context.toString() + "' and dsn='" + dsnStr + "'");
-
-        Dsn dsn = new Dsn(dsnStr);
+        Log.d(TAG, "raven init with ctx='" + ctx.toString() + "' and dsn='" + dsn + "'");
         if ("false".equalsIgnoreCase(dsn.getOptions().get(DefaultRavenFactory.ASYNC_OPTION))) {
             throw new IllegalArgumentException("Raven Android cannot use synchronous connections, remove '"
                 + DefaultRavenFactory.ASYNC_OPTION + "=false' from your DSN.");
         }
 
         // actual instance is stored statically on Raven
-        RavenFactory.ravenInstance(dsn);
+        DefaultRavenFactory.ravenInstance(dsn);
         setupUncaughtExceptionHandler();
-        // TODO: sendAllCachedCapturedEvents();
     }
 
-    private static boolean checkPermission(Context context, String permission) {
-        int res = context.checkCallingOrSelfPermission(permission);
+    /**
+     * Check whether the application has been granted a certain permission.
+     *
+     * @param ctx Android application ctx
+     * @param permission Permission as a string
+     * @return true if permissions is granted
+     */
+    private static boolean checkPermission(Context ctx, String permission) {
+        int res = ctx.checkCallingOrSelfPermission(permission);
         return (res == PackageManager.PERMISSION_GRANTED);
     }
 
-    private static boolean isConnected(Context context) {
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    /**
+     * Check whether the application has internet access at a point in time.
+     *
+     * @param ctx Android appliation ctx
+     * @return true if the application has internet access
+     */
+    private static boolean isConnected(Context ctx) {
+        ConnectivityManager connectivityManager =
+            (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
+    /**
+     * Check whether Raven should attempt to send an event, or just immediately store it.
+     *
+     * @return true if Raven should attempt to send an event
+     */
     private static boolean shouldAttemptToSend() {
         if (!checkPermission(context, android.Manifest.permission.ACCESS_NETWORK_STATE)) {
             // we can't check whether the connection is up, so the
@@ -80,6 +137,10 @@ public class Raven {
         return isConnected(context);
     }
 
+    /**
+     * Configures an Android uncaught exception handler which sends events to
+     * Sentry, then calls the preexisting uncaught exception handler.
+     */
     private static void setupUncaughtExceptionHandler() {
         Thread.UncaughtExceptionHandler currentHandler = Thread.getDefaultUncaughtExceptionHandler();
         if (currentHandler != null) {
--- raven-android/src/main/java/com/getsentry/raven/android/RavenUncaughtExceptionHandler.java
+++ raven-android/src/main/java/com/getsentry/raven/android/RavenUncaughtExceptionHandler.java
@@ -2,22 +2,45 @@ package com.getsentry.raven.android;
 
 import android.util.Log;
 
+/**
+ * Sends any uncaught exception to Sentry, then passes the exception on to the pre-existing
+ * uncaught exception handler.
+ */
 class RavenUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
 
+    /**
+     * Logger tag.
+     */
     private static final String TAG = RavenUncaughtExceptionHandler.class.getName();
 
+    /**
+     * Reference to the pre-existing uncaught exception handler.
+     */
     private Thread.UncaughtExceptionHandler defaultExceptionHandler;
 
-    public RavenUncaughtExceptionHandler(Thread.UncaughtExceptionHandler defaultExceptionHandler) {
+    /**
+     * Construct the {@link RavenUncaughtExceptionHandler}, storing the pre-existing uncaught exception
+     * handler.
+     *
+     * @param defaultExceptionHandler pre-existing uncaught exception handler
+     */
+    RavenUncaughtExceptionHandler(Thread.UncaughtExceptionHandler defaultExceptionHandler) {
         this.defaultExceptionHandler = defaultExceptionHandler;
     }
 
+    /**
+     * Sends any uncaught exception to Sentry, then passes the exception on to the pre-existing
+     * uncaught exception handler.
+     *
+     * @param thread thread that threw the error
+     * @param thrown the uncaught throwable
+     */
     @Override
     public void uncaughtException(Thread thread, Throwable thrown) {
         Log.d(TAG, "uncaught exception received");
 
         try {
-            // TODO: Raven.capture
+            com.getsentry.raven.Raven.capture(thrown);
         } catch (Exception e) {
             Log.e(TAG, "error sending excepting to Sentry", e);
         }
