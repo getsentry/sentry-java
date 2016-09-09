ceb4b0298c3f3883ee80c750ff3fda4928cd8235
Brett Hoerner <brett@bretthoerner.com>
2016-09-09 16:35:51 -0500

    Fix full build, cleanup temp cache dir.

--- raven-android/src/test/java/com/getsentry/raven/android/EventCacheIT.java
+++ raven-android/src/test/java/com/getsentry/raven/android/EventCacheIT.java
@@ -2,20 +2,27 @@ package com.getsentry.raven.android;
 
 import com.getsentry.raven.event.Event;
 import com.getsentry.raven.event.EventBuilder;
-import com.getsentry.raven.stub.SentryStub;
+import org.apache.maven.artifact.ant.shaded.FileUtils;
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.Callable;
 
 @RunWith(RobolectricTestRunner.class)
 public class EventCacheIT extends AndroidTest {
+
+    private static final File CACHE_DIR = new File("./test-cache-directory");
+
+    @After
+    public void teardown() throws IOException {
+        FileUtils.deleteDirectory(CACHE_DIR);
+    }
 
     @Test
     public void test() throws Exception {
@@ -23,7 +30,7 @@ public class EventCacheIT extends AndroidTest {
 
         EventCacheITActivity activity = Robolectric.setupActivity(EventCacheITActivity.class);
         Event event = new EventBuilder().withMessage("message").build();
-        activity.setupEventCache(new File("./cache"), 2);
+        activity.setupEventCache(CACHE_DIR, 2);
         activity.storeEvent(event);
         activity.flushEvents();
 
--- raven-android/src/test/java/com/getsentry/raven/android/RavenIT.java
+++ raven-android/src/test/java/com/getsentry/raven/android/RavenIT.java
@@ -1,9 +1,6 @@
 package com.getsentry.raven.android;
 
-import com.getsentry.raven.stub.SentryStub;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
@@ -13,7 +10,7 @@ import java.util.concurrent.Callable;
 
 @RunWith(RobolectricTestRunner.class)
 public class RavenIT extends AndroidTest {
-    
+
     @Test
     public void test() throws Exception {
         Assert.assertEquals(sentryStub.getEventCount(), 0);
