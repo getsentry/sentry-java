ae4dd804557a4c5a1b31750bd651177b8f9653c2
Brett Hoerner <brett@bretthoerner.com>
2016-10-03 18:21:37 -0500

    Store last event sent in RavenContext. (#255)

--- raven/src/main/java/com/getsentry/raven/servlet/RavenServletRequestListener.java
+++ raven/src/main/java/com/getsentry/raven/servlet/RavenServletRequestListener.java
@@ -1,5 +1,9 @@
 package com.getsentry.raven.servlet;
 
+import com.getsentry.raven.Raven;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
@@ -11,6 +15,8 @@ import javax.servlet.http.HttpServletRequest;
  * in the event sent to Sentry.
  */
 public class RavenServletRequestListener implements ServletRequestListener {
+    private static final Logger logger = LoggerFactory.getLogger(RavenServletRequestListener.class);
+
     private static final ThreadLocal<HttpServletRequest> THREAD_REQUEST = new ThreadLocal<>();
 
     public static HttpServletRequest getServletRequest() {
@@ -20,12 +26,19 @@ public class RavenServletRequestListener implements ServletRequestListener {
     @Override
     public void requestDestroyed(ServletRequestEvent servletRequestEvent) {
         THREAD_REQUEST.remove();
+
+        try {
+            Raven.getStoredInstance().getContext().clear();
+        } catch (Exception e) {
+            logger.error("Error clearing RavenContext state.", e);
+        }
     }
 
     @Override
     public void requestInitialized(ServletRequestEvent servletRequestEvent) {
         ServletRequest servletRequest = servletRequestEvent.getServletRequest();
-        if (servletRequest instanceof HttpServletRequest)
+        if (servletRequest instanceof HttpServletRequest) {
             THREAD_REQUEST.set((HttpServletRequest) servletRequest);
+        }
     }
 }
