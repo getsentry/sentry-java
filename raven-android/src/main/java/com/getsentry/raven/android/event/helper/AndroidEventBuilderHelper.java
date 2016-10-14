feed36b692dd1dd57e8c3072044639da245fe7d5
Brett Hoerner <brett@bretthoerner.com>
2016-10-14 13:17:59 -0500

    Add raven-android submodule. (#261)

---
+++ raven-android/src/main/java/com/getsentry/raven/android/event/helper/AndroidEventBuilderHelper.java
@@ -0,0 +1,40 @@
+package com.getsentry.raven.android.event.helper;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import com.getsentry.raven.event.EventBuilder;
+import com.getsentry.raven.event.helper.EventBuilderHelper;
+
+/**
+ * EventBuilderHelper that makes use of Android Context to populate some Event fields.
+ */
+public class AndroidEventBuilderHelper implements EventBuilderHelper {
+
+    /**
+     * Logger tag.
+     */
+    public static final String TAG = AndroidEventBuilderHelper.class.getName();
+
+    private Context ctx;
+
+    /**
+     * Construct given the provided Android {@link Context}.
+     *
+     * @param ctx Android application context.
+     */
+    public AndroidEventBuilderHelper(Context ctx) {
+        this.ctx = ctx;
+    }
+
+    @Override
+    public void helpBuildingEvent(EventBuilder eventBuilder) {
+        try {
+            int versionCode = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), 0).versionCode;
+            eventBuilder.withRelease(Integer.toString(versionCode));
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(TAG, "Couldn't find package version: " + e);
+        }
+    }
+
+}
