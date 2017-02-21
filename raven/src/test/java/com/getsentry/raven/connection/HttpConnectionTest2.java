101d886534df62b8064e35f517787cbced70c520
Brett Hoerner <brett@bretthoerner.com>
2017-02-21 16:01:49 -0600

    Simplify test.

--- raven/src/test/java/com/getsentry/raven/connection/HttpConnectionTest2.java
+++ raven/src/test/java/com/getsentry/raven/connection/HttpConnectionTest2.java
@@ -1,64 +1,48 @@
 package com.getsentry.raven.connection;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.junit.Assert.fail;
 
+import com.getsentry.raven.BaseTest2;
+import com.getsentry.raven.event.Event;
 import com.getsentry.raven.event.EventBuilder;
 import com.getsentry.raven.marshaller.json.JsonMarshaller;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import org.junit.Rule;
 import org.junit.Test;
 
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 
-public class HttpConnectionTest2 {
+public class HttpConnectionTest2 extends BaseTest2 {
 
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().dynamicPort());
-
-    private String getDsn(String projectId) {
-        return "http://public:private@localhost:" + wireMockRule.port() + "/" + projectId;
-    }
-
-    private URI getSentryUri() throws URISyntaxException {
-        return new URI("http://localhost:" + wireMockRule.port() + "/");
-    }
-
-    private URL getSentryStoreUrl(String projectId) throws URISyntaxException {
-        return HttpConnection.getSentryApiUrl(getSentryUri(), "1");
+    private HttpConnection getHttpConnection() throws URISyntaxException {
+        HttpConnection httpConnection = new HttpConnection(getSentryStoreUrl(), "public", "private",
+            null, null);
+        httpConnection.setMarshaller(new JsonMarshaller());
+        return httpConnection;
     }
 
     @Test
     public void test() throws URISyntaxException {
         wireMockRule
-            .stubFor(post(urlEqualTo("/api/1/store/"))
+            .stubFor(post(urlEqualTo(STORE_URL))
             .willReturn(aResponse().withStatus(429)));
 
-        HttpConnection httpConnection = new HttpConnection(
-            getSentryStoreUrl("1"),
-            "public",
-            "private",
-            null,
-            null);
-        httpConnection.setMarshaller(new JsonMarshaller());
+        HttpConnection httpConnection = getHttpConnection();
+        Event event = new EventBuilder().build();
 
         try {
-            httpConnection.send(new EventBuilder().build());
+            httpConnection.send(event);
             fail("Expected ConnectionException to be thrown");
         } catch (ConnectionException e) {
             // expected
         }
 
         wireMockRule
-            .verify(postRequestedFor(urlMatching("/api/1/store/")));
+            .verify(postRequestedFor(urlMatching(STORE_URL)));
 
         // TODO: this shouldn't throw an error, which is good (because its in backoff)
         //       but it's bad because we're depending on the clock not ticking over 1
         //       second -- what I want/need is a way to replace the clock in tests
-        httpConnection.send(new EventBuilder().build());
+        httpConnection.send(event);
     }
 
 }
