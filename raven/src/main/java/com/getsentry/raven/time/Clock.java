e1877a4910f93260afc796a2fa2c3cf5f7d874bb
Brett Hoerner <brett@bretthoerner.com>
2017-02-27 16:53:10 -0600

    Add Clock interface for testing. (#315)

---
+++ raven/src/main/java/com/getsentry/raven/time/Clock.java
@@ -0,0 +1,24 @@
+package com.getsentry.raven.time;
+
+import java.util.Date;
+
+/**
+ * Clock interface, used to inject a Clock dependency so time can be adjusted in tests.
+ */
+interface Clock {
+
+    /**
+     * Returns the Clock's time in milliseconds.
+     *
+     * @return the Clock's time in milliseconds.
+     */
+    long millis();
+
+    /**
+     * Returns the Clock's time as a Date object.
+     *
+     * @return the Clock's time as a Date object.
+     */
+    Date date();
+
+}
