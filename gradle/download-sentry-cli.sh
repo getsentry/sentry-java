88660b70094b8ae768f9803cc72f4b61dfc140ee
Armin Ronacher <armin.ronacher@active-4.com>
2017-06-20 13:00:23 +0200

    Added download script for sentry-cli

---
+++ gradle/download-sentry-cli.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd $(dirname "$0")
+REPO=getsentry/sentry-cli
+VERSION=1.13.3
+PLATFORMS="Darwin-x86_64 Linux-i686 Linux-x86_64 Windows-i686"
+
+for plat in $PLATFORMS; do
+  suffix=''
+  if [[ $plat == *"Windows"* ]]; then
+    suffix='.exe'
+  fi
+  echo "${plat}"
+  download_url=https://github.com/$REPO/releases/download/$VERSION/sentry-cli-${plat}${suffix}
+  fn="src/main/resources/bin/sentry-cli-${plat}${suffix}"
+  curl -SL --progress-bar "$download_url" -o "$fn"
+  chmod +x "$fn"
+done
