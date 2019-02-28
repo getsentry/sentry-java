4331bfc2c0a02a689054812c324a46e954e02b58
Brett Hoerner <brett@bretthoerner.com>
2019-02-28 09:12:51 -0600

    Revert "feat: Support DexGuard (#684)"
    This reverts commit 08014a0c7c47a2d2a0c18491ad33bfdee9f911fd.

--- sentry-android-gradle-plugin/src/main/groovy/io/sentry/android/gradle/SentryPlugin.groovy
+++ sentry-android-gradle-plugin/src/main/groovy/io/sentry/android/gradle/SentryPlugin.groovy
@@ -104,14 +104,12 @@ class SentryPlugin implements Plugin<Project> {
      */
     static Task getProguardTask(Project project, ApplicationVariant variant) {
         def names = [
-                "transformClassesAndResourcesWithProguardFor${variant.name.capitalize()}",
                 // Android Studio 3.3 includes the R8 shrinker.
                 "transformClassesAndResourcesWithR8For${variant.name.capitalize()}",
-                //Dexguard's task
-                "transformDexWithDexFor${variant.name.capitalize()}",
+                "transformClassesAndResourcesWithProguardFor${variant.name.capitalize()}"
         ]
 
-        return names.findResult { project.tasks.findByName(it) } ?: project.tasks.findByName("proguard${names[0]}")
+        return names.findResult { project.tasks.findByName(it) } ?: project.tasks.findByName("proguard${names[1]}")
     }
 
     /**
@@ -124,9 +122,7 @@ class SentryPlugin implements Plugin<Project> {
     static Task getDexTask(Project project, ApplicationVariant variant) {
         def names = [
             "transformClassesWithDexFor${variant.name.capitalize()}",
-            "transformClassesWithDexBuilderFor${variant.name.capitalize()}",
-            //Pre-dex step for DexGuard
-            "transformClassesWithPreDexFor${variant.name.capitalize()}"
+            "transformClassesWithDexBuilderFor${variant.name.capitalize()}"
         ]
 
         def rv = null
@@ -137,7 +133,7 @@ class SentryPlugin implements Plugin<Project> {
             }
         }
 
-        return names.findResult { project.tasks.findByName(it) } ?: project.tasks.findByName("dex${names[0]}")
+        return project.tasks.findByName("dex${names[0]}")
     }
 
     /**
