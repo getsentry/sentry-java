312eb73c188be8eba82007057799efb2c59093ba
Lukas Krejci <lukas@krejci.pw>
2019-08-27 21:12:31 +0200

    Rewrote UserInterfaceTest.

--- sentry/src/test/java/io/sentry/event/interfaces/UserInterfaceTest.java
+++ sentry/src/test/java/io/sentry/event/interfaces/UserInterfaceTest.java
@@ -1,7 +1,7 @@
 package io.sentry.event.interfaces;
 
 import io.sentry.BaseTest;
-import org.testng.annotations.Test;
+import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
