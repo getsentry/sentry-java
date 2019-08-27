8c64a609cb9a225b3c3a3de8bb9612e926a25e5b
Lukas Krejci <lukas@krejci.pw>
2019-08-27 21:01:52 +0200

    Rewrote the MessageInterfaceTest.

--- sentry/src/test/java/io/sentry/event/interfaces/MessageInterfaceTest.java
+++ sentry/src/test/java/io/sentry/event/interfaces/MessageInterfaceTest.java
@@ -1,7 +1,7 @@
 package io.sentry.event.interfaces;
 
 import io.sentry.BaseTest;
-import org.testng.annotations.Test;
+import org.junit.Test;
 
 import java.util.Collections;
 import java.util.List;
