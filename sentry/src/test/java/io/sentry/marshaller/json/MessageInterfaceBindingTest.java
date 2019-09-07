915636f7a3397904c29d3c15597146ed40a4832a
Lukas Krejci <lukas@krejci.pw>
2019-09-07 17:52:53 +0200

    Reworked MessageInterfaceBindingTest.

--- sentry/src/test/java/io/sentry/marshaller/json/MessageInterfaceBindingTest.java
+++ sentry/src/test/java/io/sentry/marshaller/json/MessageInterfaceBindingTest.java
@@ -1,11 +1,8 @@
 package io.sentry.marshaller.json;
 
 import io.sentry.BaseTest;
-import mockit.Injectable;
-import mockit.NonStrictExpectations;
-import mockit.Tested;
 import io.sentry.event.interfaces.MessageInterface;
-import org.testng.annotations.Test;
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,25 +10,22 @@ import java.util.List;
 import static io.sentry.marshaller.json.JsonComparisonUtil.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MessageInterfaceBindingTest extends BaseTest {
-    @Tested
-    private MessageInterfaceBinding interfaceBinding = null;
-    @Injectable
-    private MessageInterface mockMessageInterface = null;
-
     @Test
     public void testSimpleMessage() throws Exception {
         final JsonGeneratorParser jsonGeneratorParser = newJsonGenerator();
         final String message = "550ee459-cbb5-438e-91d2-b0bbdefab670";
         final List<String> parameters = Arrays.asList("33ed929b-d803-46b6-a57b-9c0feab1f468",
                 "5fc10379-6392-470d-9de5-e4cb805ab78c");
-        new NonStrictExpectations() {{
-            mockMessageInterface.getMessage();
-            result = message;
-            mockMessageInterface.getParameters();
-            result = parameters;
-        }};
+
+        MessageInterface mockMessageInterface = mock(MessageInterface.class);
+        when(mockMessageInterface.getMessage()).thenReturn(message);
+        when(mockMessageInterface.getParameters()).thenReturn(parameters);
+
+        MessageInterfaceBinding interfaceBinding = new MessageInterfaceBinding();
 
         interfaceBinding.writeInterface(jsonGeneratorParser.generator(), mockMessageInterface);
 
