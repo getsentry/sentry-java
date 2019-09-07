bfdd4ead16e704e985cebe5caccd3f8351288ac8
Lukas Krejci <lukas@krejci.pw>
2019-09-07 17:18:41 +0200

    Rewrote HttpInterfaceBindingTest.

--- sentry/src/test/java/io/sentry/marshaller/json/HttpInterfaceBindingTest.java
+++ sentry/src/test/java/io/sentry/marshaller/json/HttpInterfaceBindingTest.java
@@ -1,13 +1,13 @@
 package io.sentry.marshaller.json;
 
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import io.sentry.BaseTest;
 import io.sentry.event.interfaces.HttpInterface;
-import mockit.Injectable;
-import mockit.NonStrictExpectations;
-import mockit.Tested;
-import org.testng.annotations.Test;
-import org.testng.collections.Lists;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -16,97 +16,55 @@ import java.util.Map;
 import static io.sentry.marshaller.json.JsonComparisonUtil.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class HttpInterfaceBindingTest extends BaseTest {
-    @Tested
     private HttpInterfaceBinding interfaceBinding = null;
-    @Injectable
     private HttpInterface mockMessageInterface = null;
+    private JsonGeneratorParser jsonGeneratorParser;
+
+    @Before
+    public void setup() throws Exception {
+        mockMessageInterface = mock(HttpInterface.class);
+        interfaceBinding = new HttpInterfaceBinding();
+        jsonGeneratorParser = newJsonGenerator();
+
+        final Map<String, Collection<String>> headers = new HashMap<>();
+        headers.put("Header1", singletonList("Value1"));
+        headers.put("Header2", asList("Value1", "Value2"));
+        final HashMap<String, String> cookies = new HashMap<>();
+        cookies.put("Cookie1", "Value1");
+
+        when(mockMessageInterface.getHeaders()).thenReturn(headers);
+        when(mockMessageInterface.getRequestUrl()).thenReturn("http://host/url");
+        when(mockMessageInterface.getMethod()).thenReturn("GET");
+        when(mockMessageInterface.getQueryString()).thenReturn("query");
+        when(mockMessageInterface.getCookies()).thenReturn(cookies);
+        when(mockMessageInterface.getRemoteAddr()).thenReturn("1.2.3.4");
+        when(mockMessageInterface.getServerName()).thenReturn("server-name");
+        when(mockMessageInterface.getServerPort()).thenReturn(1234);
+        when(mockMessageInterface.getLocalPort()).thenReturn(5678);
+        when(mockMessageInterface.getProtocol()).thenReturn("HTTP");
+        when(mockMessageInterface.getLocalAddr()).thenReturn("5.6.7.8");
+        when(mockMessageInterface.getLocalName()).thenReturn("local-name");
+        when(mockMessageInterface.getBody()).thenReturn("body");
+    }
 
     @Test
     public void testHeaders() throws Exception {
-        final JsonGeneratorParser jsonGeneratorParser = newJsonGenerator();
-        final Map<String, Collection<String>> headers = new HashMap<>();
-        headers.put("Header1", Lists.newArrayList("Value1"));
-        headers.put("Header2", Lists.newArrayList("Value1", "Value2"));
-        final HashMap<String, String> cookies = new HashMap<>();
-        cookies.put("Cookie1", "Value1");
-        new NonStrictExpectations() {{
-            mockMessageInterface.getHeaders();
-            result = headers;
-            mockMessageInterface.getRequestUrl();
-            result = "http://host/url";
-            mockMessageInterface.getMethod();
-            result = "GET";
-            mockMessageInterface.getQueryString();
-            result = "query";
-            mockMessageInterface.getCookies();
-            result = cookies;
-            mockMessageInterface.getRemoteAddr();
-            result = "1.2.3.4";
-            mockMessageInterface.getServerName();
-            result = "server-name";
-            mockMessageInterface.getServerPort();
-            result = 1234;
-            mockMessageInterface.getLocalPort();
-            result = 5678;
-            mockMessageInterface.getProtocol();
-            result = "HTTP";
-            mockMessageInterface.getLocalAddr();
-            result = "5.6.7.8";
-            mockMessageInterface.getLocalName();
-            result = "local-name";
-            mockMessageInterface.getBody();
-            result = "body";
-        }};
-
         interfaceBinding.writeInterface(jsonGeneratorParser.generator(), mockMessageInterface);
-
         JsonNode value = jsonGeneratorParser.value();
         assertThat(value, is(jsonResource("/io/sentry/marshaller/json/Http1.json")));
     }
 
     @Test
     public void testBodyWithParameters() throws Exception {
-        final JsonGeneratorParser jsonGeneratorParser = newJsonGenerator();
-        final Map<String, Collection<String>> headers = new HashMap<>();
-        headers.put("Header1", Lists.newArrayList("Value1"));
-        headers.put("Header2", Lists.newArrayList("Value1", "Value2"));
-        final HashMap<String, String> cookies = new HashMap<>();
-        cookies.put("Cookie1", "Value1");
         final HashMap<String, Collection<String>> parameters = new HashMap<>();
-        parameters.put("Parameter1", Lists.newArrayList("Value1"));
-        parameters.put("Parameter2", Lists.newArrayList("Value2", "Value3"));
-        new NonStrictExpectations() {{
-            mockMessageInterface.getHeaders();
-            result = headers;
-            mockMessageInterface.getRequestUrl();
-            result = "http://host/url";
-            mockMessageInterface.getMethod();
-            result = "GET";
-            mockMessageInterface.getQueryString();
-            result = "query";
-            mockMessageInterface.getCookies();
-            result = cookies;
-            mockMessageInterface.getRemoteAddr();
-            result = "1.2.3.4";
-            mockMessageInterface.getServerName();
-            result = "server-name";
-            mockMessageInterface.getServerPort();
-            result = 1234;
-            mockMessageInterface.getLocalPort();
-            result = 5678;
-            mockMessageInterface.getProtocol();
-            result = "HTTP";
-            mockMessageInterface.getLocalAddr();
-            result = "5.6.7.8";
-            mockMessageInterface.getLocalName();
-            result = "local-name";
-            mockMessageInterface.getBody();
-            result = "body";
-            mockMessageInterface.getParameters();
-            result = parameters;
-        }};
+        parameters.put("Parameter1", singletonList("Value1"));
+        parameters.put("Parameter2", asList("Value2", "Value3"));
+
+        when(mockMessageInterface.getParameters()).thenReturn(parameters);
 
         interfaceBinding.writeInterface(jsonGeneratorParser.generator(), mockMessageInterface);
 
