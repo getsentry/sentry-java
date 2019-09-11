3ee6ec09aa062ca2ee4faf075ada47c4385040de
Bruno Garcia <bruno@brunogarcia.com>
2019-09-11 17:44:23 -0400

    version all artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,10 @@
 import com.diffplug.spotless.LineEnding
 
 plugins {
-    `java-library`
     java
     id("com.diffplug.gradle.spotless") version "3.24.2" apply true
     jacoco
 }
-
-group = "io.sentry"
-version = "2.0.0-SNAPSHOT"
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -30,6 +26,8 @@ allprojects {
         jcenter()
         mavenCentral()
     }
+    group = "io.sentry"
+    version = "2.0.0-SNAPSHOT"
 }
 
 spotless {
