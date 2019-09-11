289e398c20ec7f2dc8046da3d886ae1687dd4951
Patrick <dreamlike.sky@foxmail.com>
2019-09-11 22:53:28 +0800

    SentryClient add shouldSendEventCallbacks and eventSendCallbacks from applicationContext

--- sentry-spring-boot-starter/src/main/java/io/sentry/spring/autoconfigure/SentryAutoConfiguration.java
+++ sentry-spring-boot-starter/src/main/java/io/sentry/spring/autoconfigure/SentryAutoConfiguration.java
@@ -1,16 +1,24 @@
 package io.sentry.spring.autoconfigure;
 
+import io.sentry.Sentry;
+import io.sentry.connection.EventSendCallback;
+import io.sentry.event.helper.ShouldSendEventCallback;
 import io.sentry.spring.SentryExceptionResolver;
 import io.sentry.spring.SentryServletContextInitializer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import javax.annotation.PostConstruct;
+import java.util.Map;
 
 /**
  * Spring Auto Configuration for Sentry.
@@ -20,6 +28,21 @@ import org.springframework.web.servlet.HandlerExceptionResolver;
 @ConditionalOnWebApplication
 @ConditionalOnProperty(name = "sentry.enabled", havingValue = "true", matchIfMissing = true)
 public class SentryAutoConfiguration {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @PostConstruct
+    public void init() {
+        Map<String, ShouldSendEventCallback> shouldSendEventCallbackMap = this.applicationContext.getBeansOfType(ShouldSendEventCallback.class);
+        for (ShouldSendEventCallback shouldSendEventCallback : shouldSendEventCallbackMap.values()) {
+            Sentry.getStoredClient().addShouldSendEventCallback(shouldSendEventCallback);
+        }
+        Map<String, EventSendCallback> eventSendCallbackMap = this.applicationContext.getBeansOfType(EventSendCallback.class);
+        for (EventSendCallback eventSendCallback : eventSendCallbackMap.values()) {
+            Sentry.getStoredClient().addEventSendCallback(eventSendCallback);
+        }
+    }
 
     /**
      * Resolves a {@link HandlerExceptionResolver}.
--- sentry-spring-boot-starter/src/test/java/io/sentry/spring/autoconfigure/SentryAutoConfigurationTest.java
+++ sentry-spring-boot-starter/src/test/java/io/sentry/spring/autoconfigure/SentryAutoConfigurationTest.java
@@ -1,7 +1,15 @@
 package io.sentry.spring.autoconfigure;
 
+import io.sentry.Sentry;
+import io.sentry.SentryClient;
+import io.sentry.connection.*;
+import io.sentry.context.ContextManager;
+import io.sentry.context.SingletonContextManager;
+import io.sentry.event.Event;
+import io.sentry.event.helper.ShouldSendEventCallback;
 import io.sentry.spring.SentryExceptionResolver;
 import io.sentry.spring.SentryServletContextInitializer;
+import io.sentry.time.FixedClock;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,11 +20,23 @@ import org.springframework.boot.autoconfigure.web.HttpMessageConvertersAutoConfi
 import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext;
 import org.springframework.boot.test.util.EnvironmentTestUtils;
 
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 public class SentryAutoConfigurationTest {
 
+	private final String publicKey = "9bcf4a8c-f353-4f25-9dda-76a873fff905";
+	private final String secretKey = "56a9d05e-9032-4fdd-8f67-867d526422f9";
+	private static final Date FIXED_DATE = new Date(1483228800L);
+
 	private AnnotationConfigEmbeddedWebApplicationContext context;
+	private static AtomicBoolean shouldEventcalled = new AtomicBoolean(false);
+	private static AtomicBoolean sendEventCalled = new AtomicBoolean(false);
+
+
 
 	@Before
 	public void setup() {
@@ -55,11 +75,55 @@ public class SentryAutoConfigurationTest {
 		assertThat(beans).isEmpty();
 	}
 
+	@Test
+	public void testSentryInitEventSendCallbacks() {
+		load();
+		FixedClock fixedClock = new FixedClock(FIXED_DATE);
+		LockdownManager lockdownManager = mock(LockdownManager.class, withSettings()
+				.useConstructor(fixedClock)
+				.defaultAnswer(CALLS_REAL_METHODS));
+		Connection mockConnection = mock(AbstractConnection.class, withSettings()
+				.useConstructor(publicKey, secretKey, lockdownManager)
+				.defaultAnswer(CALLS_REAL_METHODS));
+		ContextManager contextManager = new SingletonContextManager();
+		SentryClient sentryClient = new SentryClient(mockConnection, contextManager);
+		Sentry.setStoredClient(sentryClient);
+		this.context.refresh();
+		Event mockEvent = mock(Event.class);
+		sentryClient.sendEvent(mockEvent);
+		assertThat(shouldEventcalled.get()).isTrue();
+		assertThat(sendEventCalled.get()).isTrue();
+	}
+
 	private void load() {
 		this.context.register(EmbeddedServletContainerAutoConfiguration.class,
 				HttpMessageConvertersAutoConfiguration.class,
 				PropertyPlaceholderAutoConfiguration.class,
-				SentryAutoConfiguration.class);
+				SentryAutoConfiguration.class,
+				TestShouldSendEventCallback.class,
+				TestEventSendCallback.class);
 	}
 
+	private static class TestShouldSendEventCallback implements ShouldSendEventCallback{
+
+		@Override
+		public boolean shouldSend(Event event) {
+			shouldEventcalled.set(true);
+			return true;
+		}
+	}
+
+	private static class TestEventSendCallback implements EventSendCallback {
+
+
+		@Override
+		public void onFailure(Event event, Exception exception) {
+
+		}
+
+		@Override
+		public void onSuccess(Event event) {
+			sendEventCalled.set(true);
+		}
+	}
 }
