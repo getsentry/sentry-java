8eee63eb8325e8b2189dd5322f335f946fd8f8cb
Bruno Garcia <bruno@brunogarcia.com>
2019-09-16 14:48:27 -0400

    test: basic tests

---
+++ sentry-core/src/test/java/io/sentry/NoOpSentryClientTest.kt
@@ -0,0 +1,23 @@
+package io.sentry
+
+import java.util.UUID
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertEquals
+
+class NoOpSentryClientTest {
+    private var sut: NoOpSentryClient = NoOpSentryClient()
+
+    @Test
+    fun `client is always disabled`() = assertFalse(sut.isEnabled)
+
+    @Test
+    fun `captureEvent is returns empty UUID`() =
+        assertEquals(UUID.fromString("00000000-0000-0000-0000-000000000000"), sut.captureEvent(null))
+
+    @Test
+    fun `close does not affect captureEvent`() {
+        sut.close()
+        assertEquals(UUID.fromString("00000000-0000-0000-0000-000000000000"), sut.captureEvent(null))
+    }
+}
