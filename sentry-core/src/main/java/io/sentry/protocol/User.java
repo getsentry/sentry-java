cb05a4adaea873a43679e096cd95876d1655edb0
Bruno Garcia <bruno@brunogarcia.com>
2019-09-25 22:04:14 +0200

    doc: more javadoc

--- sentry-core/src/main/java/io/sentry/protocol/User.java
+++ sentry-core/src/main/java/io/sentry/protocol/User.java
@@ -2,6 +2,9 @@ package io.sentry.protocol;
 
 import java.util.Map;
 
+/**
+ * The user affected by an event.
+ */
 public class User {
   private String email;
   private String id;
@@ -9,42 +12,82 @@ public class User {
   private String ipAddress;
   private Map<String, String> other;
 
+  /**
+   * Gets the e-mail address of the user.
+   * @return the e-mail.
+   */
   public String getEmail() {
     return email;
   }
 
+  /**
+   * Gets the e-mail address of the user.
+   * @param email the e-mail.
+   */
   public void setEmail(String email) {
     this.email = email;
   }
 
+  /**
+   * Gets the id of the user.
+   * @return the id.
+   */
   public String getId() {
     return id;
   }
 
+  /**
+   * Sets the id of the user.
+   * @param id the user id.
+   */
   public void setId(String id) {
     this.id = id;
   }
 
+  /**
+   * Gets the username of the user.
+   * @return the username.
+   */
   public String getUsername() {
     return username;
   }
 
+  /**
+   * Sets the username of the user.
+   * @param username the username.
+   */
   public void setUsername(String username) {
     this.username = username;
   }
 
+  /**
+   * Gets the IP address of the user.
+   * @return the IP address of the user.
+   */
   public String getIpAddress() {
     return ipAddress;
   }
 
+  /**
+   * Sets the IP address of the user.
+   * @param ipAddress the IP address of the user.
+   */
   public void setIpAddress(String ipAddress) {
     this.ipAddress = ipAddress;
   }
 
+  /**
+   * Gets other user related data.
+   * @return the other user data.
+   */
   public Map<String, String> getOther() {
     return other;
   }
 
+  /**
+   * Sets other user related data.
+   * @param other the other user related data..
+   */
   public void setOther(Map<String, String> other) {
     this.other = other;
   }
