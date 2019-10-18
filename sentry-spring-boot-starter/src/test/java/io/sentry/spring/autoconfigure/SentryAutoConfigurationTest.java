10f9840fe170b0613718e5c20852f73486c18b5c
Alexey Zhokhov <alexey@zhokhov.com>
2019-10-18 18:24:03 +0800

    Code format: tabs -> spaces.

--- sentry-spring-boot-starter/src/test/java/io/sentry/spring/autoconfigure/SentryAutoConfigurationTest.java
+++ sentry-spring-boot-starter/src/test/java/io/sentry/spring/autoconfigure/SentryAutoConfigurationTest.java
@@ -5,7 +5,6 @@ import io.sentry.spring.SentryServletContextInitializer;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.HttpMessageConvertersAutoConfiguration;
@@ -16,50 +15,50 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class SentryAutoConfigurationTest {
 
-	private AnnotationConfigEmbeddedWebApplicationContext context;
+    private AnnotationConfigEmbeddedWebApplicationContext context;
 
-	@Before
-	public void setup() {
-		if (this.context == null) {
-			this.context = new AnnotationConfigEmbeddedWebApplicationContext();
-		}
-	}
+    @Before
+    public void setup() {
+        if (this.context == null) {
+            this.context = new AnnotationConfigEmbeddedWebApplicationContext();
+        }
+    }
 
-	@After
-	public void teardown() {
-		if (this.context != null) {
-			this.context.close();
-		}
-	}
+    @After
+    public void teardown() {
+        if (this.context != null) {
+            this.context.close();
+        }
+    }
 
-	@Test
-	public void testSentryExceptionResolverIsAvailable() {
-		load();
-		this.context.refresh();
-		String[] exceptionResolverBeans = this.context
-				.getBeanNamesForType(SentryExceptionResolver.class);
-		String[] servletContextInitialerBeans = this.context
-				.getBeanNamesForType(SentryServletContextInitializer.class);
+    @Test
+    public void testSentryExceptionResolverIsAvailable() {
+        load();
+        this.context.refresh();
+        String[] exceptionResolverBeans = this.context
+                .getBeanNamesForType(SentryExceptionResolver.class);
+        String[] servletContextInitialerBeans = this.context
+                .getBeanNamesForType(SentryServletContextInitializer.class);
 
-		assertThat(exceptionResolverBeans).contains("sentryExceptionResolver");
-		assertThat(servletContextInitialerBeans)
-				.contains("sentryServletContextInitializer");
-	}
+        assertThat(exceptionResolverBeans).contains("sentryExceptionResolver");
+        assertThat(servletContextInitialerBeans)
+                .contains("sentryServletContextInitializer");
+    }
 
-	@Test
-	public void testSentryAutoConfigurationIsEnabled() {
-		load();
-		EnvironmentTestUtils.addEnvironment(this.context, "sentry.enabled:false");
-		this.context.refresh();
-		String[] beans = this.context.getBeanNamesForType(SentryExceptionResolver.class);
-		assertThat(beans).isEmpty();
-	}
+    @Test
+    public void testSentryAutoConfigurationIsEnabled() {
+        load();
+        EnvironmentTestUtils.addEnvironment(this.context, "sentry.enabled:false");
+        this.context.refresh();
+        String[] beans = this.context.getBeanNamesForType(SentryExceptionResolver.class);
+        assertThat(beans).isEmpty();
+    }
 
-	private void load() {
-		this.context.register(EmbeddedServletContainerAutoConfiguration.class,
-				HttpMessageConvertersAutoConfiguration.class,
-				PropertyPlaceholderAutoConfiguration.class,
-				SentryAutoConfiguration.class);
-	}
+    private void load() {
+        this.context.register(EmbeddedServletContainerAutoConfiguration.class,
+                HttpMessageConvertersAutoConfiguration.class,
+                PropertyPlaceholderAutoConfiguration.class,
+                SentryAutoConfiguration.class);
+    }
 
 }
