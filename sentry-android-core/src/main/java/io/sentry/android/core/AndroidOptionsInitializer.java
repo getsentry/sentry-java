b3c937dd49f8555f853a6afda48a5c9eafee6d97
Bruno Garcia <bruno@brunogarcia.com>
2019-10-22 04:05:23 -0400

    ref: Reuse logger instance if possible (#43)

--- sentry-android-core/src/main/java/io/sentry/android/core/AndroidOptionsInitializer.java
+++ sentry-android-core/src/main/java/io/sentry/android/core/AndroidOptionsInitializer.java
@@ -1,12 +1,17 @@
 package io.sentry.android.core;
 
 import android.content.Context;
+import io.sentry.core.ILogger;
 import io.sentry.core.SentryOptions;
 
 class AndroidOptionsInitializer {
   static void init(SentryOptions options, Context context) {
+    init(options, context, new AndroidLogger());
+  }
+
+  static void init(SentryOptions options, Context context, ILogger logger) {
     // Firstly set the logger, if `debug=true` configured, logging can start asap.
-    options.setLogger(new AndroidLogger());
+    options.setLogger(logger);
 
     // TODO this needs to fetch the data from somewhere - defined at build time?
     options.setSentryClientName("sentry-android/0.0.1");
