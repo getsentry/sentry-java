b61c7933875e70ea748af67cc63fa2e3387da1a2
Bruno Garcia <bruno@brunogarcia.com>
2019-10-22 14:28:19 -0400

    fix: captureMessage sets to event (getsentry/sentry-android#50)

--- sentry-core/src/main/java/io/sentry/core/ISentryClient.java
+++ sentry-core/src/main/java/io/sentry/core/ISentryClient.java
@@ -25,6 +25,7 @@ public interface ISentryClient {
     SentryEvent event = new SentryEvent();
     Message sentryMessage = new Message();
     sentryMessage.setFormatted(message);
+    event.setMessage(sentryMessage);
     return captureEvent(event, scope);
   }
 
