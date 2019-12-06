fe9febfa43591dc041ba2e44269b719122fc8baf
Manoel Aranda Neto <marandaneto@gmail.com>
2019-12-06 16:22:34 +0100

    chore: 2.0.0-beta01

--- sentry-native-sample/build.gradle.kts
+++ sentry-native-sample/build.gradle.kts
@@ -70,7 +70,7 @@ android {
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
-    implementation("io.sentry:sentry-android:2.0.0-alpha09")
+    implementation("io.sentry:sentry-android:2.0.0-beta01")
 
     implementation(Config.Libs.appCompat)
 
