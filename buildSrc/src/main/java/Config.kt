eb432519604cc41cc8aec808030335d3b206b2fd
Manoel Aranda Neto <marandaneto@gmail.com>
2019-12-11 11:17:41 -0800

    chore: 2.0.0-beta02

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -50,10 +50,10 @@ object Config {
         val SENTRY_CLIENT_NAME = "sentry.java.android"
         val group = "io.sentry"
 //        TODO: change version to publish new version
-        val version = "2.0.0-beta01"
+        val version = "2.0.0-beta02"
         val description = "SDK for sentry.io"
 //        TODO: change version code to publish new version, follow the pattern of `version`
-        val buildVersionCode = 20010
+        val buildVersionCode = 20011
         val website = "https://sentry.io"
         val userOrg = "getsentry"
         val repoName = "sentry-android"
