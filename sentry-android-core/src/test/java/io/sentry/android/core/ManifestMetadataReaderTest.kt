77237aa4ecac084c512936b734ab3a5752b443ae
Bruno Garcia <bruno@brunogarcia.com>
2020-02-11 08:47:20 -0500

    feat: Take sampleRate from metadata (#262)

--- sentry-android-core/src/test/java/io/sentry/android/core/ManifestMetadataReaderTest.kt
+++ sentry-android-core/src/test/java/io/sentry/android/core/ManifestMetadataReaderTest.kt
@@ -1,17 +1,25 @@
 package io.sentry.android.core
 
 import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.anyVararg
+import com.nhaarman.mockitokotlin2.argWhere
 import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.never
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
 import io.sentry.core.ILogger
 import io.sentry.core.SentryLevel
 import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 import org.junit.runner.RunWith
 
@@ -42,5 +50,73 @@ class ManifestMetadataReaderTest {
         val logger = mock<ILogger>()
         options.setLogger(logger)
         verify(logger, never()).log(eq(SentryLevel.ERROR), any<String>(), any())
+    }
+
+    @Test
+    fun `applyMetadata reads sampleRate from metadata`() {
+        // Arrange
+        val options = SentryAndroidOptions()
+        val expectedPackageName = "io.sentry.test"
+        val expectedSampleRate = 0.99
+        val contextMock: Context = mock()
+        whenever(contextMock.packageName).thenReturn(expectedPackageName)
+        val bundle: Bundle = mock()
+        whenever(bundle.getDouble(eq(ManifestMetadataReader.SAMPLE_RATE), anyVararg())).thenReturn(expectedSampleRate)
+        val packageManagerMock: PackageManager = mock()
+        val applicationInfo: ApplicationInfo = mock()
+        applicationInfo.metaData = bundle
+        whenever(packageManagerMock.getApplicationInfo(argWhere { it == expectedPackageName }, eq(PackageManager.GET_META_DATA))).thenReturn(applicationInfo)
+        whenever(contextMock.packageManager).thenReturn(packageManagerMock)
+
+        // Act
+        ManifestMetadataReader.applyMetadata(contextMock, options)
+
+        // Assert
+        assertEquals(expectedSampleRate, options.sampleRate)
+    }
+
+    @Test
+    fun `applyMetadata does not override sampleRate from options`() {
+        // Arrange
+        val expectedSampleRate = 0.99
+        val options = SentryAndroidOptions()
+        options.sampleRate = expectedSampleRate
+        val expectedPackageName = "io.sentry.test"
+        val contextMock: Context = mock()
+        whenever(contextMock.packageName).thenReturn(expectedPackageName)
+        val bundle: Bundle = mock()
+        whenever(bundle.getDouble(eq(ManifestMetadataReader.SAMPLE_RATE), anyVararg())).thenReturn(0.1)
+        val packageManagerMock: PackageManager = mock()
+        val applicationInfo: ApplicationInfo = mock()
+        applicationInfo.metaData = bundle
+        whenever(packageManagerMock.getApplicationInfo(argWhere { it == expectedPackageName }, eq(PackageManager.GET_META_DATA))).thenReturn(applicationInfo)
+        whenever(contextMock.packageManager).thenReturn(packageManagerMock)
+
+        // Act
+        ManifestMetadataReader.applyMetadata(contextMock, options)
+
+        // Assert
+        assertEquals(expectedSampleRate, options.sampleRate)
+    }
+
+    @Test
+    fun `applyMetadata without specifying sampleRate, stays null`() {
+        // Arrange
+        val options = SentryAndroidOptions()
+        val expectedPackageName = "io.sentry.test"
+        val contextMock: Context = mock()
+        whenever(contextMock.packageName).thenReturn(expectedPackageName)
+        val bundle: Bundle = mock()
+        val packageManagerMock: PackageManager = mock()
+        val applicationInfo: ApplicationInfo = mock()
+        applicationInfo.metaData = bundle
+        whenever(packageManagerMock.getApplicationInfo(argWhere { it == expectedPackageName }, eq(PackageManager.GET_META_DATA))).thenReturn(applicationInfo)
+        whenever(contextMock.packageManager).thenReturn(packageManagerMock)
+
+        // Act
+        ManifestMetadataReader.applyMetadata(contextMock, options)
+
+        // Assert
+        assertNull(options.sampleRate)
     }
 }
