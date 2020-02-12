14e563589e36f0e787295b7905e8801e2113b143
Manoel Aranda Neto <marandaneto@gmail.com>
2020-02-12 16:52:36 +0100

    add javadocs to plugin conf

--- sentry-android-gradle-plugin/src/main/groovy/io/sentry/android/gradle/SentryPluginExtension.groovy
+++ sentry-android-gradle-plugin/src/main/groovy/io/sentry/android/gradle/SentryPluginExtension.groovy
@@ -4,6 +4,20 @@ class SentryPluginExtension {
     def boolean autoProguardConfig = true;
     def boolean autoUpload = true;
     def String manifestPath = null;
-    def boolean includeNativeSources = false;
+
+    /**
+     * Disables or enables the automatic configuration of Native Symbols
+     * for Sentry. This executes sentry-cli automatically so
+     * you don't need to do it manually.
+     * Default is disabled.
+     */
     def boolean uploadNativeSymbols = false;
+
+    /**
+     * Includes or not the source code of native code for Sentry.
+     * This executes sentry-cli with the --include-sources param. automatically so
+     * you don't need to do it manually.
+     * Default is disabled.
+     */
+    def boolean includeNativeSources = false;
 }
