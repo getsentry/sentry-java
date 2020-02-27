e59fdec0305cac16b0040e06b24fad931c64a355
Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>
2020-02-27 09:36:57 +0100

    bump bintray plugin 1.0.1 (getsentry/sentry-android#284)

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -79,7 +79,7 @@ object Config {
     }
 
     object Deploy {
-        val novodaBintrayPlugin = "com.novoda:bintray-release:1.0.0"
+        val novodaBintrayPlugin = "com.novoda:bintray-release:1.0.1"
         val novodaBintray = "com.novoda.bintray-release"
         val sign = true
         val mavenCentralSync = true
