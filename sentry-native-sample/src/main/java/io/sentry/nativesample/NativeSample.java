5ed8e2d64151bb6cda031762c37b6b45b5a2f348
Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>
2020-04-07 14:22:34 +0200

    remove sentry-native-sample from this repo (getsentry/sentry-android#346)
    * remove sentry-native-sample from this repo
    * remove parallel mode as its not necessary anymore

--- sentry-native-sample/src/main/java/io/sentry/nativesample/NativeSample.java
+++
@@ -1,9 +0,0 @@
-package io.sentry.nativesample;
-
-public class NativeSample {
-  public static native void message();
-
-  static {
-    System.loadLibrary("native-sample");
-  }
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
 rootProject.name = "sentry"
 rootProject.buildFileName = "build.gradle.kts"
 
-include("sentry-android", "sentry-android-ndk", "sentry-android-core", "sentry-core", "sentry-sample", "sentry-native-sample")
+include("sentry-android", "sentry-android-ndk", "sentry-android-core", "sentry-core", "sentry-sample")
