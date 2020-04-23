34071827b4659b2bb72598112b8951d416101e9b
Arpad Borsos <arpad.borsos@sentry.io>
2020-04-23 11:30:41 +0200

    fix: Merge loadLibrary calls for sentry-native and clean up CMake files (getsentry/sentry-android#373)

--- sentry-android-ndk/src/main/java/io/sentry/android/ndk/SentryNdk.java
+++ sentry-android-ndk/src/main/java/io/sentry/android/ndk/SentryNdk.java
@@ -9,22 +9,19 @@ public final class SentryNdk {
   private SentryNdk() {}
 
   static {
+    // On older Android versions, it was necessary to manually call "`System.loadLibrary` on all
+    // transitive dependencies before loading [the] main library."
+    // The dependencies of `libsentry.so` are currently `lib{c,m,dl,log}.so`.
+    // See
+    // https://android.googlesource.com/platform/bionic/+/master/android-changes-for-ndk-developers.md#changes-to-library-dependency-resolution
+    System.loadLibrary("log");
     System.loadLibrary("sentry");
-  }
-
-  static {
     System.loadLibrary("sentry-android");
   }
 
   private static native void initSentryNative(SentryOptions options);
 
-  private static native void verificationEventNative();
-
   public static void init(SentryOptions options) {
     initSentryNative(options);
-  }
-
-  public static void verificationEvent() {
-    verificationEventNative();
   }
 }
