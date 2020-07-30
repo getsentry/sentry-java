ac183ef78edc0fe92e22609f62b3b7a89cf67f01
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2020-07-30 13:31:38 +0200

    Add console application sample. (getsentry/sentry-android#502)

---
+++ sentry-samples/sentry-samples-console/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    java
+    id(Config.QualityPlugins.gradleVersions)
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    implementation(project(":sentry-core"))
+}
