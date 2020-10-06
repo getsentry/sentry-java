6c76de52aa736e011273bf719495180d179cd190
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2020-10-06 11:33:23 +0200

    Update sentry-spring-boot-starter/src/main/java/io/sentry/spring/boot/InAppIncludesResolver.java
    Co-authored-by: Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>

--- sentry-spring-boot-starter/src/main/java/io/sentry/spring/boot/InAppIncludesResolver.java
+++ sentry-spring-boot-starter/src/main/java/io/sentry/spring/boot/InAppIncludesResolver.java
@@ -20,7 +20,7 @@ import org.springframework.context.ApplicationContextAware;
  */
 @Open
 class InAppIncludesResolver implements ApplicationContextAware {
-  @Nullable private ApplicationContext applicationContext;
+  private @Nullable ApplicationContext applicationContext;
 
   @Nullable
   List<String> resolveInAppIncludes() {
