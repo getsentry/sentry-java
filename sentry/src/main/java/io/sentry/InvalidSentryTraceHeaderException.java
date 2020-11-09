ffa560c1f4bc6484373bd70af6e5b45c8e874b19
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2020-11-09 13:50:34 +0100

    Polish.

--- sentry/src/main/java/io/sentry/InvalidSentryTraceHeaderException.java
+++ sentry/src/main/java/io/sentry/InvalidSentryTraceHeaderException.java
@@ -4,14 +4,14 @@ import org.jetbrains.annotations.NotNull;
 
 public final class InvalidSentryTraceHeaderException extends Exception {
   private static final long serialVersionUID = 1L;
-  private final String sentryTraceHeader;
+  private final @NotNull String sentryTraceHeader;
 
   public InvalidSentryTraceHeaderException(final @NotNull String sentryTraceHeader) {
     super("sentry-trace header does not conform to expected format: " + sentryTraceHeader);
     this.sentryTraceHeader = sentryTraceHeader;
   }
 
-  public String getSentryTraceHeader() {
+  public @NotNull String getSentryTraceHeader() {
     return sentryTraceHeader;
   }
 }
