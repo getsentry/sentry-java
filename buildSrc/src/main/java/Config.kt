969dc5816da110d21677619184e5a6061677d8c3
Manoel Aranda Neto <marandaneto@gmail.com>
2020-11-12 15:29:59 +0100

    fix

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -27,7 +27,7 @@ object Config {
         val compileSdkVersion = sdkVersion
         // CMake: Projects that don't set a specific CMake version are built with CMake 3.10.2
 
-        // ndk is set to 21.4.x but I see on the logs the version 21.1.x, not sure if it's a bug
+        // ndk is set to 21.3.x but I see on the logs the version 21.1.x, not sure if it's a bug
         // on the Gradle plugin picking up the wrong version.
         val ndkVersion = "21.3.6528147"
         val abiFilters = listOf("x86", "armeabi-v7a", "x86_64", "arm64-v8a")
