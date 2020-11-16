94691a299a8acbf2e658d6be378e1b8131030b6c
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2020-11-16 13:09:31 +0100

    Resolve HTTP Proxy parameters from the external configuration. (#1035)

--- sentry/src/main/java/io/sentry/config/PropertiesProvider.java
+++ sentry/src/main/java/io/sentry/config/PropertiesProvider.java
@@ -12,4 +12,17 @@ public interface PropertiesProvider {
    */
   @Nullable
   String getProperty(@NotNull String property);
+
+  /**
+   * Resolves property given by it's name.
+   *
+   * @param property - the property name
+   * @param defaultValue - the default value if property is not set
+   * @return property value or the default value if not found.
+   */
+  @NotNull
+  default String getProperty(@NotNull String property, @NotNull String defaultValue) {
+    final String result = getProperty(property);
+    return result != null ? result : defaultValue;
+  }
 }
