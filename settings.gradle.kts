4199bb25f2c74aa93664635f3ece1c5f4b79509d
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2020-11-19 10:03:03 +0100

    Bring back original settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,9 @@
 rootProject.name = "sentry-root"
 rootProject.buildFileName = "build.gradle.kts"
 
-include(
-//    "sentry-android",
-//    "sentry-android-ndk",
-//    "sentry-android-core",
+include("sentry-android",
+    "sentry-android-ndk",
+    "sentry-android-core",
     "sentry",
     "sentry-test-support",
     "sentry-log4j2",
@@ -12,8 +11,8 @@ include(
     "sentry-servlet",
     "sentry-spring",
     "sentry-spring-boot-starter",
-//    "sentry-android-timber",
-//    "sentry-samples:sentry-samples-android",
+    "sentry-android-timber",
+    "sentry-samples:sentry-samples-android",
     "sentry-samples:sentry-samples-console",
     "sentry-samples:sentry-samples-log4j2",
     "sentry-samples:sentry-samples-logback",
