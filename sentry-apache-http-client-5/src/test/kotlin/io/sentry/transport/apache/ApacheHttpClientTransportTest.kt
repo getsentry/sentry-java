883aaa97379d1070f8ddfe5a1231f298a3ced19c
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2021-02-17 14:56:05 +0100

    Check if increasing timeout to 100ms solves the flakiness

--- sentry-apache-http-client-5/src/test/kotlin/io/sentry/transport/apache/ApacheHttpClientTransportTest.kt
+++ sentry-apache-http-client-5/src/test/kotlin/io/sentry/transport/apache/ApacheHttpClientTransportTest.kt
@@ -124,7 +124,7 @@ class ApacheHttpClientTransportTest {
             sut.send(SentryEnvelope.from(fixture.options.serializer, SentryEvent(), null))
             sut.send(SentryEnvelope.from(fixture.options.serializer, SentryEvent(), null))
 
-            sut.flush(50)
+            sut.flush(100)
 
             verify(fixture.currentlyRunning, times(3)).decrement()
         }
