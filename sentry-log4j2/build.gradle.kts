a49b0c7a22023cb27f1babd1f0648206a5ce0f3a
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2021-03-12 08:23:45 +0100

    Drop Novoda Bintray plugin.

--- sentry-log4j2/build.gradle.kts
+++ sentry-log4j2/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id(Config.QualityPlugins.errorProne)
     id(Config.QualityPlugins.gradleVersions)
     id(Config.BuildPlugins.buildConfig) version Config.BuildPlugins.buildConfigVersion
+    `maven-publish`
 }
 
 configure<JavaPluginConvention> {
@@ -74,4 +75,40 @@ buildConfig {
 val generateBuildConfig by tasks
 tasks.withType<JavaCompile>().configureEach {
     dependsOn(generateBuildConfig)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            pom {
+                description.set(Config.Sentry.description)
+                url.set(Config.Sentry.website)
+                packaging = "jar"
+                licenses {
+                    license {
+                        name.set(Config.Sentry.licence)
+                        url.set(Config.Sentry.licenceUrl)
+                    }
+                }
+                developers {
+                    developer {
+                        id.set(Config.Sentry.userOrg)
+                        email.set(Config.Sentry.devEmail)
+                        name.set(Config.Sentry.devName)
+                    }
+                }
+                scm {
+                    connection.set(Config.Sentry.scmConnection)
+                    developerConnection.set(Config.Sentry.scmDevConnection)
+                    url.set(Config.Sentry.scmUrl)
+                }
+                issueManagement {
+                    url.set(Config.Sentry.issueTracker)
+                }
+            }
+            groupId = project.group.toString()
+            artifactId = project.name
+        }
+    }
 }
