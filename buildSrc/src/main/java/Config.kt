33d4276011a4d5686eb796a2f7ffb0b3c9446d1b
Bruno Garcia <bruno@brunogarcia.com>
2021-03-14 10:23:23 -0400

    ref: contact through sdk email

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -127,7 +127,7 @@ object Config {
         val issueTracker = "https://github.com/getsentry/sentry-java/issues"
         val repository = "https://github.com/getsentry/sentry-java"
         val devName = "Sentry Team and Contributors"
-        val devEmail = "oss@sentry.io"
+        val devEmail = "sdks@sentry.io"
         val scmConnection = "scm:git:git://github.com/getsentry/sentry-java.git"
         val scmDevConnection = "scm:git:ssh://github.com:getsentry/sentry-java.git"
         val scmUrl = "https://github.com/getsentry/sentry-java/tree/main"
