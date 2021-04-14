15081b334290526a040cd102f128513f45e1d21f
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2021-04-14 09:38:00 +0200

    Polish test.

--- sentry-apache-http-client-5/src/test/kotlin/io/sentry/transport/apache/ApacheHttpClientTransportFactoryTest.kt
+++ sentry-apache-http-client-5/src/test/kotlin/io/sentry/transport/apache/ApacheHttpClientTransportFactoryTest.kt
@@ -1,12 +1,12 @@
 package io.sentry.transport.apache
 
 import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.spy
-import com.nhaarman.mockitokotlin2.times
-import com.nhaarman.mockitokotlin2.verify
 import io.sentry.SentryOptions
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
+import org.apache.hc.client5.http.config.RequestConfig
+import org.apache.hc.client5.http.impl.async.InternalHttpAsyncClient
 
 class ApacheHttpClientTransportFactoryTest {
 
@@ -20,9 +20,26 @@ class ApacheHttpClientTransportFactoryTest {
     @Test
     fun `options timeouts are used when creating ApacheHttpClientTransport`() {
         val factory = ApacheHttpClientTransportFactory()
-        val options = spy(SentryOptions())
-        factory.create(options, mock())
-        verify(options, times(2)).connectionTimeoutMillis
-        verify(options).readTimeoutMillis
+        val options = SentryOptions().apply {
+            this.connectionTimeoutMillis = 1500
+            this.readTimeoutMillis = 2500
+        }
+        val transport = factory.create(options, mock()) as ApacheHttpClientTransport
+        val requestConfig = transport.getClient().getRequestConfig()
+        assertEquals(1500, requestConfig.connectTimeout.toMilliseconds())
+        assertEquals(1500, requestConfig.connectionRequestTimeout.toMilliseconds())
+        assertEquals(2500, requestConfig.responseTimeout.toMilliseconds())
     }
+
+    private fun ApacheHttpClientTransport.getClient(): InternalHttpAsyncClient = this.getProperty("httpclient")
+    private fun InternalHttpAsyncClient.getRequestConfig(): RequestConfig = this.getProperty("defaultConfig")
+
+    private inline fun <reified T> Any.getProperty(name: String): T =
+        try {
+            this::class.java.getDeclaredField(name)
+        } catch (_: NoSuchFieldException) {
+            this::class.java.superclass.getDeclaredField(name)
+        }.apply {
+            this.isAccessible = true
+        }.get(this) as T
 }
