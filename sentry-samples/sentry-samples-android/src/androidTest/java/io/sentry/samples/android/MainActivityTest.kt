513f0983982c5991912de39ae460c50cdf1a4270
denrase <denrase@gmail.com>
2021-04-19 09:24:53 +0200

    Handle more dialogs

--- sentry-samples/sentry-samples-android/src/androidTest/java/io/sentry/samples/android/MainActivityTest.kt
+++ sentry-samples/sentry-samples-android/src/androidTest/java/io/sentry/samples/android/MainActivityTest.kt
@@ -1,6 +1,5 @@
 package io.sentry.samples.android
 
-import android.content.Context
 import android.support.test.uiautomator.UiDevice
 import android.support.test.uiautomator.UiSelector
 import androidx.test.espresso.Espresso.onView
@@ -13,7 +12,6 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import com.microsoft.appcenter.espresso.Factory
 import com.microsoft.appcenter.espresso.ReportHelper
-import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 
@@ -26,11 +24,6 @@ class MainActivityTest {
     @get:Rule
     val reportHelper: ReportHelper = Factory.getReportHelper()
 
-    @After
-    fun tearDown() {
-        reportHelper.label("tearDown")
-    }
-
     // Tests are run in alphabetical order...
 
     // We crash in a service to test if an envelope was created.
@@ -38,20 +31,23 @@ class MainActivityTest {
     fun a_initSdkAndCrash() {
         reportHelper.label("initSdkAndCrash")
         onView(withId(R.id.native_crash)).perform(scrollTo(), click())
-        Thread.sleep(5000)
+        Thread.sleep(10000)
 
         // Close either app or android dialog
-
 
         val mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
         val okButton = mDevice.findObject(UiSelector().text("OK"))
         val closeAppButton = mDevice.findObject(UiSelector().text("Close app"))
+        val allowButton = mDevice.findObject(UiSelector().text("OK"))
         when {
             okButton.exists() -> {
                 okButton.click()
             }
             closeAppButton.exists() -> {
                 closeAppButton.click()
+            }
+            allowButton.exists() -> {
+                allowButton.click()
             }
             else -> {
                 mDevice.pressBack()
@@ -62,6 +58,23 @@ class MainActivityTest {
     // Check if envelope file is here.
     @Test
     fun b_checkIfFileExists() {
+
+        val mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        val okButton = mDevice.findObject(UiSelector().text("OK"))
+        val closeAppButton = mDevice.findObject(UiSelector().text("Close app"))
+        val allowButton = mDevice.findObject(UiSelector().text("OK"))
+        when {
+            okButton.exists() -> {
+                okButton.click()
+            }
+            closeAppButton.exists() -> {
+                closeAppButton.click()
+            }
+            allowButton.exists() -> {
+                allowButton.click()
+            }
+        }
+
         onView(withId(R.id.button_copy_file)).perform(click())
         onView(withId(R.id.text_view_copy_file))
             .check(matches(withText("SUCCESS")))
