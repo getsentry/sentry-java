69f707e183743976666115f2a63f03290f3bd56d
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2021-04-30 09:49:52 +0200

    Add nullability annotations to `ISerializer`

--- sentry/src/main/java/io/sentry/ISerializer.java
+++ sentry/src/main/java/io/sentry/ISerializer.java
@@ -1,5 +1,8 @@
 package io.sentry;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -8,13 +11,13 @@ import java.io.Writer;
 import java.util.Map;
 
 public interface ISerializer {
-  <T> T deserialize(Reader reader, Class<T> clazz);
+  <T> @Nullable T deserialize(@NotNull Reader reader, @NotNull Class<T> clazz);
 
-  SentryEnvelope deserializeEnvelope(InputStream inputStream);
+  @Nullable SentryEnvelope deserializeEnvelope(@NotNull InputStream inputStream);
 
-  <T> void serialize(T entity, Writer writer) throws IOException;
+  <T> void serialize(@NotNull T entity, @NotNull Writer writer) throws IOException;
 
-  void serialize(SentryEnvelope envelope, OutputStream outputStream) throws Exception;
+  void serialize(@NotNull SentryEnvelope envelope, @NotNull OutputStream outputStream) throws Exception;
 
-  String serialize(Map<String, Object> data) throws Exception;
+  @NotNull String serialize(@NotNull Map<String, Object> data) throws Exception;
 }
