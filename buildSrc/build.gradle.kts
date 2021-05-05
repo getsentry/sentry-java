b1f79e15377f758344e1c99155944c99d4990710
Manoel Aranda Neto <marandaneto@gmail.com>
2021-05-05 10:34:21 +0200

    fix

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     `kotlin-dsl`
 }
 
+repositories {
+    mavenCentral()
+}
+
 // Disabling the warning about the use of experimental Kotlin compiler features
 kotlinDslPluginOptions {
     experimentalWarning.set(false)
