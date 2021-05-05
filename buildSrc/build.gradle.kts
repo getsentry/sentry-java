914d422a3842fa674aecede336eb0289f5447b19
Manoel Aranda Neto <marandaneto@gmail.com>
2021-05-05 10:31:21 +0200

    remove jcenter

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     `kotlin-dsl`
 }
 
-repositories {
-    jcenter()
-}
-
 // Disabling the warning about the use of experimental Kotlin compiler features
 kotlinDslPluginOptions {
     experimentalWarning.set(false)
