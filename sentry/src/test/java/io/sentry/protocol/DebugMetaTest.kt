be4928319dc006967582dc903dfc185c525e1b9e
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2021-05-12 11:47:59 +0200

    Polish.

--- sentry/src/test/java/io/sentry/protocol/DebugMetaTest.kt
+++ sentry/src/test/java/io/sentry/protocol/DebugMetaTest.kt
@@ -8,9 +8,10 @@ class DebugMetaTest {
 
     @Test
     fun `when setDebugImages receives immutable list as an argument, its still possible to add more debugImages`() {
-        val meta = DebugMeta()
-        meta.images = listOf(DebugImage(), DebugImage())
-        meta.images!! += DebugImage()
+        val meta = DebugMeta().apply {
+            images = listOf(DebugImage(), DebugImage())
+            images!! += DebugImage()
+        }
         assertNotNull(meta.images) {
             assertEquals(3, it.size)
         }
