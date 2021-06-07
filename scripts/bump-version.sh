3763714e052563d74ae79c80e453e723aa478d85
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-06-07 14:29:38 +0200

    Remove `echo`s

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -17,9 +17,7 @@ VERSION_CODE_PATTERN="buildVersionCode"
 VERSION_NUMBER="$( awk "/$VERSION_CODE_PATTERN/" $GRADLE_FILEPATH | grep -o '[0-9]\+' )"
 ((VERSION_NUMBER++))
 sed -ie "s/$VERSION_CODE_PATTERN=.*$/$VERSION_CODE_PATTERN=$VERSION_NUMBER/g" $GRADLE_FILEPATH
-echo "buildVersionCode done"
 
 # Replace `versionName` with the given version
 VERSION_NAME_PATTERN="versionName"
 sed -ie "s/$VERSION_NAME_PATTERN=.*$/$VERSION_NAME_PATTERN=$NEW_VERSION/g" $GRADLE_FILEPATH
-echo "versionName done"
