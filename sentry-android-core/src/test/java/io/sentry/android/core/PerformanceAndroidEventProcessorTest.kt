2b554bbf19335e813f6d07f18c21241e80c9070f
Manoel Aranda Neto <marandaneto@gmail.com>
2021-06-10 12:41:09 +0200

    fix

--- sentry-android-core/src/test/java/io/sentry/android/core/PerformanceAndroidEventProcessorTest.kt
+++ sentry-android-core/src/test/java/io/sentry/android/core/PerformanceAndroidEventProcessorTest.kt
@@ -1,7 +1,10 @@
 package io.sentry.android.core
 
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import io.sentry.IHub
 import io.sentry.SentryTracer
+import io.sentry.TransactionContext
 import io.sentry.protocol.SentryTransaction
 import java.util.Date
 import kotlin.test.BeforeTest
@@ -13,10 +16,13 @@ class PerformanceAndroidEventProcessorTest {
     private class Fixture {
         val options = SentryAndroidOptions()
 
-        val tracer = SentryTracer(mock(), mock())
+        val hub = mock<IHub>()
+        val context = TransactionContext("name", "op")
+        val tracer = SentryTracer(context, hub)
 
         fun getSut(tracesSampleRate: Double? = 1.0): PerformanceAndroidEventProcessor {
             options.tracesSampleRate = tracesSampleRate
+            whenever(hub.options).thenReturn(options)
             return PerformanceAndroidEventProcessor(options)
         }
     }
@@ -32,7 +38,7 @@ class PerformanceAndroidEventProcessorTest {
     fun `add cold start measurement`() {
         val sut = fixture.getSut()
 
-        var tr = SentryTransaction(fixture.tracer)
+        var tr = getTransaction()
         setAppStart()
 
         tr = sut.process(tr, null)
@@ -44,7 +50,7 @@ class PerformanceAndroidEventProcessorTest {
     fun `add warm start measurement`() {
         val sut = fixture.getSut()
 
-        var tr = SentryTransaction(fixture.tracer)
+        var tr = getTransaction()
         setAppStart(false)
 
         tr = sut.process(tr, null)
@@ -56,12 +62,12 @@ class PerformanceAndroidEventProcessorTest {
     fun `do not add app start metric twice`() {
         val sut = fixture.getSut()
 
-        var tr1 = SentryTransaction(fixture.tracer)
+        var tr1 = getTransaction()
         setAppStart(false)
 
         tr1 = sut.process(tr1, null)
 
-        var tr2 = SentryTransaction(fixture.tracer)
+        var tr2 = getTransaction()
         tr2 = sut.process(tr2, null)
 
         assertTrue(tr1.measurements.containsKey("app_start_warm"))
@@ -72,7 +78,7 @@ class PerformanceAndroidEventProcessorTest {
     fun `do not add app start metric if its not ready`() {
         val sut = fixture.getSut()
 
-        var tr = SentryTransaction(fixture.tracer)
+        var tr = getTransaction()
 
         tr = sut.process(tr, null)
 
@@ -83,7 +89,7 @@ class PerformanceAndroidEventProcessorTest {
     fun `do not add app start metric if performance is disabled`() {
         val sut = fixture.getSut(tracesSampleRate = null)
 
-        var tr = SentryTransaction(fixture.tracer)
+        var tr = getTransaction()
 
         tr = sut.process(tr, null)
 
@@ -94,5 +100,10 @@ class PerformanceAndroidEventProcessorTest {
         AppStartState.getInstance().isColdStart = coldStart
         AppStartState.getInstance().setAppStartTime(0, Date())
         AppStartState.getInstance().setAppStartEnd()
+    }
+
+    private fun getTransaction(): SentryTransaction {
+        fixture.tracer.startChild("app.start")
+        return SentryTransaction(fixture.tracer)
     }
 }
