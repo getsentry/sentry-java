513a3ec5a91f04bde6b5365a1f51af111d4220f0
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-06-10 18:36:00 +0200

    feat: enable manual Sentry init in sample app by default

--- sentry-samples/sentry-samples-android/src/main/java/io/sentry/samples/android/MyApplication.java
+++ sentry-samples/sentry-samples-android/src/main/java/io/sentry/samples/android/MyApplication.java
@@ -2,9 +2,8 @@ package io.sentry.samples.android;
 
 import android.app.Application;
 import android.os.StrictMode;
-
-// import io.sentry.android.fragment.FragmentLifecycleIntegration;
-// import io.sentry.android.core.SentryAndroid;
+import io.sentry.android.core.SentryAndroid;
+import io.sentry.android.fragment.FragmentLifecycleIntegration;
 
 /** Apps. main Application. */
 public class MyApplication extends Application {
@@ -16,18 +15,17 @@ public class MyApplication extends Application {
 
     // Example how to initialize the SDK manually which allows access to SentryOptions callbacks.
     // Make sure you disable the auto init via manifest meta-data: io.sentry.auto-init=false
-    // SentryAndroid.init(
-    //    this,
-    //    options -> {
-    //      options.setBeforeSend(
-    //          (event, hint) -> {
-    //            event.setTag("sample-key", "before-send");
-    //            return event;
-    //          });
-    //      options.setAnrTimeoutIntervalMillis(2000);
-    //      options.addIntegration(
-    //          new FragmentLifecycleIntegration(MyApplication.this));
-    //    });
+    SentryAndroid.init(
+        this,
+        options -> {
+          options.setBeforeSend(
+              (event, hint) -> {
+                event.setTag("sample-key", "before-send");
+                return event;
+              });
+          options.setAnrTimeoutIntervalMillis(2000);
+          options.addIntegration(new FragmentLifecycleIntegration(MyApplication.this));
+        });
   }
 
   private void strictMode() {
