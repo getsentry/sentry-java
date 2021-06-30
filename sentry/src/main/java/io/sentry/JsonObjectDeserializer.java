e1d2162e214f703477fd9f6dcba5f4e3647a0b38
denrase <denrase@gmail.com>
2021-06-30 16:13:07 +0200

    change implementations to state holding tokens

--- sentry/src/main/java/io/sentry/JsonObjectDeserializer.java
+++ sentry/src/main/java/io/sentry/JsonObjectDeserializer.java
@@ -11,32 +11,49 @@ import java.util.Map;
 
 import io.sentry.vendor.gson.stream.JsonToken;
 
+
 @ApiStatus.Internal
 public final class JsonObjectDeserializer {
 
-  private final ArrayList<JsonToken> tokenStack = new ArrayList<>();
+  // Tokens
 
-  private @Nullable String currentName;
-  private @Nullable ArrayList<Object> currentArray;
+  static final class TokenName {
+    final String name;
+    TokenName(@NotNull String name) {
+      this.name = name;
+    }
+  }
+
+  static final class TokenArray {
+    final ArrayList<Object> array = new ArrayList<>();;
+  }
+
+  static final class TokenMap {
+    final HashMap<String, Object> map = new HashMap<>();
+  }
+
+  private final ArrayList<Object> tokens = new ArrayList<>();
 
   public Map<String, Object> deserialize(@NotNull JsonObjectReader reader, @NotNull ILogger logger) throws IOException {
-    Map<String, Object> map = new HashMap<>();
+    final HashMap<String, Object> map = new HashMap<>();
 
     JsonToken token = reader.peek();
     switch (token) {
       case BEGIN_ARRAY:
         reader.beginArray();
-        currentArray = new ArrayList<>();
-        addCurrentToken(JsonToken.BEGIN_ARRAY);
+        addCurrentToken(new TokenArray());
         break;
       case END_ARRAY:
         reader.endArray();
-        if (currentArray != null) {
-          map.put(currentName, new ArrayList<>(currentArray));
-          currentArray.clear();
-        }
+
+        TokenArray tokenArray = (TokenArray) getCurrentToken();
         removeCurrentToken(); // Array
+        TokenName tokenName = (TokenName) getCurrentToken();
         removeCurrentToken(); // Name
+
+        if (tokenName != null && tokenArray != null) {
+          map.put(tokenName.name, tokenArray.array);
+        }
         break;
       case BEGIN_OBJECT:
         reader.beginObject();
@@ -44,54 +61,58 @@ public final class JsonObjectDeserializer {
         break;
       case END_OBJECT:
         reader.endObject();
-        removeCurrentToken();
+        removeCurrentToken(); // Map
+        removeCurrentToken(); // Name or Nothing
         break;
       case NAME:
-        currentName = reader.nextName();
-        addCurrentToken(JsonToken.NAME);
+        addCurrentToken(new TokenName(reader.nextName()));
         break;
       case STRING:
-        if (currentToken() == JsonToken.NAME) {
-          map.put(currentName, reader.nextString());
-          currentName = null;
-          removeCurrentToken();
-        } else if (currentToken() == JsonToken.BEGIN_ARRAY && currentArray != null) {
-          currentArray.add(reader.nextString());
+        if (getCurrentToken() instanceof TokenName) {
+          TokenName tokenNameString = (TokenName) getCurrentToken();
+          map.put(tokenNameString.name, reader.nextString());
+          removeCurrentToken(); // Name
+        } else if (getCurrentToken() instanceof TokenArray) {
+          TokenArray tokenArrayString = (TokenArray) getCurrentToken();
+          tokenArrayString.array.add(reader.nextString());
         }
         break;
       case NUMBER:
-        if (currentToken() == JsonToken.NAME) {
-          map.put(currentName, nextNumber(reader));
-          currentName = null;
-          removeCurrentToken();
-        } else if (currentToken() == JsonToken.BEGIN_ARRAY && currentArray != null) {
-          currentArray.add(nextNumber(reader));
+        if (getCurrentToken() instanceof TokenName) {
+          TokenName tokenNameNumber = (TokenName) getCurrentToken();
+          map.put(tokenNameNumber.name, nextNumber(reader));
+          removeCurrentToken(); // Name
+        } else if (getCurrentToken() instanceof TokenArray) {
+          TokenArray tokenArrayNumber = (TokenArray) getCurrentToken();
+          tokenArrayNumber.array.add(nextNumber(reader));
         }
         break;
       case BOOLEAN:
-        if (currentToken() == JsonToken.NAME) {
-          map.put(currentName, reader.nextBoolean());
-          currentName = null;
-          removeCurrentToken();
-        } else if (currentToken() == JsonToken.BEGIN_ARRAY && currentArray != null) {
-          currentArray.add(reader.nextBoolean());
+        if (getCurrentToken() instanceof TokenName) {
+          TokenName tokenNameBoolean = (TokenName) getCurrentToken();
+          map.put(tokenNameBoolean.name, reader.nextBoolean());
+          removeCurrentToken(); // Name
+        } else if (getCurrentToken() instanceof TokenArray) {
+          TokenArray tokenArrayBoolean = (TokenArray) getCurrentToken();
+          tokenArrayBoolean.array.add(reader.nextBoolean());
         }
         break;
       case NULL:
         reader.nextNull();
-        if (currentToken() == JsonToken.NAME) {
-          map.put(currentName, null);
-          currentName = null;
-          removeCurrentToken();
-        } else if (currentToken() == JsonToken.BEGIN_ARRAY && currentArray != null) {
-          currentArray.add(null);
+        if (getCurrentToken() instanceof TokenName) {
+          TokenName tokenNameNull = (TokenName) getCurrentToken();
+          map.put(tokenNameNull.name, null);
+          removeCurrentToken(); // Name
+        } else if (getCurrentToken() instanceof TokenArray) {
+          TokenArray tokenArrayNull = (TokenArray) getCurrentToken();
+          tokenArrayNull.array.add(null);
         }
         break;
       case END_DOCUMENT:
         break;
     }
 
-    if (currentToken() != null) {
+    if (getCurrentToken() != null) {
       map.putAll(deserialize(reader, logger));
     }
 
@@ -112,21 +133,21 @@ public final class JsonObjectDeserializer {
     return reader.nextLong();
   }
 
-  private @Nullable JsonToken currentToken() {
-    if (tokenStack.isEmpty()) {
+  private @Nullable Object getCurrentToken() {
+    if (tokens.isEmpty()) {
       return null;
     }
-    return tokenStack.get(tokenStack.size() - 1);
+    return tokens.get(tokens.size() - 1);
   }
 
-  private void addCurrentToken(JsonToken token) {
-    tokenStack.add(token);
+  private void addCurrentToken(Object token) {
+    tokens.add(token);
   }
 
   private void removeCurrentToken() {
-    if (tokenStack.isEmpty()) {
+    if (tokens.isEmpty()) {
       return;
     }
-    tokenStack.remove(tokenStack.size() - 1);
+    tokens.remove(tokens.size() - 1);
   }
 }
--- sentry/src/test/java/io/sentry/JsonObjectDeserializerTest.kt
+++ sentry/src/test/java/io/sentry/JsonObjectDeserializerTest.kt
@@ -20,57 +20,89 @@ class JsonObjectDeserializerTest {
     @Test
     fun `deserialize json string`() {
         val json = "{\"fixture-key\": \"fixture-value\"}"
-        val result = deserialize(json)
+        val actual = deserialize(json)
 
-        assertEquals(mapOf("fixture-key" to "fixture-value"), result)
+        assertEquals(mapOf("fixture-key" to "fixture-value"), actual)
     }
 
     @Test
     fun `deserialize json int`() {
         val json = "{\"fixture-key\": 123}"
-        val result = deserialize(json)
+        val actual = deserialize(json)
 
-        assertEquals(mapOf("fixture-key" to 123), result)
+        assertEquals(mapOf("fixture-key" to 123), actual)
     }
 
     @Test
     fun `deserialize json double`() {
         val json = "{\"fixture-key\": 123.321}"
-        val result = deserialize(json)
+        val actual = deserialize(json)
 
-        assertEquals(mapOf("fixture-key" to 123.321), result)
+        assertEquals(mapOf("fixture-key" to 123.321), actual)
     }
 
     @Test
     fun `deserialize json boolean`() {
         val json = "{\"fixture-key\": true}"
-        val result = deserialize(json)
+        val actual = deserialize(json)
 
-        assertEquals(mapOf("fixture-key" to true), result)
+        assertEquals(mapOf("fixture-key" to true), actual)
     }
 
     @Test
     fun `deserialize json string array`() {
         val json = "{\"fixture-key\":[\"fixture-entry-1\",\"fixture-entry-2\"]}"
-        val result = deserialize(json)
+        val actual = deserialize(json)
 
-        assertEquals(mapOf("fixture-key" to listOf("fixture-entry-1", "fixture-entry-2")), result)
+        assertEquals(mapOf("fixture-key" to listOf("fixture-entry-1", "fixture-entry-2")), actual)
     }
 
     @Test
     fun `deserialize json int array`() {
         val json = "{\"fixture-key\":[1,2]}"
-        val result = deserialize(json)
+        val actual = deserialize(json)
 
-        assertEquals(mapOf("fixture-key" to listOf(1, 2)), result)
+        assertEquals(mapOf("fixture-key" to listOf(1, 2)), actual)
     }
 
     @Test
     fun `deserialize json double array`() {
         val json = "{\"fixture-key\":[1.1,2.2]}"
-        val result = deserialize(json)
+        val actual = deserialize(json)
 
-        assertEquals(mapOf("fixture-key" to listOf(1.1, 2.2)), result)
+        assertEquals(mapOf("fixture-key" to listOf(1.1, 2.2)), actual)
+    }
+
+    @Test
+    fun `deserialize json object with nesting`() {
+        val json = """
+            "fixture-key": {
+                "string": "fixture-string",
+                "int": 123,
+                "double": 123.321,
+                "boolean": true,
+                "array": ["a", "b", "c"],
+                "object": {
+                    "key": "value"
+                },
+            }
+        """.trimIndent()
+
+        val expected = mapOf<String, Any>(
+            "fixture-key" to mapOf(
+                "string" to "fixture-string",
+                "int" to 123,
+                "double" to 123.321,
+                "boolean" to true,
+                "array" to listOf("a", "b", "c"),
+                "object" to mapOf(
+                    "key" to "value"
+                )
+            )
+        )
+
+        val actual = deserialize(json)
+        assertEquals(expected, actual)
     }
 
     // Helper
