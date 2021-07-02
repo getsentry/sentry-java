d3e50839c06432d27efc7137e2ea1dccabfb17e8
iker barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-07-02 17:09:09 +0200

    Increment `buildVersionCode` in the postrelease

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -12,12 +12,6 @@ echo $NEW_VERSION
 
 GRADLE_FILEPATH="gradle.properties"
 
-# Increment `buildVersionCode`
-VERSION_CODE_PATTERN="buildVersionCode"
-VERSION_NUMBER="$( awk "/$VERSION_CODE_PATTERN/" $GRADLE_FILEPATH | grep -o '[0-9]\+' )"
-((VERSION_NUMBER++))
-sed -i "" -e "s/$VERSION_CODE_PATTERN=.*$/$VERSION_CODE_PATTERN=$VERSION_NUMBER/g" $GRADLE_FILEPATH
-
 # Replace `versionName` with the given version
 VERSION_NAME_PATTERN="versionName"
 sed -i "" -e "s/$VERSION_NAME_PATTERN=.*$/$VERSION_NAME_PATTERN=$NEW_VERSION/g" $GRADLE_FILEPATH
--- scripts/post-release.sh
+++ scripts/post-release.sh
@@ -9,3 +9,9 @@ NEW_VERSION="${2}"
 
 # Add a new unreleased entry in the changelog
 sed -i "" 's/# Changelog/# Changelog\n\n## Unreleased/' CHANGELOG.md
+
+# Increment `buildVersionCode`
+VERSION_CODE_PATTERN="buildVersionCode"
+VERSION_NUMBER="$( awk "/$VERSION_CODE_PATTERN/" $GRADLE_FILEPATH | grep -o '[0-9]\+' )"
+((VERSION_NUMBER++))
+sed -ie "s/$VERSION_CODE_PATTERN=.*$/$VERSION_CODE_PATTERN=$VERSION_NUMBER/g" $GRADLE_FILEPATH
