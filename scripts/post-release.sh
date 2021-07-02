6d6246d1f6a4b61d3b1c324306f5cf2933ee31a9
iker barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-07-02 17:11:32 +0200

    Increment version name and make it a snapshot in postrelease

--- scripts/post-release.sh
+++ scripts/post-release.sh
@@ -15,3 +15,13 @@ VERSION_CODE_PATTERN="buildVersionCode"
 VERSION_NUMBER="$( awk "/$VERSION_CODE_PATTERN/" $GRADLE_FILEPATH | grep -o '[0-9]\+' )"
 ((VERSION_NUMBER++))
 sed -ie "s/$VERSION_CODE_PATTERN=.*$/$VERSION_CODE_PATTERN=$VERSION_NUMBER/g" $GRADLE_FILEPATH
+
+# Increment `versionName` and make it a snapshot
+VERSION_NAME_PATTERN="versionName"
+version="$( awk "/$VERSION_NAME_PATTERN/" $GRADLE_FILEPATH | egrep -o '[0-9].*$' )" # from the first digit until the end
+version_digit_to_bump="$( awk "/$VERSION_NAME_PATTERN/" $GRADLE_FILEPATH | egrep -o '[0-9]+$')"
+((version_digit_to_bump++))
+# Using `*` instead of `+` for compatibility. The result is the same,
+# since the version to be bumped is extracted using `+`.
+new_version="$( echo $version | sed "s/[0-9]*$/$version_digit_to_bump/g" )"
+sed -i "" -e "s/$VERSION_NAME_PATTERN=.*$/$VERSION_NAME_PATTERN=$new_version-SNAPSHOT/g" $GRADLE_FILEPATH
