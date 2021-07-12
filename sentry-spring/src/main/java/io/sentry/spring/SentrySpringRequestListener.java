0047ab4f097fadf7156c46a3ed1a5f292276b60d
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2021-07-12 12:19:26 +0200

    Cleanup.

--- sentry-spring/src/main/java/io/sentry/spring/SentrySpringRequestListener.java
+++ sentry-spring/src/main/java/io/sentry/spring/SentrySpringRequestListener.java
@@ -5,22 +5,14 @@ import io.sentry.Breadcrumb;
 import io.sentry.HubAdapter;
 import io.sentry.IHub;
 import io.sentry.util.Objects;
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import javax.servlet.ReadListener;
-import javax.servlet.ServletInputStream;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.core.Ordered;
+
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.core.Ordered;
-import org.springframework.util.StreamUtils;
+import java.io.IOException;
 
 /** Pushes new {@link io.sentry.Scope} on each incoming HTTP request. */
 @Open
@@ -87,69 +79,5 @@ public class SentrySpringRequestListener implements ServletRequestListener, Orde
   @Override
   public int getOrder() {
     return Ordered.HIGHEST_PRECEDENCE;
-  }
-
-  static final class CachedBodyHttpServletRequest extends HttpServletRequestWrapper {
-
-    private final byte[] cachedBody;
-
-    public CachedBodyHttpServletRequest(HttpServletRequest request) throws IOException {
-      super(request);
-      InputStream requestInputStream = request.getInputStream();
-      this.cachedBody = StreamUtils.copyToByteArray(requestInputStream);
-    }
-
-    @Override
-    public ServletInputStream getInputStream() {
-      return new CachedBodyServletInputStream(this.cachedBody);
-    }
-
-    @Override
-    public BufferedReader getReader() {
-      // Create a reader from cachedContent
-      // and return it
-      ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(this.cachedBody);
-      return new BufferedReader(
-          new InputStreamReader(byteArrayInputStream, StandardCharsets.UTF_8));
-    }
-  }
-
-  static final class CachedBodyServletInputStream extends ServletInputStream {
-
-    private final InputStream cachedBodyInputStream;
-
-    public CachedBodyServletInputStream(byte[] cachedBody) {
-      this.cachedBodyInputStream = new ByteArrayInputStream(cachedBody);
-    }
-
-    @Override
-    @SuppressWarnings("EmptyCatch")
-    public boolean isFinished() {
-      try {
-        return cachedBodyInputStream.available() == 0;
-      } catch (IOException e) {
-      }
-      return false;
-    }
-
-    @Override
-    public boolean isReady() {
-      return true;
-    }
-
-    @Override
-    public void setReadListener(ReadListener readListener) {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public int read() throws IOException {
-      return cachedBodyInputStream.read();
-    }
-
-    @Override
-    public int read(@NotNull byte[] b, int off, int len) throws IOException {
-      return cachedBodyInputStream.read(b, off, len);
-    }
   }
 }
