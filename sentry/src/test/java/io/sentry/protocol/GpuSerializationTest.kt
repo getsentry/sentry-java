67404fb5a5f5ef4c4fc8481b80b3fe5b974f2176
denrase <denrase@gmail.com>
2021-07-27 13:30:18 +0200

    implement gpu ser/deser

--- sentry/src/test/java/io/sentry/protocol/GpuSerializationTest.kt
+++ sentry/src/test/java/io/sentry/protocol/GpuSerializationTest.kt
@@ -11,29 +11,36 @@ import java.io.StringWriter
 import kotlin.test.assertEquals
 import org.junit.Test
 
-class BrowserSerializationTest {
+class GpuSerializationTest {
 
     private class Fixture {
         var logger: ILogger = mock()
 
-        fun getSut() = Browser().apply {
-            name = "e1c723db-7408-4043-baa7-f4e96234e5dc"
-            version = "724a48e9-2d35-416b-9f79-132beba2473a"
+        fun getSut() = Gpu().apply {
+            name = "d623a6b5-e1ab-4402-931b-c06f5a43a5ae"
+            id = -596576280
+            vendorId = 1874778041
+            vendorName = "d732cf76-07dc-48e2-8920-96d6bfc2439d"
+            memorySize = -1484004451
+            apiType = "95dfc8bc-88ae-4d66-b85f-6c88ad45b80f"
+            isMultiThreadedRendering = true
+            version = "3f3f73c3-83a2-423a-8a6f-bb3de0d4a6ae"
+            npotSupport = "e06b074a-463c-45de-a959-cbabd461d99d"
         }
     }
     private val fixture = Fixture()
 
     @Test
     fun serialize() {
-        val expected = sanitizedFile("gson/browser.json")
+        val expected = sanitizedFile("gson/gpu.json")
         val actual = serializeToString(fixture.getSut())
         assertEquals(expected, actual)
     }
 
     @Test
     fun deserialize() {
-        val expectedJson = sanitizedFile("gson/browser.json")
-        val actual = deserialize(expectedJson)
+        val expectedJson = sanitizedFile("gson/gpu.json")
+        val actual = deserializeBrowser(expectedJson)
         val actualJson = serializeToString(actual)
         assertEquals(expectedJson, actualJson)
     }
@@ -57,8 +64,8 @@ class BrowserSerializationTest {
         return wrt.toString()
     }
 
-    private fun deserialize(json: String): Browser {
+    private fun deserializeBrowser(json: String): Gpu {
         val reader = JsonObjectReader(StringReader(json))
-        return Browser.Deserializer().deserialize(reader, fixture.logger)
+        return Gpu.Deserializer().deserialize(reader, fixture.logger)
     }
 }
