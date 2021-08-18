7f51fbc697dc28cbd089b8106da68b53f0d13119
denrase <denrase@gmail.com>
2021-08-18 16:12:55 +0200

    add test json file and implement serialization interfaces

--- sentry/src/main/java/io/sentry/protocol/OperatingSystem.java
+++ sentry/src/main/java/io/sentry/protocol/OperatingSystem.java
@@ -1,15 +1,22 @@
 package io.sentry.protocol;
 
+import io.sentry.ILogger;
 import io.sentry.IUnknownPropertiesConsumer;
+import io.sentry.JsonDeserializer;
+import io.sentry.JsonObjectReader;
+import io.sentry.JsonObjectWriter;
+import io.sentry.JsonSerializable;
+import io.sentry.JsonUnknown;
 import io.sentry.util.CollectionUtils;
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.TestOnly;
 
-public final class OperatingSystem implements IUnknownPropertiesConsumer {
+public final class OperatingSystem
+    implements IUnknownPropertiesConsumer, JsonUnknown, JsonSerializable {
   public static final String TYPE = "os";
 
   /** Name of the operating system. */
@@ -98,15 +105,99 @@ public final class OperatingSystem implements IUnknownPropertiesConsumer {
     this.rooted = rooted;
   }
 
-  @TestOnly
-  @Nullable
-  Map<String, Object> getUnknown() {
-    return unknown;
-  }
-
   @ApiStatus.Internal
   @Override
   public void acceptUnknownProperties(final @NotNull Map<String, Object> unknown) {
     this.unknown = new ConcurrentHashMap<>(unknown);
+  }
+
+  // JsonSerializable
+
+  public static final class JsonKeys {
+    public static final String NAME = "name";
+    public static final String VERSION = "version";
+    public static final String RAW_DESCRIPTION = "raw_description";
+    public static final String BUILD = "build";
+    public static final String KERNEL_VERSION = "kernel_version";
+    public static final String ROOTED = "rooted";
+  }
+
+  @Override
+  public void serialize(@NotNull JsonObjectWriter writer, @NotNull ILogger logger)
+      throws IOException {
+    writer.beginObject();
+    if (name != null) {
+      writer.name(JsonKeys.NAME).value(name);
+    }
+    if (version != null) {
+      writer.name(JsonKeys.VERSION).value(version);
+    }
+    if (rawDescription != null) {
+      writer.name(JsonKeys.RAW_DESCRIPTION).value(rawDescription);
+    }
+    if (build != null) {
+      writer.name(JsonKeys.BUILD).value(build);
+    }
+    if (kernelVersion != null) {
+      writer.name(JsonKeys.KERNEL_VERSION).value(kernelVersion);
+    }
+    if (rooted != null) {
+      writer.name(JsonKeys.ROOTED).value(rooted);
+    }
+    writer.endObject();
+  }
+
+  @Nullable
+  @Override
+  public Map<String, Object> getUnknown() {
+    return unknown;
+  }
+
+  @Override
+  public void setUnknown(@Nullable Map<String, Object> unknown) {
+    this.unknown = unknown;
+  }
+
+  public static final class Deserializer implements JsonDeserializer<OperatingSystem> {
+
+    @Override
+    public @NotNull OperatingSystem deserialize(
+        @NotNull JsonObjectReader reader, @NotNull ILogger logger) throws Exception {
+      reader.beginObject();
+      OperatingSystem operatingSystem = new OperatingSystem();
+      Map<String, Object> unknown = null;
+      do {
+        final String nextName = reader.nextName();
+        switch (nextName) {
+          case JsonKeys.NAME:
+            operatingSystem.name = reader.nextStringOrNull();
+            break;
+          case JsonKeys.VERSION:
+            operatingSystem.version = reader.nextStringOrNull();
+            break;
+          case JsonKeys.RAW_DESCRIPTION:
+            operatingSystem.rawDescription = reader.nextStringOrNull();
+            break;
+          case JsonKeys.BUILD:
+            operatingSystem.build = reader.nextStringOrNull();
+            break;
+          case JsonKeys.KERNEL_VERSION:
+            operatingSystem.kernelVersion = reader.nextStringOrNull();
+            break;
+          case JsonKeys.ROOTED:
+            operatingSystem.rooted = reader.nextBooleanOrNull();
+            break;
+          default:
+            if (unknown == null) {
+              unknown = new ConcurrentHashMap<>();
+            }
+            reader.nextUnknown(logger, unknown, nextName);
+            break;
+        }
+      } while (reader.hasNext());
+      operatingSystem.setUnknown(unknown);
+      reader.endObject();
+      return operatingSystem;
+    }
   }
 }
