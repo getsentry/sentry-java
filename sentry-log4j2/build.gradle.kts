85b0677622a0f38b38994131bb2898462bee53ed
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2021-08-19 09:09:13 +0200

    Remove unused dependencies.

--- sentry-log4j2/build.gradle.kts
+++ sentry-log4j2/build.gradle.kts
@@ -36,7 +36,6 @@ dependencies {
     testImplementation(kotlin(Config.kotlinStdLib))
     testImplementation(Config.TestLibs.kotlinTestJunit)
     testImplementation(Config.TestLibs.mockitoKotlin)
-    testImplementation(Config.TestLibs.awaitility)
 }
 
 configure<SourceSetContainer> {
