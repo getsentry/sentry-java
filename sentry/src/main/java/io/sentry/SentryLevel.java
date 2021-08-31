5c9daa479b96eeac4fb50851313d120a21b889aa
denrase <denrase@gmail.com>
2021-08-31 11:20:07 +0200

    breadcrumb serialization

--- sentry/src/main/java/io/sentry/SentryLevel.java
+++ sentry/src/main/java/io/sentry/SentryLevel.java
@@ -1,10 +1,29 @@
 package io.sentry;
 
+import java.io.IOException;
+import java.util.Locale;
+import org.jetbrains.annotations.NotNull;
+
 /** the SentryLevel */
-public enum SentryLevel {
+public enum SentryLevel implements JsonSerializable {
   DEBUG,
   INFO,
   WARNING,
   ERROR,
-  FATAL
+  FATAL;
+
+  @Override
+  public void serialize(@NotNull JsonObjectWriter writer, @NotNull ILogger logger)
+      throws IOException {
+    writer.value(name().toLowerCase(Locale.ROOT));
+  }
+
+  static final class Deserializer implements JsonDeserializer<SentryLevel> {
+
+    @Override
+    public @NotNull SentryLevel deserialize(
+        @NotNull JsonObjectReader reader, @NotNull ILogger logger) throws Exception {
+      return SentryLevel.valueOf(reader.nextString().toUpperCase(Locale.ROOT));
+    }
+  }
 }
