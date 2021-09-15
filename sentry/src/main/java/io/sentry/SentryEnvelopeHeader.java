8910397a087aeaafd9eb30ef4f4d00626c42dd92
denrase <denrase@gmail.com>
2021-09-15 16:53:14 +0200

    implement serialization for sentry envelope headers

--- sentry/src/main/java/io/sentry/SentryEnvelopeHeader.java
+++ sentry/src/main/java/io/sentry/SentryEnvelopeHeader.java
@@ -2,11 +2,14 @@ package io.sentry;
 
 import io.sentry.protocol.SdkVersion;
 import io.sentry.protocol.SentryId;
+import io.sentry.vendor.gson.stream.JsonToken;
+import java.io.IOException;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @ApiStatus.Internal
-public final class SentryEnvelopeHeader {
+public final class SentryEnvelopeHeader implements JsonSerializable {
   // Event Id must be set if the envelope holds an event, or an item that is related to the event
   // (e.g: attachments, user feedback)
   private final @Nullable SentryId eventId;
@@ -33,5 +36,53 @@ public final class SentryEnvelopeHeader {
 
   public @Nullable SdkVersion getSdkVersion() {
     return sdkVersion;
+  }
+
+  // JsonSerializable
+
+  public static final class JsonKeys {
+    public static final String EVENT_ID = "event_id";
+    public static final String SDK = "sdk";
+  }
+
+  @Override
+  public void serialize(@NotNull JsonObjectWriter writer, @NotNull ILogger logger)
+      throws IOException {
+    writer.beginObject();
+    if (eventId != null) {
+      writer.name(JsonKeys.EVENT_ID).value(logger, eventId);
+    }
+    if (sdkVersion != null) {
+      writer.name(JsonKeys.SDK).value(logger, sdkVersion);
+    }
+    writer.endObject();
+  }
+
+  public static final class Deserializer implements JsonDeserializer<SentryEnvelopeHeader> {
+    @Override
+    public @NotNull SentryEnvelopeHeader deserialize(
+        @NotNull JsonObjectReader reader, @NotNull ILogger logger) throws Exception {
+      reader.beginObject();
+
+      SentryId eventId = null;
+      SdkVersion sdkVersion = null;
+
+      while (reader.peek() == JsonToken.NAME) {
+        final String nextName = reader.nextName();
+        switch (nextName) {
+          case JsonKeys.EVENT_ID:
+            eventId = new SentryId.Deserializer().deserialize(reader, logger);
+            break;
+          case JsonKeys.SDK:
+            sdkVersion = new SdkVersion.Deserializer().deserialize(reader, logger);
+            break;
+          default:
+            break;
+        }
+      }
+      SentryEnvelopeHeader sentryEnvelopeHeader = new SentryEnvelopeHeader(eventId, sdkVersion);
+      reader.endObject();
+      return sentryEnvelopeHeader;
+    }
   }
 }
--- sentry/src/main/java/io/sentry/SentryEnvelopeItemHeader.java
+++ sentry/src/main/java/io/sentry/SentryEnvelopeItemHeader.java
@@ -1,13 +1,15 @@
 package io.sentry;
 
 import io.sentry.util.Objects;
+import io.sentry.vendor.gson.stream.JsonToken;
+import java.io.IOException;
 import java.util.concurrent.Callable;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @ApiStatus.Internal
-public final class SentryEnvelopeItemHeader {
+public final class SentryEnvelopeItemHeader implements JsonSerializable {
 
   private final @Nullable String contentType;
   private final @Nullable String fileName;
@@ -39,7 +41,8 @@ public final class SentryEnvelopeItemHeader {
     return fileName;
   }
 
-  SentryEnvelopeItemHeader(
+  @ApiStatus.Internal
+  public SentryEnvelopeItemHeader(
       final @NotNull SentryItemType type,
       int length,
       final @Nullable String contentType,
@@ -82,5 +85,85 @@ public final class SentryEnvelopeItemHeader {
    */
   public @Nullable String getAttachmentType() {
     return attachmentType;
+  }
+
+  // JsonSerializable
+
+  public static final class JsonKeys {
+    public static final String CONTENT_TYPE = "content_type";
+    public static final String FILENAME = "filename";
+    public static final String TYPE = "type";
+    public static final String ATTACHMENT_TYPE = "attachment_type";
+    public static final String LENGTH = "length";
+  }
+
+  @Override
+  public void serialize(@NotNull JsonObjectWriter writer, @NotNull ILogger logger)
+      throws IOException {
+    writer.beginObject();
+    if (contentType != null) {
+      writer.name(JsonKeys.CONTENT_TYPE).value(contentType);
+    }
+    if (fileName != null) {
+      writer.name(JsonKeys.FILENAME).value(fileName);
+    }
+    writer.name(JsonKeys.TYPE).value(logger, type);
+    if (attachmentType != null) {
+      writer.name(JsonKeys.ATTACHMENT_TYPE).value(attachmentType);
+    }
+    writer.name(JsonKeys.LENGTH).value(length);
+    writer.endObject();
+  }
+
+  public static final class Deserializer implements JsonDeserializer<SentryEnvelopeItemHeader> {
+    @Override
+    public @NotNull SentryEnvelopeItemHeader deserialize(
+        @NotNull JsonObjectReader reader, @NotNull ILogger logger) throws Exception {
+      reader.beginObject();
+
+      String contentType = null;
+      String fileName = null;
+      SentryItemType type = null;
+      int length = 0;
+      String attachmentType = null;
+
+      while (reader.peek() == JsonToken.NAME) {
+        final String nextName = reader.nextName();
+        switch (nextName) {
+          case JsonKeys.CONTENT_TYPE:
+            contentType = reader.nextStringOrNull();
+            break;
+          case JsonKeys.FILENAME:
+            fileName = reader.nextStringOrNull();
+            break;
+          case JsonKeys.TYPE:
+            type = new SentryItemType.Deserializer().deserialize(reader, logger);
+            break;
+          case JsonKeys.LENGTH:
+            length = reader.nextInt();
+            break;
+          case JsonKeys.ATTACHMENT_TYPE:
+            attachmentType = reader.nextStringOrNull();
+            break;
+          default:
+            break;
+        }
+      }
+      if (type == null) {
+        throw missingRequiredFieldException(JsonKeys.TYPE, logger);
+      }
+      SentryEnvelopeItemHeader sentryEnvelopeHeader =
+          new SentryEnvelopeItemHeader(type, length, contentType, fileName, attachmentType);
+      reader.endObject();
+      return sentryEnvelopeHeader;
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private Exception missingRequiredFieldException(String field, ILogger logger) {
+      String message = "Missing required field \"" + field + "\"";
+      Exception exception = new IllegalStateException(message);
+      logger.log(SentryLevel.ERROR, message, exception);
+      return exception;
+    }
   }
 }
