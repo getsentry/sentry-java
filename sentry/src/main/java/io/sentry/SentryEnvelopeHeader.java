e624de709d9dbe7922820c266a623bcf81a9e97d
Denis Andrašec <denrase@gmail.com>
2021-09-21 16:49:08 +0200

    Vendor Json: SentryTransaction (#1720)

--- sentry/src/main/java/io/sentry/SentryEnvelopeHeader.java
+++ sentry/src/main/java/io/sentry/SentryEnvelopeHeader.java
@@ -2,16 +2,23 @@ package io.sentry;
 
 import io.sentry.protocol.SdkVersion;
 import io.sentry.protocol.SentryId;
+import io.sentry.vendor.gson.stream.JsonToken;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @ApiStatus.Internal
-public final class SentryEnvelopeHeader {
+public final class SentryEnvelopeHeader implements JsonSerializable, JsonUnknown {
   // Event Id must be set if the envelope holds an event, or an item that is related to the event
   // (e.g: attachments, user feedback)
   private final @Nullable SentryId eventId;
 
   private final @Nullable SdkVersion sdkVersion;
+
+  private @Nullable Map<String, Object> unknown;
 
   public SentryEnvelopeHeader(
       final @Nullable SentryId eventId, final @Nullable SdkVersion sdkVersion) {
@@ -33,5 +40,79 @@ public final class SentryEnvelopeHeader {
 
   public @Nullable SdkVersion getSdkVersion() {
     return sdkVersion;
+  }
+
+  // JsonSerializable
+
+  public static final class JsonKeys {
+    public static final String EVENT_ID = "event_id";
+    public static final String SDK = "sdk";
+  }
+
+  @Override
+  public void serialize(@NotNull JsonObjectWriter writer, @NotNull ILogger logger)
+      throws IOException {
+    writer.beginObject();
+    if (eventId != null) {
+      writer.name(JsonKeys.EVENT_ID).value(logger, eventId);
+    }
+    if (sdkVersion != null) {
+      writer.name(JsonKeys.SDK).value(logger, sdkVersion);
+    }
+    if (unknown != null) {
+      for (String key : unknown.keySet()) {
+        Object value = unknown.get(key);
+        writer.name(key);
+        writer.value(logger, value);
+      }
+    }
+    writer.endObject();
+  }
+
+  public static final class Deserializer implements JsonDeserializer<SentryEnvelopeHeader> {
+    @Override
+    public @NotNull SentryEnvelopeHeader deserialize(
+        @NotNull JsonObjectReader reader, @NotNull ILogger logger) throws Exception {
+      reader.beginObject();
+
+      SentryId eventId = null;
+      SdkVersion sdkVersion = null;
+      Map<String, Object> unknown = null;
+
+      while (reader.peek() == JsonToken.NAME) {
+        final String nextName = reader.nextName();
+        switch (nextName) {
+          case JsonKeys.EVENT_ID:
+            eventId = new SentryId.Deserializer().deserialize(reader, logger);
+            break;
+          case JsonKeys.SDK:
+            sdkVersion = new SdkVersion.Deserializer().deserialize(reader, logger);
+            break;
+          default:
+            if (unknown == null) {
+              unknown = new HashMap<>();
+            }
+            reader.nextUnknown(logger, unknown, nextName);
+            break;
+        }
+      }
+      SentryEnvelopeHeader sentryEnvelopeHeader = new SentryEnvelopeHeader(eventId, sdkVersion);
+      sentryEnvelopeHeader.setUnknown(unknown);
+      reader.endObject();
+      return sentryEnvelopeHeader;
+    }
+  }
+
+  // JsonUnknown
+
+  @Nullable
+  @Override
+  public Map<String, Object> getUnknown() {
+    return unknown;
+  }
+
+  @Override
+  public void setUnknown(@Nullable Map<String, Object> unknown) {
+    this.unknown = unknown;
   }
 }
---
+++ sentry/src/test/java/io/sentry/protocol/SentrySpanSerializationTest.kt
@@ -0,0 +1,71 @@
+package io.sentry.protocol
+
+import com.nhaarman.mockitokotlin2.mock
+import io.sentry.DateUtils
+import io.sentry.FileFromResources
+import io.sentry.ILogger
+import io.sentry.JsonObjectReader
+import io.sentry.JsonObjectWriter
+import io.sentry.JsonSerializable
+import io.sentry.SpanId
+import io.sentry.SpanStatus
+import java.io.StringReader
+import java.io.StringWriter
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class SentrySpanSerializationTest {
+
+    class Fixture {
+        val logger = mock<ILogger>()
+
+        fun getSut() = SentrySpan(
+            DateUtils.getDateTime("1953-11-21T02:06:08.000Z"),
+            DateUtils.getDateTime("1937-04-10T18:24:03.000Z"),
+            SentryId("5b1f73d39486827b9e60ceb1fc23277a"),
+            SpanId("4584593a-5d9b-4a55-931f-cfe89c93907d"),
+            SpanId("57518091-aed1-47a6-badf-11696035b5f4"),
+            "42e6bd1a-c45e-414d-8066-ed5196fbc686",
+            "b026345e-ecd1-4555-8d6c-cd6d9f865c89",
+            SpanStatus.ALREADY_EXISTS,
+            mapOf("f1333f3a-916a-47b7-8dd6-d6d15fa96e03" to "d4a07684-5b3e-4d08-b605-f9364c398124"),
+            mapOf("518276a7-88d7-408f-ab36-af342f2d7715" to "4a1c2d6c-3f49-41cc-b2ca-d1b36f7ea5a6")
+        )
+    }
+    private val fixture = Fixture()
+
+    @Test
+    fun serialize() {
+        val expected = sanitizedFile("gson/sentry_span.json")
+        val actual = serialize(fixture.getSut())
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun deserialize() {
+        val expectedJson = sanitizedFile("gson/sentry_span.json")
+        val actual = deserialize(expectedJson)
+        val actualJson = serialize(actual)
+        assertEquals(expectedJson, actualJson)
+    }
+
+    // Helper
+
+    private fun sanitizedFile(path: String): String {
+        return FileFromResources.invoke(path)
+            .replace(Regex("[\n\r]"), "")
+            .replace(" ", "")
+    }
+
+    private fun serialize(jsonSerializable: JsonSerializable): String {
+        val wrt = StringWriter()
+        val jsonWrt = JsonObjectWriter(wrt)
+        jsonSerializable.serialize(jsonWrt, fixture.logger)
+        return wrt.toString()
+    }
+
+    private fun deserialize(json: String): SentrySpan {
+        val reader = JsonObjectReader(StringReader(json))
+        return SentrySpan.Deserializer().deserialize(reader, fixture.logger)
+    }
+}
