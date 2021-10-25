e861e57c40ed793fe6744da4502cd4679028df62
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2021-10-25 11:26:08 +0200

    Add graphql exception handler.

---
+++ sentry-graphql-java/src/test/kotlin/io/sentry/graphql/java/SentryDataFetcherExceptionHandlerTest.kt
@@ -0,0 +1,25 @@
+package io.sentry.graphql.java
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import graphql.execution.DataFetcherExceptionHandler
+import graphql.execution.DataFetcherExceptionHandlerParameters
+import io.sentry.IHub
+import kotlin.test.Test
+
+class SentryDataFetcherExceptionHandlerTest {
+
+    @Test
+    fun `passes exception to Sentry and invokes delegate`() {
+        val hub = mock<IHub>()
+        val delegate = mock<DataFetcherExceptionHandler>()
+        val handler = SentryDataFetcherExceptionHandler(hub, delegate)
+
+        val exception = RuntimeException()
+        val parameters = DataFetcherExceptionHandlerParameters.newExceptionParameters().exception(exception).build()
+        handler.onException(parameters)
+
+        verify(hub).captureException(exception)
+        verify(delegate).onException(parameters)
+    }
+}
