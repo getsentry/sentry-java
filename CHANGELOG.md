0eb650523d428e41aa74688fea4dde10b94fb0ce
Roman Zavarnitsyn <rom4ek93@gmail.com>
2021-12-14 08:20:31 +0100

    feat: Add runtime classes for FileReader/FileWriter instrumentation (#1843)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@
 * Feat: Add locale to device context and deprecate language (#1832)
 * Ref: change `java.util.Random` to `java.security.SecureRandom` for possible security reasons (#1831)
 * Feat: Add `SentryFileInputStream` and `SentryFileOutputStream` for File I/O performance instrumentation (#1826)
+- Feat: Add `SentryFileReader` and `SentryFileWriter` for File I/O instrumentation (#1843)
 
 ## 5.4.3
 
---
+++ sentry/src/test/java/io/sentry/instrumentation/file/SentryFileWriterTest.kt
@@ -0,0 +1,55 @@
+package io.sentry.instrumentation.file
+
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import io.sentry.IHub
+import io.sentry.SentryOptions
+import io.sentry.SentryTracer
+import io.sentry.SpanStatus.OK
+import io.sentry.TransactionContext
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import java.io.File
+import kotlin.test.assertEquals
+
+class SentryFileWriterTest {
+    class Fixture {
+        val hub = mock<IHub>()
+        val sentryTracer = SentryTracer(TransactionContext("name", "op"), hub)
+
+        internal fun getSut(
+            tmpFile: File,
+            activeTransaction: Boolean = true,
+        ): SentryFileWriter {
+            whenever(hub.options).thenReturn(SentryOptions())
+            if (activeTransaction) {
+                whenever(hub.span).thenReturn(sentryTracer)
+            }
+            return SentryFileWriter(tmpFile, hub)
+        }
+    }
+
+    @get:Rule
+    val tmpDir = TemporaryFolder()
+
+    private val fixture = Fixture()
+
+    private val tmpFile: File get() = tmpDir.newFile("test.txt")
+
+    @Test
+    fun `captures a span`() {
+        val writer = fixture.getSut(tmpFile)
+        writer.write("TEXT")
+        writer.close()
+
+        assertEquals(fixture.sentryTracer.children.size, 1)
+        val fileIOSpan = fixture.sentryTracer.children.first()
+        assertEquals(fileIOSpan.spanContext.operation, "file.write")
+        assertEquals(fileIOSpan.spanContext.description, "test.txt (4 B)")
+        assertEquals(fileIOSpan.data["file.size"], 4L)
+        assertEquals(fileIOSpan.throwable, null)
+        assertEquals(fileIOSpan.isFinished, true)
+        assertEquals(fileIOSpan.status, OK)
+    }
+}
