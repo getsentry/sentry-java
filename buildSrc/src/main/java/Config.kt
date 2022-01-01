6292942ce475e619ca4381967e9e325c4f8d6d85
marko asplund <marko.asplund@wolt.com>
2022-01-01 17:28:34 +0200

    Fix security issue SNYK-JAVA-COMGOOGLECODEGSON-1730327 by upgrading gson lib

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -45,7 +45,7 @@ object Config {
         val okhttpBom = "com.squareup.okhttp3:okhttp-bom:$okHttpVersion"
         val okhttp = "com.squareup.okhttp3:okhttp"
         // only bump gson if https://github.com/google/gson/issues/1597 is fixed
-        private val gsonVersion = "2.8.5"
+        private val gsonVersion = "2.8.9"
         val gsonDep = "com.google.code.gson:gson"
         val gson = "$gsonDep:$gsonVersion"
         val leakCanary = "com.squareup.leakcanary:leakcanary-android:2.7"
