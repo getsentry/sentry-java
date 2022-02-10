33aeeb081e38aac8da02fd4101b6dfeb25d424ce
Kamil Ogórek <kamil.ogorek@gmail.com>
2022-02-10 16:18:53 +0100

    ref: Use perl instead of sed for craft scripts (#1904)

--- scripts/post-release.sh
+++ scripts/post-release.sh
@@ -15,7 +15,7 @@ git checkout main
 GRADLE_FILEPATH="gradle.properties"
 
 # Add a new unreleased entry in the changelog
-sed -i "" 's/# Changelog/# Changelog\n\n## Unreleased/' CHANGELOG.md
+perl -pi -e 's/# Changelog/# Changelog\n\n## Unreleased/' CHANGELOG.md
 
 # Increment `versionName` and make it a snapshot
 # Incrementing the version name before the release (`bump-version.sh`) sets a
@@ -32,7 +32,7 @@ version_digit_to_bump="$( awk "/$VERSION_NAME_PATTERN/" $GRADLE_FILEPATH | egrep
 # Using `*` instead of `+` for compatibility. The result is the same,
 # since the version to be bumped is extracted using `+`.
 new_version="$( echo $version | sed "s/[0-9]*$/$version_digit_to_bump/g" )"
-sed -i "" -e "s/$VERSION_NAME_PATTERN=.*$/$VERSION_NAME_PATTERN=$new_version-SNAPSHOT/g" $GRADLE_FILEPATH
+perl -pi -e "s/$VERSION_NAME_PATTERN=.*$/$VERSION_NAME_PATTERN=$new_version-SNAPSHOT/g" $GRADLE_FILEPATH
 
 git add .
 git commit -m "Prepare $new_version"
