710290fcd66585fc8788d25bcb36257d6e34c2c6
Kamil Ogórek <kamil.ogorek@gmail.com>
2022-02-14 14:58:14 +0100

    ci: Bump craft and update post-release script (#1910)

--- scripts/post-release.sh
+++ scripts/post-release.sh
@@ -5,12 +5,6 @@
 
 set -eux
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $SCRIPT_DIR/..
-
-OLD_VERSION="${1}"
-NEW_VERSION="${2}"
-
 git checkout main
 GRADLE_FILEPATH="gradle.properties"
 
