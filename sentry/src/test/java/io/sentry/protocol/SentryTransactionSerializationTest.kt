142b83eb772761db959bf8f61bfc15189d8d42b0
Alexander Dinauer <alexander.dinauer@sentry.io>
2022-09-07 14:57:09 +0200

    Fix test

--- sentry/src/test/java/io/sentry/protocol/SentryTransactionSerializationTest.kt
+++ sentry/src/test/java/io/sentry/protocol/SentryTransactionSerializationTest.kt
@@ -27,7 +27,8 @@ class SentryTransactionSerializationTest {
             mapOf(
                 "386384cb-1162-49e7-aea1-db913d4fca63" to MeasurementValueSerializationTest.Fixture().getSut(),
                 "186384cb-1162-49e7-aea1-db913d4fca63" to MeasurementValueSerializationTest.Fixture().getSut(0.4000000059604645f, "test2")
-            )
+            ),
+            TransactionInfo(TransactionNameSource.CUSTOM.apiName())
         ).apply {
             SentryBaseEventSerializationTest.Fixture().update(this)
         }
