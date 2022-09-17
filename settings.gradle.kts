429f8787ddd65f3c811e06bf867708370e0209ad
Ivan Dlugos <6349682+vaind@users.noreply.github.com>
2022-09-17 19:19:07 +0200

    CI: performance impact measurements (#2171)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -41,7 +41,9 @@ include(
     "sentry-samples:sentry-samples-spring-boot-webflux",
     "sentry-samples:sentry-samples-netflix-dgs",
     "sentry-android-integration-tests:sentry-uitest-android-benchmark",
-    "sentry-android-integration-tests:sentry-uitest-android"
+    "sentry-android-integration-tests:sentry-uitest-android",
+    "sentry-android-integration-tests:test-app-plain",
+    "sentry-android-integration-tests:test-app-sentry",
 )
 
 gradle.beforeProject {
