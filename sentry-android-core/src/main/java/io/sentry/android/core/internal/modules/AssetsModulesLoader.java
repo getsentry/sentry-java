25e6b110627d6dd1820ff042a0e9b9ba4e7e3fde
Roman Zavarnitsyn <rom4ek93@gmail.com>
2022-10-31 17:44:03 +0100

    Common logic into abstract ModulesLoader

--- sentry-android-core/src/main/java/io/sentry/android/core/internal/modules/AssetsModulesLoader.java
+++ sentry-android-core/src/main/java/io/sentry/android/core/internal/modules/AssetsModulesLoader.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import io.sentry.ILogger;
 import io.sentry.SentryLevel;
 import io.sentry.internal.modules.IModulesLoader;
+import io.sentry.internal.modules.ModulesLoader;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -18,54 +19,28 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @ApiStatus.Internal
-public final class AssetsModulesLoader implements IModulesLoader {
+public final class AssetsModulesLoader extends ModulesLoader {
 
-  private @Nullable Map<String, String> cachedModules = null;
-  private final @NotNull ILogger logger;
   private final @NotNull WeakReference<Context> contextRef;
 
   public AssetsModulesLoader(final @NotNull Context context, final @NotNull ILogger logger) {
-    this.logger = logger;
+    super(logger);
     this.contextRef = new WeakReference<>(context);
   }
 
-  @Override public @Nullable Map<String, String> getOrLoadModules() {
-    if (cachedModules != null) {
-      return cachedModules;
-    }
-    cachedModules = extractModules();
-    return cachedModules;
-  }
-
-  @SuppressWarnings("CharsetObjectCanBeUsed")
-  private Map<String, String> extractModules() {
+  @Override
+  protected Map<String, String> loadModules() {
     final Map<String, String> modules = new TreeMap<>();
-    try {
-      final Context context = contextRef.get();
-      if (context == null) {
-        return modules;
-      }
+    final Context context = contextRef.get();
+    if (context == null) {
+      return modules;
+    }
 
-      try (final BufferedReader reader =
-             new BufferedReader(new InputStreamReader(context.getAssets().open("sentry-external-modules.txt"), Charset.forName("UTF-8")))) {
-        String module = reader.readLine();
-        while (module != null) {
-          int sep = module.lastIndexOf(':');
-          final String group = module.substring(0, sep);
-          final String version = module.substring(sep + 1);
-          modules.put(group, version);
-          module = reader.readLine();
-        }
-        logger.log(SentryLevel.DEBUG, "Extracted %d modules from resources.", modules.size());
-      } catch (FileNotFoundException e) {
-        logger.log(SentryLevel.INFO, "sentry-external-modules.txt file was not found.");
-      } catch (IOException e) {
-        logger.log(SentryLevel.ERROR, "Error extracting modules.", e);
-      } catch (RuntimeException e) {
-        logger.log(SentryLevel.ERROR, "sentry-external-modules.txt file is malformed.", e);
-      }
-    } catch (SecurityException e) {
-      logger.log(SentryLevel.INFO, "Access to resources denied.", e);
+    try {
+      final InputStream stream = context.getAssets().open(EXTERNAL_MODULES_FILENAME);
+      return parseStream(stream);
+    } catch (IOException e) {
+      logger.log(SentryLevel.ERROR, "Error extracting modules.", e);
     }
     return modules;
   }
