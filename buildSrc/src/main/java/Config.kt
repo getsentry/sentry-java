00fa357d2a5734f5a9586100ca425774dfd1e233
Alexander Dinauer <alexander.dinauer@sentry.io>
2022-11-22 06:44:46 +0100

    Bump Spring Boot to 2.7.5

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -4,7 +4,7 @@ object Config {
     val kotlinVersion = "1.6.10"
     val kotlinStdLib = "stdlib-jdk8"
 
-    val springBootVersion = "2.7.4"
+    val springBootVersion = "2.7.5"
     val springBoot3Version = "3.0.0-RC2"
     val kotlinCompatibleLanguageVersion = "1.4"
 
