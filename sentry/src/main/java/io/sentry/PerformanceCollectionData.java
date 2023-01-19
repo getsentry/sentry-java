23b2e36cc70218cbd61a8e5a7006a800e99dac24
stefanosiano <stefano.siano@sentry.io>
2023-01-19 11:15:55 +0100

    added some javadoc

--- sentry/src/main/java/io/sentry/PerformanceCollectionData.java
+++ sentry/src/main/java/io/sentry/PerformanceCollectionData.java
@@ -13,18 +13,27 @@ public final class PerformanceCollectionData {
   private @Nullable MemoryCollectionData uncommittedMemoryData = null;
   private @Nullable CpuCollectionData uncommittedCpuData = null;
 
+  /**
+   * Add a {@link io.sentry.MemoryCollectionData} to internal uncommitted data. To save the data
+   * call {@code commitData}. Only the last uncommitted memory data will be retained.
+   */
   public void addMemoryData(final @Nullable MemoryCollectionData memoryCollectionData) {
     if (memoryCollectionData != null) {
       uncommittedMemoryData = memoryCollectionData;
     }
   }
 
+  /**
+   * Add a {@link io.sentry.CpuCollectionData} to internal uncommitted data. To save the data call
+   * {@code commitData()}. Only the last uncommitted cpu data will be retained.
+   */
   public void addCpuData(final @Nullable CpuCollectionData cpuCollectionData) {
     if (cpuCollectionData != null) {
       uncommittedCpuData = cpuCollectionData;
     }
   }
 
+  /** Save any uncommitted data. */
   public void commitData() {
     if (uncommittedMemoryData != null) {
       memoryData.add(uncommittedMemoryData);
