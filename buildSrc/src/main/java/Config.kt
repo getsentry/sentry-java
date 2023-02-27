0d32bde0bd8c901d10f9bc74eaa6ff2b3daf645a
Roman Zavarnitsyn <rom4ek93@gmail.com>
2023-02-27 11:53:15 +0100

    Bump AGP versions

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -1,7 +1,7 @@
 import java.math.BigDecimal
 
 object Config {
-    val AGP = System.getenv("VERSION_AGP") ?: "7.3.0"
+    val AGP = System.getenv("VERSION_AGP") ?: "7.4.1"
     val kotlinVersion = "1.6.10"
     val kotlinStdLib = "stdlib-jdk8"
 
