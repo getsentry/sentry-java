a10eeb716d62f71bebf2748ddee347446c55e1c1
Alexander Dinauer <alexander.dinauer@sentry.io>
2023-03-01 15:43:37 +0100

    Fix memory leak in Webflux (Spring Boot 2)

--- sentry-spring-jakarta/src/main/java/io/sentry/spring/jakarta/webflux/SentryWebFilterWithThreadLocalAccessor.java
+++ sentry-spring-jakarta/src/main/java/io/sentry/spring/jakarta/webflux/SentryWebFilterWithThreadLocalAccessor.java
@@ -6,11 +6,13 @@ import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilterChain;
 
 import io.sentry.IHub;
+import io.sentry.NoOpHub;
+import io.sentry.Sentry;
 import reactor.core.publisher.Mono;
 
 /** Manages {@link io.sentry.Scope} in Webflux request processing. */
 @ApiStatus.Experimental
-public final class SentryWebFilterWithThreadLocalAccessor extends SentryWebFilter {
+public final class SentryWebFilterWithThreadLocalAccessor extends AbstractSentryWebFilter {
 
   public SentryWebFilterWithThreadLocalAccessor(final @NotNull IHub hub) {
     super(hub);
@@ -20,6 +22,16 @@ public final class SentryWebFilterWithThreadLocalAccessor extends SentryWebFilte
   public Mono<Void> filter(
       final @NotNull ServerWebExchange serverWebExchange,
       final @NotNull WebFilterChain webFilterChain) {
-    return ReactorUtils.withSentryNewMainHubClone(super.filter(serverWebExchange, webFilterChain));
+    return ReactorUtils.withSentryNewMainHubClone(webFilterChain
+      .filter(serverWebExchange)
+      .doFinally(
+        __ -> {
+          doFinally(Sentry.getCurrentHub());
+          Sentry.setCurrentHub(NoOpHub.getInstance());
+        })
+      .doFirst(
+        () -> {
+          doFirst(serverWebExchange, Sentry.getCurrentHub());
+        }));
   }
 }
