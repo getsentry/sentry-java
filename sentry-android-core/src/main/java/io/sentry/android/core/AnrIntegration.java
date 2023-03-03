00f8485ceab170076cf1d913d66cfa3ed6dd49c4
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2023-03-03 09:40:51 +0100

    Abort and link transactions when an ANR is detected

--- sentry-android-core/src/main/java/io/sentry/android/core/AnrIntegration.java
+++ sentry-android-core/src/main/java/io/sentry/android/core/AnrIntegration.java
@@ -4,10 +4,12 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import io.sentry.Hint;
 import io.sentry.IHub;
+import io.sentry.ITransaction;
 import io.sentry.Integration;
 import io.sentry.SentryEvent;
 import io.sentry.SentryLevel;
 import io.sentry.SentryOptions;
+import io.sentry.SpanStatus;
 import io.sentry.exception.ExceptionMechanismException;
 import io.sentry.hints.AbnormalExit;
 import io.sentry.protocol.Mechanism;
@@ -95,9 +97,23 @@ public final class AnrIntegration implements Integration, Closeable {
     final SentryEvent event = new SentryEvent(anrThrowable);
     event.setLevel(SentryLevel.ERROR);
 
+    // In case there's an ongoing transaction we want to finish it
+    // and assign the trace span context to the event
+    hub.configureScope(
+        scope -> {
+          @Nullable ITransaction transaction = scope.getTransaction();
+          if (transaction != null) {
+            event.setTransaction(transaction.getName());
+            event.getContexts().setTrace(transaction.getSpanContext());
+
+            if (!transaction.isFinished()) {
+              transaction.finish(SpanStatus.ABORTED);
+            }
+          }
+        });
+
     final AnrHint anrHint = new AnrHint(isAppInBackground);
     final Hint hint = HintUtils.createWithTypeCheckHint(anrHint);
-
     hub.captureEvent(event, hint);
   }
 
