9f44057be2ec9cf2618183c70a65c798046c152b
buenaflor <giancarlo.buenaflor@sentry.io>
2023-03-27 11:30:59 +0200

    make parameters nullable and use final

--- sentry-android-core/src/main/java/io/sentry/android/core/SentryLogcatAdapter.java
+++ sentry-android-core/src/main/java/io/sentry/android/core/SentryLogcatAdapter.java
@@ -6,6 +6,7 @@ import io.sentry.Sentry;
 import io.sentry.SentryLevel;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * This class replaces {@link android.util.Log} with its own implementations which creates a {@link
@@ -15,94 +16,100 @@ import org.jetbrains.annotations.NotNull;
 @ApiStatus.Internal
 public final class SentryLogcatAdapter {
 
-  private static void addAsBreadcrumb(@NotNull String tag, @NotNull SentryLevel level, String msg) {
+  private static void addAsBreadcrumb(
+      @Nullable String tag, @NotNull SentryLevel level, @Nullable String msg) {
     addAsBreadcrumb(tag, level, msg, null);
   }
 
   private static void addAsBreadcrumb(
-      @NotNull String tag, @NotNull SentryLevel level, Throwable tr) {
+      @Nullable String tag, @NotNull SentryLevel level, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, level, null, tr);
   }
 
   private static void addAsBreadcrumb(
-      @NotNull String tag, @NotNull SentryLevel level, String msg, Throwable tr) {
+      @Nullable final String tag,
+      @NotNull final SentryLevel level,
+      @Nullable final String msg,
+      @Nullable final Throwable tr) {
     Breadcrumb breadcrumb = new Breadcrumb();
-    breadcrumb.setCategory("log");
+    breadcrumb.setCategory("Logcat");
     breadcrumb.setMessage(msg);
     breadcrumb.setLevel(level);
-    breadcrumb.setData("tag", tag);
-    if (tr != null) {
+    if (tag != null) {
+      breadcrumb.setData("tag", tag);
+    }
+    if (tr != null && tr.getMessage() != null) {
       breadcrumb.setData("throwable", tr.getMessage());
     }
     Sentry.addBreadcrumb(breadcrumb);
   }
 
-  public static int v(String tag, @NotNull String msg) {
+  public static int v(@Nullable String tag, @Nullable String msg) {
     addAsBreadcrumb(tag, SentryLevel.DEBUG, msg);
     return Log.v(tag, msg);
   }
 
-  public static int v(String tag, String msg, Throwable tr) {
+  public static int v(@Nullable String tag, @Nullable String msg, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.DEBUG, msg, tr);
     return Log.v(tag, msg, tr);
   }
 
-  public static int d(String tag, @NotNull String msg) {
+  public static int d(@Nullable String tag, @Nullable String msg) {
     addAsBreadcrumb(tag, SentryLevel.DEBUG, msg);
     return Log.d(tag, msg);
   }
 
-  public static int d(String tag, String msg, Throwable tr) {
+  public static int d(@Nullable String tag, @Nullable String msg, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.DEBUG, msg, tr);
     return Log.d(tag, msg, tr);
   }
 
-  public static int i(String tag, @NotNull String msg) {
+  public static int i(@Nullable String tag, @Nullable String msg) {
     addAsBreadcrumb(tag, SentryLevel.INFO, msg);
     return Log.i(tag, msg);
   }
 
-  public static int i(String tag, String msg, Throwable tr) {
+  public static int i(@Nullable String tag, @Nullable String msg, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.INFO, msg, tr);
     return Log.i(tag, msg, tr);
   }
 
-  public static int w(String tag, @NotNull String msg) {
+  public static int w(@Nullable String tag, @Nullable String msg) {
     addAsBreadcrumb(tag, SentryLevel.WARNING, msg);
     return Log.w(tag, msg);
   }
 
-  public static int w(String tag, String msg, Throwable tr) {
+  public static int w(@Nullable String tag, @Nullable String msg, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.WARNING, msg, tr);
     return Log.w(tag, msg, tr);
   }
 
-  public static int w(String tag, Throwable tr) {
+  public static int w(@Nullable String tag, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.WARNING, tr);
     return Log.w(tag, tr);
   }
 
-  public static int e(String tag, @NotNull String msg) {
+  public static int e(@Nullable String tag, @Nullable String msg) {
     addAsBreadcrumb(tag, SentryLevel.ERROR, msg);
     return Log.e(tag, msg);
   }
 
-  public static int e(String tag, String msg, Throwable tr) {
+  public static int e(@Nullable String tag, @Nullable String msg, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.ERROR, msg, tr);
     return Log.e(tag, msg, tr);
   }
 
-  public static int wtf(String tag, String msg) {
+  public static int wtf(@Nullable String tag, @Nullable String msg) {
     addAsBreadcrumb(tag, SentryLevel.ERROR, msg);
     return Log.wtf(tag, msg);
   }
 
-  public static int wtf(String tag, @NotNull Throwable tr) {
+  public static int wtf(@Nullable String tag, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.ERROR, tr);
     return Log.wtf(tag, tr);
   }
 
-  public static int wtf(String tag, String msg, Throwable tr) {
+  public static int wtf(@Nullable String tag, @Nullable String msg, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.ERROR, msg, tr);
     return Log.wtf(tag, msg, tr);
   }
