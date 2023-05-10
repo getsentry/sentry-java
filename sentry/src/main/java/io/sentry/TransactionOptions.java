f8d3e91047f0cf932f820c7eba442ba5f413d6fe
Lukas Bloder <lukas.bloder@gmail.com>
2023-05-10 17:08:48 +0200

    add javadoc to TransactionOptions

--- sentry/src/main/java/io/sentry/TransactionOptions.java
+++ sentry/src/main/java/io/sentry/TransactionOptions.java
@@ -5,50 +5,24 @@ import org.jetbrains.annotations.Nullable;
 /** Sentry Transaction options */
 public final class TransactionOptions extends SpanOptions {
 
+  /**
+   * Arbitrary data used in {@link SamplingContext} to determine if transaction is going to be
+   * sampled.
+   */
   private @Nullable CustomSamplingContext customSamplingContext = null;
+
+  /** Defines if transaction should be bound to scope */
   private boolean bindToScope = false;
+
+  /** The start timestamp of the transaction */
   private @Nullable SentryDate startTimestamp = null;
-  private boolean waitForChildren = false;
-
-  private @Nullable Long idleTimeout = null;
-  private @Nullable TransactionFinishedCallback transactionFinishedCallback = null;
-
-  public @Nullable CustomSamplingContext getCustomSamplingContext() {
-    return customSamplingContext;
-  }
-
-  public void setCustomSamplingContext(@Nullable CustomSamplingContext customSamplingContext) {
-    this.customSamplingContext = customSamplingContext;
-  }
-
-  public boolean isBindToScope() {
-    return bindToScope;
-  }
-
-  public void setBindToScope(boolean bindToScope) {
-    this.bindToScope = bindToScope;
-  }
-
-  public @Nullable SentryDate getStartTimestamp() {
-    return startTimestamp;
-  }
-
-  public void setStartTimestamp(@Nullable SentryDate startTimestamp) {
-    this.startTimestamp = startTimestamp;
-  }
 
   /**
    * When `waitForChildren` is set to `true`, tracer will finish only when both conditions are met
    * (the order of meeting condition does not matter): - tracer itself is finished - all child spans
    * are finished.
    */
-  public boolean isWaitForChildren() {
-    return waitForChildren;
-  }
-
-  public void setWaitForChildren(boolean waitForChildren) {
-    this.waitForChildren = waitForChildren;
-  }
+  private boolean waitForChildren = false;
 
   /**
    * The idle time, measured in ms, to wait until the transaction will be finished. The span will
@@ -58,18 +32,118 @@ public final class TransactionOptions extends SpanOptions {
    *
    * <p>The default is 3 seconds.
    */
+  private @Nullable Long idleTimeout = null;
+
+  /**
+   * When `waitForChildren` is set to `true` and this callback is set, it's called before the
+   * transaction is captured.
+   */
+  private @Nullable TransactionFinishedCallback transactionFinishedCallback = null;
+
+  /**
+   * Gets the customSamplingContext
+   *
+   * @return customSamplingContext - the customSamplingContext
+   */
+  public @Nullable CustomSamplingContext getCustomSamplingContext() {
+    return customSamplingContext;
+  }
+
+  /**
+   * Sets the customSamplingContext
+   *
+   * @param customSamplingContext - the customSamplingContext
+   */
+  public void setCustomSamplingContext(@Nullable CustomSamplingContext customSamplingContext) {
+    this.customSamplingContext = customSamplingContext;
+  }
+
+  /**
+   * Checks if bindToScope is enabled
+   *
+   * @return true if enabled or false otherwise
+   */
+  public boolean isBindToScope() {
+    return bindToScope;
+  }
+
+  /**
+   * Sets bindToScope to enabled or disabled
+   *
+   * @param bindToScope true if enabled or false otherwise
+   */
+  public void setBindToScope(boolean bindToScope) {
+    this.bindToScope = bindToScope;
+  }
+
+  /**
+   * Gets the startTimestamp
+   *
+   * @return startTimestamp - the startTimestamp
+   */
+  public @Nullable SentryDate getStartTimestamp() {
+    return startTimestamp;
+  }
+
+  /**
+   * Sets the startTimestamp
+   *
+   * @param startTimestamp - the startTimestamp
+   */
+  public void setStartTimestamp(@Nullable SentryDate startTimestamp) {
+    this.startTimestamp = startTimestamp;
+  }
+
+  /**
+   * Checks if waitForChildren is enabled
+   *
+   * @return true if enabled or false otherwise
+   */
+  public boolean isWaitForChildren() {
+    return waitForChildren;
+  }
+
+  /**
+   * Sets waitForChildren to enabled or disabled
+   *
+   * @param waitForChildren true if enabled or false otherwise
+   */
+  public void setWaitForChildren(boolean waitForChildren) {
+    this.waitForChildren = waitForChildren;
+  }
+
+  /**
+   * Gets the idleTimeout
+   *
+   * @return idleTimeout - the idleTimeout
+   */
   public @Nullable Long getIdleTimeout() {
     return idleTimeout;
   }
 
+  /**
+   * Sets the idleTimeout
+   *
+   * @param idleTimeout - the idleTimeout
+   */
   public void setIdleTimeout(@Nullable Long idleTimeout) {
     this.idleTimeout = idleTimeout;
   }
 
+  /**
+   * Gets the transactionFinishedCallback callback
+   *
+   * @return transactionFinishedCallback - the transactionFinishedCallback callback
+   */
   public @Nullable TransactionFinishedCallback getTransactionFinishedCallback() {
     return transactionFinishedCallback;
   }
 
+  /**
+   * Sets the transactionFinishedCallback callback
+   *
+   * @param transactionFinishedCallback - the transactionFinishedCallback callback
+   */
   public void setTransactionFinishedCallback(
       @Nullable TransactionFinishedCallback transactionFinishedCallback) {
     this.transactionFinishedCallback = transactionFinishedCallback;
