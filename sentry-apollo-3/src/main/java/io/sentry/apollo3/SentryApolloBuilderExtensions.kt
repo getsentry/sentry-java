ad659db8f7b029608f162bbe77ebbbd45a9562f3
Manoel Aranda Neto <marandaneto@gmail.com>
2023-06-12 18:31:53 +0200

    fix

--- sentry-apollo-3/src/main/java/io/sentry/apollo3/SentryApolloBuilderExtensions.kt
+++ sentry-apollo-3/src/main/java/io/sentry/apollo3/SentryApolloBuilderExtensions.kt
@@ -3,7 +3,6 @@ package io.sentry.apollo3
 import com.apollographql.apollo3.ApolloClient
 import io.sentry.HubAdapter
 import io.sentry.IHub
-import io.sentry.SentryOptions
 import io.sentry.SentryOptions.DEFAULT_PROPAGATION_TARGETS
 import io.sentry.apollo3.SentryApollo3HttpInterceptor.Companion.DEFAULT_CAPTURE_FAILED_REQUESTS
 
@@ -29,9 +28,10 @@ fun ApolloClient.Builder.sentryTracing(
 fun ApolloClient.Builder.sentryTracing(
     beforeSpan: SentryApollo3HttpInterceptor.BeforeSpanCallback? = null,
     captureFailedRequests: Boolean = DEFAULT_CAPTURE_FAILED_REQUESTS,
-    failedRequestTargets: List<String> = listOf(SentryOptions.DEFAULT_PROPAGATION_TARGETS)
+    failedRequestTargets: List<String> = listOf(DEFAULT_PROPAGATION_TARGETS)
 ): ApolloClient.Builder {
     return sentryTracing(
+        hub = HubAdapter.getInstance(),
         beforeSpan = beforeSpan,
         captureFailedRequests = captureFailedRequests,
         failedRequestTargets = failedRequestTargets
