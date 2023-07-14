562dbdd08866bfcc5c1c486110c977a57a96fd45
Lukas Bloder <lukas.bloder@gmail.com>
2023-07-14 10:16:37 +0200

    only check for dsn null after checking whether sentry is enabled

--- sentry/src/main/java/io/sentry/Sentry.java
+++ sentry/src/main/java/io/sentry/Sentry.java
@@ -287,12 +287,13 @@ public final class Sentry {
     }
 
     final String dsn = options.getDsn();
-    if (dsn == null) {
-      throw new IllegalArgumentException(
-          "DSN is required. Use empty string or set enabled to false in SentryOptions to disable SDK.");
-    } else if (dsn.isEmpty() || !options.isEnabled()) {
+
+    if(!options.isEnabled() || (dsn != null && dsn.isEmpty())) {
       close();
       return false;
+    } else if (dsn == null) {
+      throw new IllegalArgumentException(
+        "DSN is required. Use empty string or set enabled to false in SentryOptions to disable SDK.");
     }
 
     @SuppressWarnings("unused")
