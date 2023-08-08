227ad2cdeac3d31f68f7bd7d1eb0a856a2a31c69
buenaflor <giancarlo.buenaflor@sentry.io>
2023-08-08 14:34:34 +0200

    (don't merge): debugging Kover

--- sentry-compose/build.gradle.kts
+++ sentry-compose/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     kotlin("multiplatform")
     id("com.android.library")
     id("org.jetbrains.compose")
+    id("org.jetbrains.kotlinx.kover") version "0.7.3"
     jacoco
     id(Config.QualityPlugins.jacocoAndroid)
     id(Config.QualityPlugins.gradleVersions)
@@ -111,17 +112,6 @@ android {
         if (Config.Android.shouldSkipDebugVariant(buildType.name)) {
             ignore = true
         }
-    }
-}
-
-jacoco {
-    toolVersion = "0.8.10"
-}
-
-tasks.withType<Test> {
-    configure<JacocoTaskExtension> {
-        isIncludeNoLocationClasses = true
-        excludes = listOf("jdk.internal.*")
     }
 }
 
