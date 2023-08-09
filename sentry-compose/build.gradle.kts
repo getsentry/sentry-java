e20f6fa8c0c67f5dbd9a82b52a7ee5a496bab546
buenaflor <giancarlo.buenaflor@sentry.io>
2023-08-09 14:17:09 +0200

    (don't merge): try renaming the kover report

--- sentry-compose/build.gradle.kts
+++ sentry-compose/build.gradle.kts
@@ -17,6 +17,14 @@ plugins {
     `maven-publish` // necessary for publishMavenLocal task to publish correct artifacts
 }
 
+koverReport {
+    androidReports("release") {
+        xml {
+            setReportFile(file("${buildDir}/reports/kover/report.xml"))
+        }
+    }
+}
+
 kotlin {
     explicitApi()
 
