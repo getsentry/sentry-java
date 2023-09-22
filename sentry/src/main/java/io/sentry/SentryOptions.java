c4202724253f62cb08eeb73b19f37d57c856e1e4
Alexander Dinauer <alexander.dinauer@sentry.io>
2023-09-22 09:16:52 +0200

    code review changes

--- sentry/src/main/java/io/sentry/SentryOptions.java
+++ sentry/src/main/java/io/sentry/SentryOptions.java
@@ -2456,7 +2456,7 @@ public class SentryOptions {
     }
     if (options.getIgnoredCheckIns() != null) {
       final List<String> ignoredCheckIns = new ArrayList<>(options.getIgnoredCheckIns());
-      setTracePropagationTargets(ignoredCheckIns);
+      setIgnoredCheckIns(ignoredCheckIns);
     }
   }
 
