62a746fa35934fdfda88b976bb573471f6c9f8a6
Alexander Dinauer <alexander.dinauer@sentry.io>
2023-09-25 12:00:21 +0200

    demo code

--- sentry-quartz/src/main/java/io/sentry/quartz/SentryJobListener.java
+++ sentry-quartz/src/main/java/io/sentry/quartz/SentryJobListener.java
@@ -5,29 +5,18 @@ import io.sentry.CheckIn;
 import io.sentry.CheckInStatus;
 import io.sentry.HubAdapter;
 import io.sentry.IHub;
-import io.sentry.MonitorConfig;
-import io.sentry.MonitorSchedule;
-import io.sentry.MonitorScheduleUnit;
 import io.sentry.Sentry;
 import io.sentry.SentryIntegrationPackageStorage;
 import io.sentry.SentryLevel;
 import io.sentry.protocol.SentryId;
 import io.sentry.util.Objects;
-import java.util.List;
-import java.util.TimeZone;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.quartz.CalendarIntervalTrigger;
-import org.quartz.CronTrigger;
-import org.quartz.DateBuilder;
-import org.quartz.Job;
-import org.quartz.JobDetail;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.quartz.JobKey;
 import org.quartz.JobListener;
-import org.quartz.SimpleTrigger;
 import org.quartz.Trigger;
 
 @ApiStatus.Experimental
@@ -55,19 +44,17 @@ public final class SentryJobListener implements JobListener {
   }
 
   @Override
-  public void jobToBeExecuted(JobExecutionContext context) {
+  public void jobToBeExecuted(final @NotNull JobExecutionContext context) {
     try {
       if (isDisabled()) {
         return;
       }
-      final @NotNull String slug = getSlug(context.getJobDetail());
-      final @NotNull CheckIn checkIn = new CheckIn(slug, CheckInStatus.IN_PROGRESS);
-
-      final @Nullable MonitorConfig monitorConfig = extractMonitorConfig(context);
-      if (monitorConfig != null) {
-        checkIn.setMonitorConfig(monitorConfig);
+      final @Nullable String maybeSlug = getSlug(context);
+      if (maybeSlug == null) {
+        return;
       }
-
+      final @NotNull String slug = maybeSlug;
+      final @NotNull CheckIn checkIn = new CheckIn(slug, CheckInStatus.IN_PROGRESS);
       final @NotNull SentryId checkInId = Sentry.captureCheckIn(checkIn);
       context.put(SENTRY_CHECK_IN_ID_KEY, checkInId);
       context.put(SENTRY_CHECK_IN_SLUG_KEY, slug);
@@ -79,112 +66,18 @@ public final class SentryJobListener implements JobListener {
     }
   }
 
-  private @NotNull String getSlug(final @Nullable JobDetail jobDetail) {
-    if (jobDetail == null) {
-      return "fallback";
-    }
-    final @NotNull StringBuilder slugBuilder = new StringBuilder();
-
-    final @Nullable JobKey key = jobDetail.getKey();
-    if (key != null) {
-      slugBuilder.append(key.getName());
-      slugBuilder.append("__");
-    }
-
-    final @Nullable Class<? extends Job> jobClass = jobDetail.getJobClass();
-    if (jobClass != null) {
-      slugBuilder.append(jobClass.getCanonicalName());
-    }
-
-    return slugBuilder.toString();
-  }
-
-  private @Nullable MonitorConfig extractMonitorConfig(final @NotNull JobExecutionContext context) {
-    @Nullable MonitorSchedule schedule = null;
-    @Nullable String cronExpression = null;
-    @Nullable TimeZone timeZone = TimeZone.getDefault();
-    @Nullable Integer repeatInterval = null;
-    @Nullable MonitorScheduleUnit timeUnit = null;
-
-    try {
-      List<? extends Trigger> triggersOfJob =
-          context.getScheduler().getTriggersOfJob(context.getTrigger().getJobKey());
-      for (Trigger trigger : triggersOfJob) {
-        if (trigger instanceof CronTrigger) {
-          final CronTrigger cronTrigger = (CronTrigger) trigger;
-          cronExpression = cronTrigger.getCronExpression();
-          timeZone = cronTrigger.getTimeZone();
-        } else if (trigger instanceof SimpleTrigger) {
-          final SimpleTrigger simpleTrigger = (SimpleTrigger) trigger;
-          long tmpRepeatInterval = simpleTrigger.getRepeatInterval();
-          repeatInterval = millisToMinutes(Double.valueOf(tmpRepeatInterval));
-          timeUnit = MonitorScheduleUnit.MINUTE;
-        } else if (trigger instanceof CalendarIntervalTrigger) {
-          final CalendarIntervalTrigger calendarIntervalTrigger = (CalendarIntervalTrigger) trigger;
-          DateBuilder.IntervalUnit repeatIntervalUnit =
-              calendarIntervalTrigger.getRepeatIntervalUnit();
-          int tmpRepeatInterval = calendarIntervalTrigger.getRepeatInterval();
-          if (DateBuilder.IntervalUnit.SECOND.equals(repeatIntervalUnit)) {
-            repeatInterval = secondsToMinutes(Double.valueOf(tmpRepeatInterval));
-            timeUnit = MonitorScheduleUnit.MINUTE;
-          } else if (DateBuilder.IntervalUnit.MILLISECOND.equals(repeatIntervalUnit)) {
-            repeatInterval = millisToMinutes(Double.valueOf(tmpRepeatInterval));
-            timeUnit = MonitorScheduleUnit.MINUTE;
-          } else {
-            repeatInterval = tmpRepeatInterval;
-            timeUnit = convertUnit(repeatIntervalUnit);
-          }
-        }
-      }
-    } catch (Throwable t) {
-      Sentry.getCurrentHub()
-          .getOptions()
-          .getLogger()
-          .log(SentryLevel.ERROR, "Unable to extract monitor config for check-in.", t);
-    }
-    if (cronExpression != null) {
-      schedule = MonitorSchedule.crontab(cronExpression);
-    } else if (repeatInterval != null && timeUnit != null) {
-      schedule = MonitorSchedule.interval(repeatInterval.intValue(), timeUnit);
-    }
-
-    if (schedule != null) {
-      final @Nullable MonitorConfig monitorConfig = new MonitorConfig(schedule);
-      if (timeZone != null) {
-        monitorConfig.setTimezone(timeZone.getID());
-      }
-      return monitorConfig;
-    } else {
-      return null;
-    }
-  }
-
-  private @Nullable Integer millisToMinutes(final @NotNull Double milis) {
-    return Double.valueOf((milis / 1000.0) / 60.0).intValue();
-  }
-
-  private @Nullable Integer secondsToMinutes(final @NotNull Double seconds) {
-    return Double.valueOf(seconds / 60.0).intValue();
-  }
-
-  private @Nullable MonitorScheduleUnit convertUnit(
-      final @Nullable DateBuilder.IntervalUnit intervalUnit) {
-    if (intervalUnit == null) {
+  private @Nullable String getSlug(final @NotNull JobExecutionContext context) {
+    final Trigger trigger = context.getTrigger();
+    if (trigger == null) {
       return null;
     }
 
-    if (DateBuilder.IntervalUnit.MINUTE.equals(intervalUnit)) {
-      return MonitorScheduleUnit.MINUTE;
-    } else if (DateBuilder.IntervalUnit.HOUR.equals(intervalUnit)) {
-      return MonitorScheduleUnit.HOUR;
-    } else if (DateBuilder.IntervalUnit.DAY.equals(intervalUnit)) {
-      return MonitorScheduleUnit.DAY;
-    } else if (DateBuilder.IntervalUnit.WEEK.equals(intervalUnit)) {
-      return MonitorScheduleUnit.WEEK;
-    } else if (DateBuilder.IntervalUnit.MONTH.equals(intervalUnit)) {
-      return MonitorScheduleUnit.MONTH;
-    } else if (DateBuilder.IntervalUnit.YEAR.equals(intervalUnit)) {
-      return MonitorScheduleUnit.YEAR;
+    final @Nullable JobDataMap jobDataMap = trigger.getJobDataMap();
+    if (jobDataMap != null) {
+      final @Nullable Object o = jobDataMap.get(SENTRY_CHECK_IN_SLUG_KEY);
+      if (o != null) {
+        return o.toString();
+      }
     }
 
     return null;
--- sentry-samples/sentry-samples-spring-boot/src/main/java/io/sentry/samples/spring/boot/SentryDemoApplication.java
+++ sentry-samples/sentry-samples-spring-boot/src/main/java/io/sentry/samples/spring/boot/SentryDemoApplication.java
@@ -1,6 +1,9 @@
 package io.sentry.samples.spring.boot;
 
+import static io.sentry.quartz.SentryJobListener.SENTRY_CHECK_IN_SLUG_KEY;
+
 import io.sentry.samples.spring.boot.quartz.SampleJob;
+import java.util.Collections;
 import org.quartz.JobDetail;
 import org.quartz.SimpleTrigger;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
 import org.springframework.web.client.RestTemplate;
@@ -33,27 +37,29 @@ public class SentryDemoApplication {
   @Bean
   public JobDetailFactoryBean jobDetail() {
     JobDetailFactoryBean jobDetailFactory = new JobDetailFactoryBean();
-    jobDetailFactory.setName("hello_spring_boot_2");
     jobDetailFactory.setJobClass(SampleJob.class);
-    jobDetailFactory.setDescription("Invoke Sample Job service...");
     jobDetailFactory.setDurability(true);
     return jobDetailFactory;
   }
-
-  //  @Bean
-  //  public CronTriggerFactoryBean trigger(JobDetail job) {
-  //    CronTriggerFactoryBean trigger = new CronTriggerFactoryBean();
-  //    trigger.setJobDetail(job);
-  //    trigger.setCronExpression("0 * * ? * *");
-  //    return trigger;
-  //  }
 
   @Bean
   public SimpleTriggerFactoryBean trigger(JobDetail job) {
     SimpleTriggerFactoryBean trigger = new SimpleTriggerFactoryBean();
     trigger.setJobDetail(job);
-    trigger.setRepeatInterval(2 * 60 * 1000);
+    trigger.setRepeatInterval(2 * 60 * 1000); // every two minutes
     trigger.setRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY);
+    trigger.setJobDataAsMap(
+        Collections.singletonMap(SENTRY_CHECK_IN_SLUG_KEY, "monitor_slug_simple_trigger"));
+    return trigger;
+  }
+
+  @Bean
+  public CronTriggerFactoryBean cronTrigger(JobDetail job) {
+    CronTriggerFactoryBean trigger = new CronTriggerFactoryBean();
+    trigger.setJobDetail(job);
+    trigger.setCronExpression("0 0/5 * ? * *"); // every five minutes
+    trigger.setJobDataAsMap(
+        Collections.singletonMap(SENTRY_CHECK_IN_SLUG_KEY, "monitor_slug_cron_trigger"));
     return trigger;
   }
 }
