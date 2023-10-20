86f00968b4ba59f756bb4fe01e4fb1cc61bd4f1d
Roman Zavarnitsyn <rom4ek93@gmail.com>
2023-10-20 14:39:16 +0200

    Add missing license headers (#3003)

--- sentry/src/test/java/io/sentry/transport/ReusableCountLatchTest.kt
+++ sentry/src/test/java/io/sentry/transport/ReusableCountLatchTest.kt
@@ -1,3 +1,21 @@
+/*
+ * Adapted from https://github.com/MatejTymes/JavaFixes/blob/37e74b9d0a29f7a47485c6d1bb1307f01fb93634/src/test/java/javafixes/concurrency/ReusableCountLatchTest.java
+ *
+ * Copyright (C) 2016 Matej Tymes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.sentry.transport
 
 import java.util.concurrent.Executors.newScheduledThreadPool
@@ -10,9 +28,6 @@ import kotlin.test.assertFailsWith
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
-/**
- * Tests originally copied from <a href="https://github.com/MatejTymes/JavaFixes/blob/37e74b9d0a29f7a47485c6d1bb1307f01fb93634/src/test/java/javafixes/concurrency/ReusableCountLatchTest.java">ReusableCountLatchTest</a>.
- */
 class ReusableCountLatchTest {
     @Test
     fun `should reflect initial value`() {
