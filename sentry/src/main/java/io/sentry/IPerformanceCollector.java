b2626e6ed481fa121364e8f4b11792d70c6852b5
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2023-12-15 12:00:34 +0100

    Extend performance collector APIs

---
+++ sentry/src/main/java/io/sentry/IPerformanceCollector.java
@@ -0,0 +1,7 @@
+package io.sentry;
+
+import org.jetbrains.annotations.ApiStatus;
+
+/** Used for collecting performance data while a transaction / span is running */
+@ApiStatus.Internal
+public interface IPerformanceCollector {}
--- sentry/src/main/java/io/sentry/Sentry.java
+++ sentry/src/main/java/io/sentry/Sentry.java
@@ -387,8 +387,8 @@ public final class Sentry {
       options.setMainThreadChecker(MainThreadChecker.getInstance());
     }
 
-    if (options.getCollectors().isEmpty()) {
-      options.addCollector(new JavaMemoryCollector());
+    if (options.getPerformanceCollectors().isEmpty()) {
+      options.addPerformanceCollector(new JavaMemoryCollector());
     }
 
     return true;
