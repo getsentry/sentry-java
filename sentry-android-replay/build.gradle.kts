2dbe2ce5cff19b796b959667e09a03b906fd4210
Roman Zavarnitsyn <rom4ek93@gmail.com>
2024-02-01 16:40:27 +0100

    Add new sentry-android-replay module

---
+++ sentry-android-replay/build.gradle.kts
@@ -0,0 +1,78 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    jacoco
+    id(Config.QualityPlugins.jacocoAndroid)
+    id(Config.QualityPlugins.gradleVersions)
+    id(Config.QualityPlugins.detektPlugin)
+}
+
+android {
+    compileSdk = Config.Android.compileSdkVersion
+    namespace = "io.sentry.android.replay"
+
+    defaultConfig {
+        targetSdk = Config.Android.targetSdkVersion
+        minSdk = Config.Android.minSdkVersionReplay
+
+        // for AGP 4.1
+        buildConfigField("String", "VERSION_NAME", "\"${project.version}\"")
+    }
+
+    buildTypes {
+        getByName("debug")
+        getByName("release")
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        kotlinOptions.languageVersion = Config.kotlinCompatibleLanguageVersion
+    }
+
+    testOptions {
+        animationsDisabled = true
+        unitTests.apply {
+            isReturnDefaultValues = true
+            isIncludeAndroidResources = true
+        }
+    }
+
+    lint {
+        warningsAsErrors = true
+        checkDependencies = true
+
+        // We run a full lint analysis as build part in CI, so skip vital checks for assemble tasks.
+        checkReleaseBuilds = false
+    }
+
+    variantFilter {
+        if (Config.Android.shouldSkipDebugVariant(buildType.name)) {
+            ignore = true
+        }
+    }
+}
+
+kotlin {
+    explicitApi()
+}
+
+dependencies {
+    api(projects.sentry)
+
+    implementation(kotlin(Config.kotlinStdLib, KotlinCompilerVersion.VERSION))
+
+    // tests
+    testImplementation(projects.sentryTestSupport)
+    testImplementation(Config.TestLibs.kotlinTestJunit)
+    testImplementation(Config.TestLibs.androidxJunit)
+    testImplementation(Config.TestLibs.mockitoKotlin)
+    testImplementation(Config.TestLibs.mockitoInline)
+}
+
+tasks.withType<Detekt> {
+    // Target version of the generated JVM bytecode. It is used for type resolution.
+    jvmTarget = JavaVersion.VERSION_1_8.toString()
+}
