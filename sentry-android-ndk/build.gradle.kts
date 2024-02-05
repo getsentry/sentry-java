42df453c6020e2d9656ed230b205631b7f102a08
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2024-02-05 08:44:56 +0100

    POC: Move NDK code to sentry-natice

--- sentry-android-ndk/build.gradle.kts
+++ sentry-android-ndk/build.gradle.kts
@@ -9,19 +9,11 @@ plugins {
     id(Config.QualityPlugins.gradleVersions)
 }
 
-var sentryNativeSrc: String = "sentry-native"
 val sentryAndroidSdkName: String by project
 
 android {
     compileSdk = Config.Android.compileSdkVersion
     namespace = "io.sentry.android.ndk"
-
-    sentryNativeSrc = if (File("${project.projectDir}/sentry-native-local").exists()) {
-        "sentry-native-local"
-    } else {
-        "sentry-native"
-    }
-    println("sentry-android-ndk: $sentryNativeSrc")
 
     defaultConfig {
         targetSdk = Config.Android.targetSdkVersion
@@ -29,29 +21,12 @@ android {
 
         testInstrumentationRunner = Config.TestLibs.androidJUnitRunner
 
-        externalNativeBuild {
-            cmake {
-                arguments.add(0, "-DANDROID_STL=c++_static")
-                arguments.add(0, "-DSENTRY_NATIVE_SRC=$sentryNativeSrc")
-                arguments.add(0, "-DSENTRY_SDK_NAME=$sentryAndroidSdkName")
-            }
-        }
-
         ndk {
             abiFilters.addAll(Config.Android.abiFilters)
         }
 
         // for AGP 4.1
         buildConfigField("String", "VERSION_NAME", "\"${project.version}\"")
-    }
-
-    // we use the default NDK and CMake versions based on the AGP's version
-    // https://developer.android.com/studio/projects/install-ndk#apply-specific-version
-
-    externalNativeBuild {
-        cmake {
-            path("CMakeLists.txt")
-        }
     }
 
     buildTypes {
@@ -81,10 +56,6 @@ android {
         checkReleaseBuilds = false
     }
 
-    nativeBundleExport {
-        headerDir = "${project.projectDir}/$sentryNativeSrc/include"
-    }
-
     // needed because of Kotlin 1.4.x
     configurations.all {
         resolutionStrategy.force(Config.CompileOnly.jetbrainsAnnotations)
@@ -100,6 +71,8 @@ android {
 dependencies {
     api(projects.sentry)
     api(projects.sentryAndroidCore)
+
+    implementation("io.sentry:sentry-native-ndk:1.0.1")
 
     compileOnly(Config.CompileOnly.jetbrainsAnnotations)
 
