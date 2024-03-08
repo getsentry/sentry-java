a4f8152ac1eb5cbe3702cf6d7379728a29e5d302
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2024-03-08 09:44:12 +0100

    Add ttid/ttfd contribution flags

--- sentry/src/main/java/io/sentry/SpanDataConvention.java
+++ sentry/src/main/java/io/sentry/SpanDataConvention.java
@@ -21,4 +21,6 @@ public interface SpanDataConvention {
   String FRAMES_SLOW = "frames.slow";
   String FRAMES_FROZEN = "frames.frozen";
   String FRAMES_DELAY = "frames.delay";
+  String CONTRIBUTES_TTID = "contributes_ttid";
+  String CONTRIBUTES_TTFD = "contributes_ttfd";
 }
