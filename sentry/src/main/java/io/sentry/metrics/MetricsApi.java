256ae960c665cfd692491d4d0ca8acc41a739a2d
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2024-03-20 09:38:01 +0100

    Remove code location from metrics (#3273)
    * ref: Remove unused Metrics code
    
    Remove the timestamp and SentryMetric because they aren't used.
    
    * update api
    
    * Remove code locations / stacklevel as well
    
    * Revert "Remove code locations / stacklevel as well"
    
    This reverts commit d1877da5de3d9e657c617b8edb7767e925bf97bb.
    
    * Remove code locations / stacklevel as well
    
    ---------
    
    Co-authored-by: Philipp Hofmann <philipp.hofmann@sentry.io>

--- sentry/src/main/java/io/sentry/metrics/MetricsApi.java
+++ sentry/src/main/java/io/sentry/metrics/MetricsApi.java
@@ -37,7 +37,7 @@ public final class MetricsApi {
    * @param key A unique key identifying the metric
    */
   public void increment(final @NotNull String key) {
-    increment(key, 1.0, null, null, null, 1);
+    increment(key, 1.0, null, null, null);
   }
 
   /**
@@ -47,7 +47,7 @@ public final class MetricsApi {
    * @param value The value to be added
    */
   public void increment(final @NotNull String key, final double value) {
-    increment(key, value, null, null, null, 1);
+    increment(key, value, null, null, null);
   }
 
   /**
@@ -60,7 +60,7 @@ public final class MetricsApi {
   public void increment(
       final @NotNull String key, final double value, final @Nullable MeasurementUnit unit) {
 
-    increment(key, value, unit, null, null, 1);
+    increment(key, value, unit, null, null);
   }
 
   /**
@@ -77,7 +77,7 @@ public final class MetricsApi {
       final @Nullable MeasurementUnit unit,
       final @Nullable Map<String, String> tags) {
 
-    increment(key, value, unit, tags, null, 1);
+    increment(key, value, unit, tags, null);
   }
 
   /**
@@ -97,28 +97,6 @@ public final class MetricsApi {
       final @Nullable Map<String, String> tags,
       final @Nullable Long timestampMs) {
 
-    increment(key, value, unit, tags, timestampMs, 1);
-  }
-
-  /**
-   * Emits a Counter metric
-   *
-   * @param key A unique key identifying the metric
-   * @param value The value to be added
-   * @param unit An optional unit, see {@link MeasurementUnit}
-   * @param tags Optional Tags to associate with the metric
-   * @param timestampMs The time when the metric was emitted. Defaults to the time at which the
-   *     metric is emitted, if no value is provided.
-   * @param stackLevel Optional number of stacks levels to ignore when determining the code location
-   */
-  public void increment(
-      final @NotNull String key,
-      final double value,
-      final @Nullable MeasurementUnit unit,
-      final @Nullable Map<String, String> tags,
-      final @Nullable Long timestampMs,
-      final int stackLevel) {
-
     final long timestamp = timestampMs != null ? timestampMs : System.currentTimeMillis();
     final @NotNull Map<String, String> enrichedTags =
         MetricsHelper.mergeTags(tags, aggregator.getDefaultTagsForMetrics());
@@ -127,7 +105,7 @@ public final class MetricsApi {
 
     aggregator
         .getMetricsAggregator()
-        .increment(key, value, unit, enrichedTags, timestamp, stackLevel, localMetricsAggregator);
+        .increment(key, value, unit, enrichedTags, timestamp, localMetricsAggregator);
   }
 
   /**
@@ -137,7 +115,7 @@ public final class MetricsApi {
    * @param value The value to be added
    */
   public void gauge(final @NotNull String key, final double value) {
-    gauge(key, value, null, null, null, 1);
+    gauge(key, value, null, null, null);
   }
 
   /**
@@ -149,7 +127,7 @@ public final class MetricsApi {
    */
   public void gauge(
       final @NotNull String key, final double value, final @Nullable MeasurementUnit unit) {
-    gauge(key, value, unit, null, null, 1);
+    gauge(key, value, unit, null, null);
   }
 
   /**
@@ -166,7 +144,7 @@ public final class MetricsApi {
       final @Nullable MeasurementUnit unit,
       final @Nullable Map<String, String> tags) {
 
-    gauge(key, value, unit, tags, null, 1);
+    gauge(key, value, unit, tags, null);
   }
 
   /**
@@ -186,28 +164,6 @@ public final class MetricsApi {
       final @Nullable Map<String, String> tags,
       final @Nullable Long timestampMs) {
 
-    gauge(key, value, unit, tags, timestampMs, 1);
-  }
-
-  /**
-   * Emits a Gauge metric
-   *
-   * @param key A unique key identifying the metric
-   * @param value The value to be added
-   * @param unit An optional unit, see {@link MeasurementUnit}
-   * @param tags Optional Tags to associate with the metric
-   * @param timestampMs The time when the metric was emitted. Defaults to the time at which the
-   *     metric is emitted, if no value is provided.
-   * @param stackLevel Optional number of stacks levels to ignore when determining the code location
-   */
-  public void gauge(
-      final @NotNull String key,
-      final double value,
-      final @Nullable MeasurementUnit unit,
-      final @Nullable Map<String, String> tags,
-      final @Nullable Long timestampMs,
-      final int stackLevel) {
-
     final long timestamp = timestampMs != null ? timestampMs : System.currentTimeMillis();
     final @NotNull Map<String, String> enrichedTags =
         MetricsHelper.mergeTags(tags, aggregator.getDefaultTagsForMetrics());
@@ -216,7 +172,7 @@ public final class MetricsApi {
 
     aggregator
         .getMetricsAggregator()
-        .gauge(key, value, unit, enrichedTags, timestamp, stackLevel, localMetricsAggregator);
+        .gauge(key, value, unit, enrichedTags, timestamp, localMetricsAggregator);
   }
 
   /**
@@ -226,7 +182,7 @@ public final class MetricsApi {
    * @param value The value to be added
    */
   public void distribution(final @NotNull String key, final double value) {
-    distribution(key, value, null, null, null, 1);
+    distribution(key, value, null, null, null);
   }
 
   /**
@@ -239,7 +195,7 @@ public final class MetricsApi {
   public void distribution(
       final @NotNull String key, final double value, final @Nullable MeasurementUnit unit) {
 
-    distribution(key, value, unit, null, null, 1);
+    distribution(key, value, unit, null, null);
   }
 
   /**
@@ -256,7 +212,7 @@ public final class MetricsApi {
       final @Nullable MeasurementUnit unit,
       final @Nullable Map<String, String> tags) {
 
-    distribution(key, value, unit, tags, null, 1);
+    distribution(key, value, unit, tags, null);
   }
 
   /**
@@ -276,28 +232,6 @@ public final class MetricsApi {
       final @Nullable Map<String, String> tags,
       final @Nullable Long timestampMs) {
 
-    distribution(key, value, unit, tags, timestampMs, 1);
-  }
-
-  /**
-   * Emits a Distribution metric
-   *
-   * @param key A unique key identifying the metric
-   * @param value The value to be added
-   * @param unit An optional unit, see {@link MeasurementUnit}
-   * @param tags Optional Tags to associate with the metric
-   * @param timestampMs The time when the metric was emitted. Defaults to the time at which the
-   *     metric is emitted, if no value is provided.
-   * @param stackLevel Optional number of stacks levels to ignore when determining the code location
-   */
-  public void distribution(
-      final @NotNull String key,
-      final double value,
-      final @Nullable MeasurementUnit unit,
-      final @Nullable Map<String, String> tags,
-      final @Nullable Long timestampMs,
-      final int stackLevel) {
-
     final long timestamp = timestampMs != null ? timestampMs : System.currentTimeMillis();
     final @NotNull Map<String, String> enrichedTags =
         MetricsHelper.mergeTags(tags, aggregator.getDefaultTagsForMetrics());
@@ -306,8 +240,7 @@ public final class MetricsApi {
 
     aggregator
         .getMetricsAggregator()
-        .distribution(
-            key, value, unit, enrichedTags, timestamp, stackLevel, localMetricsAggregator);
+        .distribution(key, value, unit, enrichedTags, timestamp, localMetricsAggregator);
   }
 
   /**
@@ -317,7 +250,7 @@ public final class MetricsApi {
    * @param value The value to be added
    */
   public void set(final @NotNull String key, final int value) {
-    set(key, value, null, null, null, 1);
+    set(key, value, null, null, null);
   }
 
   /**
@@ -330,7 +263,7 @@ public final class MetricsApi {
   public void set(
       final @NotNull String key, final int value, final @Nullable MeasurementUnit unit) {
 
-    set(key, value, unit, null, null, 1);
+    set(key, value, unit, null, null);
   }
 
   /**
@@ -347,7 +280,7 @@ public final class MetricsApi {
       final @Nullable MeasurementUnit unit,
       final @Nullable Map<String, String> tags) {
 
-    set(key, value, unit, tags, null, 1);
+    set(key, value, unit, tags, null);
   }
 
   /**
@@ -367,28 +300,6 @@ public final class MetricsApi {
       final @Nullable Map<String, String> tags,
       final @Nullable Long timestampMs) {
 
-    set(key, value, unit, tags, timestampMs, 1);
-  }
-
-  /**
-   * Emits a Set metric
-   *
-   * @param key A unique key identifying the metric
-   * @param value The value to be added
-   * @param unit An optional unit, see {@link MeasurementUnit}
-   * @param tags Optional Tags to associate with the metric
-   * @param timestampMs The time when the metric was emitted. Defaults to the time at which the
-   *     metric is emitted, if no value is provided.
-   * @param stackLevel Optional number of stacks levels to ignore when determining the code location
-   */
-  public void set(
-      final @NotNull String key,
-      final int value,
-      final @Nullable MeasurementUnit unit,
-      final @Nullable Map<String, String> tags,
-      final @Nullable Long timestampMs,
-      final int stackLevel) {
-
     final long timestamp = timestampMs != null ? timestampMs : System.currentTimeMillis();
     final @NotNull Map<String, String> enrichedTags =
         MetricsHelper.mergeTags(tags, aggregator.getDefaultTagsForMetrics());
@@ -397,7 +308,7 @@ public final class MetricsApi {
 
     aggregator
         .getMetricsAggregator()
-        .set(key, value, unit, enrichedTags, timestamp, stackLevel, localMetricsAggregator);
+        .set(key, value, unit, enrichedTags, timestamp, localMetricsAggregator);
   }
 
   /**
@@ -407,7 +318,7 @@ public final class MetricsApi {
    * @param value The value to be added
    */
   public void set(final @NotNull String key, final @NotNull String value) {
-    set(key, value, null, null, null, 1);
+    set(key, value, null, null, null);
   }
 
   /**
@@ -422,7 +333,7 @@ public final class MetricsApi {
       final @NotNull String value,
       final @Nullable MeasurementUnit unit) {
 
-    set(key, value, unit, null, null, 1);
+    set(key, value, unit, null, null);
   }
 
   /**
@@ -439,7 +350,7 @@ public final class MetricsApi {
       final @Nullable MeasurementUnit unit,
       final @Nullable Map<String, String> tags) {
 
-    set(key, value, unit, tags, null, 1);
+    set(key, value, unit, tags, null);
   }
 
   /**
@@ -459,28 +370,6 @@ public final class MetricsApi {
       final @Nullable Map<String, String> tags,
       final @Nullable Long timestampMs) {
 
-    set(key, value, unit, tags, timestampMs, 1);
-  }
-
-  /**
-   * Emits a Set metric
-   *
-   * @param key A unique key identifying the metric
-   * @param value The value to be added
-   * @param unit An optional unit, see {@link MeasurementUnit}
-   * @param tags Optional Tags to associate with the metric
-   * @param timestampMs The time when the metric was emitted. Defaults to the time at which the
-   *     metric is emitted, if no value is provided.
-   * @param stackLevel Optional number of stacks levels to ignore when determining the code location
-   */
-  public void set(
-      final @NotNull String key,
-      final @NotNull String value,
-      final @Nullable MeasurementUnit unit,
-      final @Nullable Map<String, String> tags,
-      final @Nullable Long timestampMs,
-      final int stackLevel) {
-
     final long timestamp = timestampMs != null ? timestampMs : System.currentTimeMillis();
     final @NotNull Map<String, String> enrichedTags =
         MetricsHelper.mergeTags(tags, aggregator.getDefaultTagsForMetrics());
@@ -489,7 +378,7 @@ public final class MetricsApi {
 
     aggregator
         .getMetricsAggregator()
-        .set(key, value, unit, enrichedTags, timestamp, stackLevel, localMetricsAggregator);
+        .set(key, value, unit, enrichedTags, timestamp, localMetricsAggregator);
   }
 
   /**
@@ -500,7 +389,7 @@ public final class MetricsApi {
    */
   public void timing(final @NotNull String key, final @NotNull Runnable callback) {
 
-    timing(key, callback, null, null, 1);
+    timing(key, callback, null, null);
   }
 
   /**
@@ -515,7 +404,7 @@ public final class MetricsApi {
       final @NotNull Runnable callback,
       final @NotNull MeasurementUnit.Duration unit) {
 
-    timing(key, callback, unit, null, 1);
+    timing(key, callback, unit, null);
   }
 
   /**
@@ -525,31 +414,12 @@ public final class MetricsApi {
    * @param callback The code block to measure
    * @param unit An optional unit, see {@link MeasurementUnit.Duration}
    * @param tags Optional Tags to associate with the metric
-   */
-  public void timing(
-      final @NotNull String key,
-      final @NotNull Runnable callback,
-      final @NotNull MeasurementUnit.Duration unit,
-      final @Nullable Map<String, String> tags) {
-
-    timing(key, callback, unit, tags, 1);
-  }
-
-  /**
-   * Emits a distribution with the time it takes to run a given code block.
-   *
-   * @param key A unique key identifying the metric
-   * @param callback The code block to measure
-   * @param unit An optional unit, see {@link MeasurementUnit.Duration}
-   * @param tags Optional Tags to associate with the metric
-   * @param stackLevel Optional number of stacks levels to ignore when determining the code location
    */
   public void timing(
       final @NotNull String key,
       final @NotNull Runnable callback,
       final @Nullable MeasurementUnit.Duration unit,
-      final @Nullable Map<String, String> tags,
-      final int stackLevel) {
+      final @Nullable Map<String, String> tags) {
 
     final @NotNull MeasurementUnit.Duration durationUnit =
         unit != null ? unit : MeasurementUnit.Duration.SECOND;
@@ -567,7 +437,7 @@ public final class MetricsApi {
     try {
       aggregator
           .getMetricsAggregator()
-          .timing(key, callback, durationUnit, enrichedTags, stackLevel, localMetricsAggregator);
+          .timing(key, callback, durationUnit, enrichedTags, localMetricsAggregator);
     } finally {
       if (span != null) {
         span.finish();
