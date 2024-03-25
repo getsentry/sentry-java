c02f1dba4f894b7e9029c13774b4581708a63087
Roman Zavarnitsyn <rom4ek93@gmail.com>
2024-03-25 14:57:44 +0100

    Call listeners when installing RootViewsSpy

--- sentry-android-replay/src/main/java/io/sentry/android/replay/Windows.kt
+++ sentry-android-replay/src/main/java/io/sentry/android/replay/Windows.kt
@@ -139,6 +139,15 @@ internal class RootViewsSpy private constructor() {
     val listeners = CopyOnWriteArrayList<OnRootViewsChangedListener>()
 
     private val delegatingViewList = object : ArrayList<View>() {
+        override fun addAll(elements: Collection<View>): Boolean {
+            listeners.forEach { listener ->
+                elements.forEach { element ->
+                    listener.onRootViewsChanged(element, true)
+                }
+            }
+            return super.addAll(elements)
+        }
+
         override fun add(element: View): Boolean {
             listeners.forEach { it.onRootViewsChanged(element, true) }
             return super.add(element)
