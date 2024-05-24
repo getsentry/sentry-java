8e4c518d960aeb3e85622a9955eee47df53f4c0f
stefanosiano <stefano.siano@sentry.io>
2024-05-24 15:09:40 +0200

    added start timestamp in ProfileStartData when a profile starts added the timestamp in ProfilingTraceData and send it in the json payload

--- sentry-android-core/src/main/java/io/sentry/android/core/AndroidProfiler.java
+++ sentry-android-core/src/main/java/io/sentry/android/core/AndroidProfiler.java
@@ -6,6 +6,7 @@ import android.os.Debug;
 import android.os.Process;
 import android.os.SystemClock;
 import io.sentry.CpuCollectionData;
+import io.sentry.DateUtils;
 import io.sentry.ILogger;
 import io.sentry.ISentryExecutorService;
 import io.sentry.MemoryCollectionData;
@@ -17,6 +18,7 @@ import io.sentry.profilemeasurements.ProfileMeasurementValue;
 import io.sentry.util.Objects;
 import java.io.File;
 import java.util.ArrayDeque;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,10 +35,13 @@ public class AndroidProfiler {
   public static class ProfileStartData {
     public final long startNanos;
     public final long startCpuMillis;
+    public final @NotNull Date startTimestamp;
 
-    public ProfileStartData(final long startNanos, final long startCpuMillis) {
+    public ProfileStartData(
+        final long startNanos, final long startCpuMillis, final @NotNull Date startTimestamp) {
       this.startNanos = startNanos;
       this.startCpuMillis = startCpuMillis;
+      this.startTimestamp = startTimestamp;
     }
   }
 
@@ -192,6 +197,7 @@ public class AndroidProfiler {
     }
 
     profileStartNanos = SystemClock.elapsedRealtimeNanos();
+    final @NotNull Date profileStartTimestamp = DateUtils.getCurrentDateTime();
     long profileStartCpuMillis = Process.getElapsedCpuTime();
 
     // We don't make any check on the file existence or writeable state, because we don't want to
@@ -203,7 +209,7 @@ public class AndroidProfiler {
       // tests)
       Debug.startMethodTracingSampling(traceFile.getPath(), BUFFER_SIZE_BYTES, intervalUs);
       isRunning = true;
-      return new ProfileStartData(profileStartNanos, profileStartCpuMillis);
+      return new ProfileStartData(profileStartNanos, profileStartCpuMillis, profileStartTimestamp);
     } catch (Throwable e) {
       endAndCollect(false, null);
       logger.log(SentryLevel.ERROR, "Unable to start a profile: ", e);
