aa3376d28c067cd85a26804eed77d9d0aeb13fcf
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2024-05-24 10:46:12 +0200

    Update ndk sample as well

--- scripts/update-sentry-native-ndk.sh
+++ scripts/update-sentry-native-ndk.sh
@@ -2,11 +2,12 @@
 set -euo pipefail
 
 cd $(dirname "$0")/../
-GRADLE_FILEPATH=sentry-android-ndk/build.gradle.kts
+GRADLE_NDK_FILEPATH=sentry-android-ndk/build.gradle.kts
+GRADLE_SAMPLE_FILEPATH=sentry-samples/sentry-samples-android/build.gradle.kts
 
 case $1 in
 get-version)
-    version=$(perl -ne 'print "$1\n" if ( m/io\.sentry:sentry-native-ndk:([0-9.]+)+/ )' $GRADLE_FILEPATH)
+    version=$(perl -ne 'print "$1\n" if ( m/io\.sentry:sentry-native-ndk:([0-9.]+)+/ )' $GRADLE_NDK_FILEPATH)
 
     echo "v$version"
     ;;
@@ -24,7 +25,8 @@ set-version)
     echo "Setting sentry-native-ndk version to '$version'"
 
     PATTERN="io\.sentry:sentry-native-ndk:([0-9.]+)+"
-    perl -pi -e "s/$PATTERN/io.sentry:sentry-native-ndk:$version/g" $GRADLE_FILEPATH
+    perl -pi -e "s/$PATTERN/io.sentry:sentry-native-ndk:$version/g" $GRADLE_NDK_FILEPATH
+    perl -pi -e "s/$PATTERN/io.sentry:sentry-native-ndk:$version/g" $GRADLE_SAMPLE_FILEPATH
     ;;
 *)
     echo "Unknown argument $1"
