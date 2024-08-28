1ab4ec9be2ddd5b91d4df4a647d5af16cb9aaa71
Alexander Dinauer <alexander.dinauer@sentry.io>
2024-08-28 11:17:25 +0200

    Remove selective attribute copying

--- sentry-opentelemetry/sentry-opentelemetry-core/src/main/java/io/sentry/opentelemetry/SpanDescriptionExtractor.java
+++ sentry-opentelemetry/sentry-opentelemetry-core/src/main/java/io/sentry/opentelemetry/SpanDescriptionExtractor.java
@@ -5,8 +5,6 @@ import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.sdk.trace.data.SpanData;
 import io.opentelemetry.semconv.SemanticAttributes;
 import io.sentry.protocol.TransactionNameSource;
-import java.util.HashMap;
-import java.util.Map;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -14,41 +12,8 @@ import org.jetbrains.annotations.Nullable;
 @ApiStatus.Internal
 public final class SpanDescriptionExtractor {
 
-  // TODO POTEL: should we rely on the OTEL attributes, that are extracted in the exporter for the
-  // datafields?
-  // We are currently extracting some attributes and add it to the span info here
-  // In the `SentrySpanExporter` we extract all attributes and add it to the dataFields
   @SuppressWarnings("deprecation")
   public @NotNull OtelSpanInfo extractSpanInfo(
-      final @NotNull SpanData otelSpan, final @Nullable OtelSpanWrapper sentrySpan) {
-    OtelSpanInfo spanInfo = extractSpanDescription(otelSpan, sentrySpan);
-
-    final @Nullable Long threadId = otelSpan.getAttributes().get(SemanticAttributes.THREAD_ID);
-    if (threadId != null) {
-      spanInfo.addDataField("thread.id", threadId);
-    }
-
-    final @Nullable String threadName =
-        otelSpan.getAttributes().get(SemanticAttributes.THREAD_NAME);
-    if (threadName != null) {
-      spanInfo.addDataField("thread.name", threadName);
-    }
-
-    final @Nullable String dbSystem = otelSpan.getAttributes().get(SemanticAttributes.DB_SYSTEM);
-    if (dbSystem != null) {
-      spanInfo.addDataField("db.system", dbSystem);
-    }
-
-    final @Nullable String dbName = otelSpan.getAttributes().get(SemanticAttributes.DB_NAME);
-    if (dbName != null) {
-      spanInfo.addDataField("db.name", dbName);
-    }
-
-    return spanInfo;
-  }
-
-  @SuppressWarnings("deprecation")
-  private OtelSpanInfo extractSpanDescription(
       final @NotNull SpanData otelSpan, final @Nullable OtelSpanWrapper sentrySpan) {
     final @NotNull Attributes attributes = otelSpan.getAttributes();
 
@@ -82,9 +47,6 @@ public final class SpanDescriptionExtractor {
     final @NotNull SpanKind kind = otelSpan.getKind();
     final @NotNull StringBuilder opBuilder = new StringBuilder("http");
     final @NotNull Attributes attributes = otelSpan.getAttributes();
-    final @NotNull Map<String, Object> dataFields = new HashMap<>();
-
-    dataFields.put("http.request.method", httpMethod);
 
     if (SpanKind.CLIENT.equals(kind)) {
       opBuilder.append(".client");
@@ -99,20 +61,8 @@ public final class SpanDescriptionExtractor {
     }
     final @NotNull String op = opBuilder.toString();
 
-    final @Nullable Long httpStatusCode =
-        attributes.get(SemanticAttributes.HTTP_RESPONSE_STATUS_CODE);
-    if (httpStatusCode != null) {
-      dataFields.put("http.response.status_code", httpStatusCode);
-    }
-
-    final @Nullable String serverAddress = attributes.get(SemanticAttributes.SERVER_ADDRESS);
-    if (serverAddress != null) {
-      dataFields.put("server.address", serverAddress);
-    }
-
     final @Nullable String urlFull = attributes.get(SemanticAttributes.URL_FULL);
     if (urlFull != null) {
-      dataFields.put("url.full", urlFull);
       if (httpPath == null) {
         httpPath = urlFull;
       }
@@ -126,7 +76,7 @@ public final class SpanDescriptionExtractor {
     final @NotNull TransactionNameSource transactionNameSource =
         httpRoute != null ? TransactionNameSource.ROUTE : TransactionNameSource.URL;
 
-    return new OtelSpanInfo(op, description, transactionNameSource, dataFields);
+    return new OtelSpanInfo(op, description, transactionNameSource);
   }
 
   @SuppressWarnings("deprecation")
