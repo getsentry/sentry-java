6ac38d4fcf9359cef3c04dae34651c64c347eb21
Lukas Bloder <lukas.bloder@gmail.com>
2024-09-10 14:50:00 +0200

    use LazyEvaluator for SpanId creation

--- sentry/src/main/java/io/sentry/SpanId.java
+++ sentry/src/main/java/io/sentry/SpanId.java
@@ -1,26 +1,24 @@
 package io.sentry;
 
-import io.sentry.util.Objects;
+import io.sentry.util.LazyEvaluator;
 import io.sentry.util.StringUtils;
 import java.io.IOException;
 import java.util.UUID;
 import org.jetbrains.annotations.NotNull;
 
 public final class SpanId implements JsonSerializable {
-  public static final SpanId EMPTY_ID = new SpanId(new UUID(0, 0));
+  public static final SpanId EMPTY_ID = new SpanId("00000000-0000-0000-0000-000000000000");
 
-  private final @NotNull String value;
+  private final @NotNull LazyEvaluator<String> lazyValue;
 
   public SpanId(final @NotNull String value) {
-    this.value = Objects.requireNonNull(value, "value is required");
+    this.lazyValue = new LazyEvaluator<>(() -> value);
   }
 
   public SpanId() {
-    this(UUID.randomUUID());
-  }
-
-  private SpanId(final @NotNull UUID uuid) {
-    this(StringUtils.normalizeUUID(uuid.toString()).replace("-", "").substring(0, 16));
+    this.lazyValue = new LazyEvaluator<>(() ->
+      StringUtils.normalizeUUID(UUID.randomUUID().toString()).replace("-", "").substring(0, 16)
+    );
   }
 
   @Override
@@ -28,17 +26,17 @@ public final class SpanId implements JsonSerializable {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     SpanId spanId = (SpanId) o;
-    return value.equals(spanId.value);
+    return lazyValue.getValue().equals(spanId.lazyValue.getValue());
   }
 
   @Override
   public int hashCode() {
-    return value.hashCode();
+    return lazyValue.getValue().hashCode();
   }
 
   @Override
   public String toString() {
-    return this.value;
+    return lazyValue.getValue();
   }
 
   // JsonElementSerializer
@@ -46,7 +44,7 @@ public final class SpanId implements JsonSerializable {
   @Override
   public void serialize(final @NotNull ObjectWriter writer, final @NotNull ILogger logger)
       throws IOException {
-    writer.value(value);
+    writer.value(lazyValue.getValue());
   }
 
   // JsonElementDeserializer
