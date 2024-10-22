d9ac076de2dcc3c753617ddcc74f5b20b5e0c81a
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-10-22 14:46:40 +0200

    test(critical): Add Mock Relay to verify crash envelope

--- scripts/test-ui-critical.sh
+++ scripts/test-ui-critical.sh
@@ -19,6 +19,9 @@ if ! command -v maestro &> /dev/null; then
     exit 1
 fi
 
+echo "Building the mock relay..."
+make buildMockRelay
+
 echo "Building the UI Test Critical app..."
 make assembleUiTestCriticalRelease
 
@@ -29,7 +32,24 @@ apkName="sentry-uitest-android-critical-release.apk"
 appPath="${baseDir}/${buildDir}/${apkName}"
 adb install -r -d "$appPath"
 
+echo "Starting the mock relay..."
+cd sentry-android-integration-tests/sentry-mock-relay/build/distributions
+unzip -o sentry-mock-relay-0.0.1.zip
+./sentry-mock-relay-0.0.1/bin/sentry-mock-relay > /dev/null & MOCK_RELAY_PID=$!
+echo "Mock relay PID: $MOCK_RELAY_PID"
+
+set +e
 echo "Running the Maestro tests..."
 maestro test \
   "${baseDir}/maestro" \
   --debug-output "${baseDir}/maestro-logs"
+MAESTRO_EXIT_CODE=$?
+
+echo "Checking mock relay results..."
+curl --fail http://localhost:8961/assertReceivedAtLeastOneCrashReport
+MOCK_RELAY_EXIT_CODE=$?
+
+echo "Stopping the mock relay..."
+kill $MOCK_RELAY_PID
+
+exit $(($MAESTRO_EXIT_CODE || $MOCK_RELAY_EXIT_CODE))
