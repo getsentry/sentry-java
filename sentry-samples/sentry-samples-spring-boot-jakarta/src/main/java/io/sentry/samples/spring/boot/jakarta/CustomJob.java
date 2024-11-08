eff23d56f6d6ef2489ee87889800bfd3a4cbe026
Alexander Dinauer <alexander.dinauer@sentry.io>
2024-11-08 12:19:55 +0100

    POTEL 54 cleanup

--- sentry-samples/sentry-samples-spring-boot-jakarta/src/main/java/io/sentry/samples/spring/boot/jakarta/CustomJob.java
+++ sentry-samples/sentry-samples-spring-boot-jakarta/src/main/java/io/sentry/samples/spring/boot/jakarta/CustomJob.java
@@ -4,6 +4,7 @@ import io.sentry.spring.jakarta.checkin.SentryCheckIn;
 import io.sentry.spring.jakarta.tracing.SentryTransaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -17,7 +18,7 @@ public class CustomJob {
   private static final Logger LOGGER = LoggerFactory.getLogger(CustomJob.class);
 
   @SentryCheckIn("monitor_slug_1")
-  //  @Scheduled(fixedRate = 3 * 60 * 1000L)
+  @Scheduled(fixedRate = 3 * 60 * 1000L)
   void execute() throws InterruptedException {
     LOGGER.info("Executing scheduled job");
     Thread.sleep(2000L);
