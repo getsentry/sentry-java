64bb1852b8c801180dc8a5f8aa1664970ed9d4a5
Alexander Dinauer <adinauer@users.noreply.github.com>
2024-11-20 11:51:01 +0100

    fix e2e test run script (#3908)
    * Send otel.kind to Sentry
    
    * fix run script for e2e tests
    
    * fix sample

--- sentry-samples/sentry-samples-spring-boot-jakarta/src/main/java/io/sentry/samples/spring/boot/jakarta/SentryDemoApplication.java
+++ sentry-samples/sentry-samples-spring-boot-jakarta/src/main/java/io/sentry/samples/spring/boot/jakarta/SentryDemoApplication.java
@@ -1,10 +1,19 @@
 package io.sentry.samples.spring.boot.jakarta;
 
+import static io.sentry.quartz.SentryJobListener.SENTRY_SLUG_KEY;
+
+import io.sentry.samples.spring.boot.jakarta.quartz.SampleJob;
+import java.util.Collections;
+import org.quartz.JobDetail;
+import org.quartz.SimpleTrigger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
+import org.springframework.scheduling.quartz.SimpleTriggerFactoryBean;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -31,32 +40,32 @@ public class SentryDemoApplication {
     return builder.build();
   }
 
-  //  @Bean
-  //  public JobDetailFactoryBean jobDetail() {
-  //    JobDetailFactoryBean jobDetailFactory = new JobDetailFactoryBean();
-  //    jobDetailFactory.setJobClass(SampleJob.class);
-  //    jobDetailFactory.setDurability(true);
-  //    jobDetailFactory.setJobDataAsMap(
-  //        Collections.singletonMap(SENTRY_SLUG_KEY, "monitor_slug_job_detail"));
-  //    return jobDetailFactory;
-  //  }
-  //
-  //  @Bean
-  //  public SimpleTriggerFactoryBean trigger(JobDetail job) {
-  //    SimpleTriggerFactoryBean trigger = new SimpleTriggerFactoryBean();
-  //    trigger.setJobDetail(job);
-  //    trigger.setRepeatInterval(2 * 60 * 1000); // every two minutes
-  //    trigger.setRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY);
-  //    trigger.setJobDataAsMap(
-  //        Collections.singletonMap(SENTRY_SLUG_KEY, "monitor_slug_simple_trigger"));
-  //    return trigger;
-  //  }
-  //
-  //  @Bean
-  //  public CronTriggerFactoryBean cronTrigger(JobDetail job) {
-  //    CronTriggerFactoryBean trigger = new CronTriggerFactoryBean();
-  //    trigger.setJobDetail(job);
-  //    trigger.setCronExpression("0 0/5 * ? * *"); // every five minutes
-  //    return trigger;
-  //  }
+  @Bean
+  public JobDetailFactoryBean jobDetail() {
+    JobDetailFactoryBean jobDetailFactory = new JobDetailFactoryBean();
+    jobDetailFactory.setJobClass(SampleJob.class);
+    jobDetailFactory.setDurability(true);
+    jobDetailFactory.setJobDataAsMap(
+        Collections.singletonMap(SENTRY_SLUG_KEY, "monitor_slug_job_detail"));
+    return jobDetailFactory;
+  }
+
+  @Bean
+  public SimpleTriggerFactoryBean trigger(JobDetail job) {
+    SimpleTriggerFactoryBean trigger = new SimpleTriggerFactoryBean();
+    trigger.setJobDetail(job);
+    trigger.setRepeatInterval(2 * 60 * 1000); // every two minutes
+    trigger.setRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY);
+    trigger.setJobDataAsMap(
+        Collections.singletonMap(SENTRY_SLUG_KEY, "monitor_slug_simple_trigger"));
+    return trigger;
+  }
+
+  @Bean
+  public CronTriggerFactoryBean cronTrigger(JobDetail job) {
+    CronTriggerFactoryBean trigger = new CronTriggerFactoryBean();
+    trigger.setJobDetail(job);
+    trigger.setCronExpression("0 0/5 * ? * *"); // every five minutes
+    return trigger;
+  }
 }
