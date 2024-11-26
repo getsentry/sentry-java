2d79b45b377b70ed757cb989250bdf3d523c6236
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2024-11-26 15:44:16 +0100

    Fix use getDeclaredField instead of getField

--- sentry-compose-helper/src/jvmMain/java/io/sentry/compose/gestures/ComposeGestureTargetLocator.java
+++ sentry-compose-helper/src/jvmMain/java/io/sentry/compose/gestures/ComposeGestureTargetLocator.java
@@ -105,7 +105,7 @@ public final class ComposeGestureTargetLocator implements GestureTargetLocator {
               // See
               // https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/platform/TestTag.kt;l=34;drc=dcaa116fbfda77e64a319e1668056ce3b032469f
               try {
-                final Field tagField = modifier.getClass().getField("tag");
+                final Field tagField = modifier.getClass().getDeclaredField("tag");
                 tagField.setAccessible(true);
                 final @Nullable Object value = tagField.get(modifier);
                 if (value instanceof String) {
