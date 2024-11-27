25b57d143d3028016e99aa5689fbea272030a2c2
Alexander Dinauer <alexander.dinauer@sentry.io>
2024-11-27 09:42:13 +0100

    Extract OTel URL_PATH span attribute into description

--- sentry-opentelemetry/sentry-opentelemetry-core/src/main/java/io/sentry/opentelemetry/SpanDescriptionExtractor.java
+++ sentry-opentelemetry/sentry-opentelemetry-core/src/main/java/io/sentry/opentelemetry/SpanDescriptionExtractor.java
@@ -68,10 +68,18 @@ public final class SpanDescriptionExtractor {
       httpPath = httpTarget;
     }
     final @NotNull String op = opBuilder.toString();
+
     final @Nullable String urlFull = attributes.get(UrlAttributes.URL_FULL);
     if (urlFull != null) {
       if (httpPath == null) {
         httpPath = urlFull;
+      }
+    }
+
+    final @Nullable String urlPath = attributes.get(UrlAttributes.URL_PATH);
+    if (urlPath != null) {
+      if (httpPath == null) {
+        httpPath = urlPath;
       }
     }
 
