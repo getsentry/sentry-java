7470ec50303d25d93c1c201ec8247295a32e7071
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2024-11-28 08:29:24 +0100

    Apply suggestions from code review
    Co-authored-by: Roman Zavarnitsyn <rom4ek93@gmail.com>

--- sentry-compose-helper/build.gradle.kts
+++ sentry-compose-helper/build.gradle.kts
@@ -21,7 +21,7 @@ kotlin {
                 compileOnly(compose.runtime)
                 compileOnly(compose.ui)
 
-                implementation("androidx.annotation:annotation:1.9.1")
+                compileOnly("androidx.annotation:annotation:1.9.1")
             }
         }
         val jvmTest by getting {
