a90f3164e0641efff997ac99dd6277e500cc7336
Alexander Dinauer <alexander.dinauer@sentry.io>
2024-12-02 11:18:31 +0100

    make todo url configurable

--- sentry-samples/sentry-samples-spring-boot-jakarta-opentelemetry/src/main/java/io/sentry/samples/spring/boot/jakarta/TodoController.java
+++ sentry-samples/sentry-samples-spring-boot-jakarta-opentelemetry/src/main/java/io/sentry/samples/spring/boot/jakarta/TodoController.java
@@ -7,6 +7,8 @@ import io.sentry.ISpan;
 import io.sentry.Sentry;
 import io.sentry.spring.jakarta.webflux.ReactorUtils;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +26,9 @@ public class TodoController {
   private final RestClient restClient;
   private final Tracer tracer;
 
+  @Value("sentry.sample.todo-url")
+  public @Nullable String todoUrl;
+
   public TodoController(
       RestTemplate restTemplate, WebClient webClient, RestClient restClient, Tracer tracer) {
     this.restTemplate = restTemplate;
@@ -38,8 +43,7 @@ public class TodoController {
     try (final @NotNull Scope spanScope = otelSpan.makeCurrent()) {
       ISpan sentrySpan = Sentry.getSpan().startChild("todoSpanSentryApi");
       try {
-        return restTemplate.getForObject(
-            "https://jsonplaceholder.typicode.com/todos/{id}", Todo.class, id);
+        return restTemplate.getForObject(todoUrl + "/todos/{id}", Todo.class, id);
       } finally {
         sentrySpan.finish();
       }
@@ -58,7 +62,7 @@ public class TodoController {
                     x ->
                         webClient
                             .get()
-                            .uri("https://jsonplaceholder.typicode.com/todos/{id}", id)
+                            .uri(todoUrl + "/todos/{id}", id)
                             .retrieve()
                             .bodyToMono(Todo.class)
                             .map(response -> response)))
@@ -73,7 +77,7 @@ public class TodoController {
       try {
         return restClient
             .get()
-            .uri("https://jsonplaceholder.typicode.com/todos/{id}", id)
+            .uri(todoUrl + "/todos/{id}", id)
             .retrieve()
             .body(Todo.class);
       } finally {
