4bf8acd9c48bc3a8848f1b516a5daeb77375954d
Roman Zavarnitsyn <rom4ek93@gmail.com>
2024-12-30 22:24:19 +0100

    Add a comment

--- sentry/src/main/java/io/sentry/SentryClient.java
+++ sentry/src/main/java/io/sentry/SentryClient.java
@@ -640,6 +640,8 @@ public final class SentryClient implements ISentryClient, IMetricsClient {
     envelopeItems.add(replayItem);
     final SentryId sentryId = event.getEventId();
 
+    // SdkVersion from ReplayOptions defaults to SdkVersion from SentryOptions and can be
+    // overwritten by the hybrid SDKs
     final SentryEnvelopeHeader envelopeHeader =
         new SentryEnvelopeHeader(
             sentryId, options.getExperimental().getSessionReplay().getSdkVersion(), traceContext);
