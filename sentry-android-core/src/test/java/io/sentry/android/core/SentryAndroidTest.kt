43ac542e94b329b91d750e5711c40dc0a11dc886
Roman Zavarnitsyn <rom4ek93@gmail.com>
2025-01-14 17:03:42 +0100

    Drop TempSesnorBreadcrumbIntegration

--- sentry-android-core/src/test/java/io/sentry/android/core/SentryAndroidTest.kt
+++ sentry-android-core/src/test/java/io/sentry/android/core/SentryAndroidTest.kt
@@ -461,7 +461,7 @@ class SentryAndroidTest {
         fixture.initSut(context = mock<Application>()) { options ->
             optionsRef = options
             options.dsn = "https://key@sentry.io/123"
-            assertEquals(21, options.integrations.size)
+            assertEquals(20, options.integrations.size)
             options.integrations.removeAll {
                 it is UncaughtExceptionHandlerIntegration ||
                     it is ShutdownHookIntegration ||
@@ -479,7 +479,6 @@ class SentryAndroidTest {
                     it is AppComponentsBreadcrumbsIntegration ||
                     it is SystemEventsBreadcrumbsIntegration ||
                     it is NetworkBreadcrumbsIntegration ||
-                    it is TempSensorBreadcrumbsIntegration ||
                     it is PhoneStateBreadcrumbsIntegration ||
                     it is SpotlightIntegration ||
                     it is ReplayIntegration
