c7c5e466d0c8d1367367a167188142336ca582d7
Roman Zavarnitsyn <rom4ek93@gmail.com>
2025-02-04 14:03:47 +0100

    Revert toLowerCase -> lowercase

--- sentry-android-replay/src/main/java/io/sentry/android/replay/viewhierarchy/ViewHierarchyNode.kt
+++ sentry-android-replay/src/main/java/io/sentry/android/replay/viewhierarchy/ViewHierarchyNode.kt
@@ -251,13 +251,13 @@ internal sealed class ViewHierarchyNode(
         }
 
         private fun View.shouldMask(options: SentryOptions): Boolean {
-            if ((tag as? String)?.toLowerCase(Locale.ROOT)?.contains(SENTRY_UNMASK_TAG) == true ||
+            if ((tag as? String)?.lowercase()?.contains(SENTRY_UNMASK_TAG) == true ||
                 getTag(R.id.sentry_privacy) == "unmask"
             ) {
                 return false
             }
 
-            if ((tag as? String)?.toLowerCase(Locale.ROOT)?.contains(SENTRY_MASK_TAG) == true ||
+            if ((tag as? String)?.lowercase()?.contains(SENTRY_MASK_TAG) == true ||
                 getTag(R.id.sentry_privacy) == "mask"
             ) {
                 return true
