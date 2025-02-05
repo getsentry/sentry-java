a4b2d6615570e1f72acf49b70a2ef34a51c2ad93
bitsandfoxes <reg@bitfox.at>
2025-02-05 18:52:45 +0100

    added  API taking traceId and spanID

--- sentry/src/main/java/io/sentry/JsonSerializer.java
+++ sentry/src/main/java/io/sentry/JsonSerializer.java
@@ -200,8 +200,8 @@ public final class JsonSerializer implements ISerializer {
     Objects.requireNonNull(writer, "The Writer object is required.");
 
     if (options.getLogger().isEnabled(SentryLevel.DEBUG)) {
-      String serialized = serializeToString(entity, options.isEnablePrettySerializationOutput());
-      options.getLogger().log(SentryLevel.DEBUG, "Serializing object: %s", serialized);
+//      String serialized = serializeToString(entity, options.isEnablePrettySerializationOutput());
+//      options.getLogger().log(SentryLevel.DEBUG, "Serializing object: %s", serialized);
     }
     JsonObjectWriter jsonObjectWriter = new JsonObjectWriter(writer, options.getMaxDepth());
     jsonObjectWriter.value(options.getLogger(), entity);
