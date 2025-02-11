d714dc82d38b5663d43ec5ee72aa590303b0f29f
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2025-02-11 15:56:22 +0100

    feat(android-ndk): add api for getting debug images by addresses (#4159)
    * feat(android-ndk): add api for getting debug images by addresses (#4089)
    
    ---------
    
    Co-authored-by: Sentry Github Bot <bot+github-bot@sentry.io>
    Co-authored-by: Markus Hintersteiner <markus.hintersteiner@sentry.io>
    
    * Update Changelog
    
    * Format code
    
    * Fix switch sync/data classes to match 7.x.x
    
    ---------
    
    Co-authored-by: Giancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
    Co-authored-by: Sentry Github Bot <bot+github-bot@sentry.io>

--- sentry-android-ndk/src/main/java/io/sentry/android/ndk/DebugImagesLoader.java
+++ sentry-android-ndk/src/main/java/io/sentry/android/ndk/DebugImagesLoader.java
@@ -7,7 +7,9 @@ import io.sentry.android.core.SentryAndroidOptions;
 import io.sentry.protocol.DebugImage;
 import io.sentry.util.Objects;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.VisibleForTesting;
@@ -22,7 +24,7 @@ public final class DebugImagesLoader implements IDebugImagesLoader {
 
   private final @NotNull NativeModuleListLoader moduleListLoader;
 
-  private static @Nullable List<DebugImage> debugImages;
+  private static volatile @Nullable List<DebugImage> debugImages;
 
   /** we need to lock it because it could be called from different threads */
   private static final @NotNull Object debugImagesLock = new Object();
@@ -60,7 +62,92 @@ public final class DebugImagesLoader implements IDebugImagesLoader {
     return debugImages;
   }
 
-  /** Clears the caching of debug images on sentry-native and here. */
+  /**
+   * Loads debug images for the given set of addresses.
+   *
+   * @param addresses Set of memory addresses to find debug images for
+   * @return Set of matching debug images, or null if debug images couldn't be loaded
+   */
+  public @Nullable Set<DebugImage> loadDebugImagesForAddresses(
+      final @NotNull Set<String> addresses) {
+    synchronized (debugImagesLock) {
+      final @Nullable List<DebugImage> allDebugImages = loadDebugImages();
+      if (allDebugImages == null) {
+        return null;
+      }
+      if (addresses.isEmpty()) {
+        return null;
+      }
+
+      final Set<DebugImage> referencedImages = filterImagesByAddresses(allDebugImages, addresses);
+      if (referencedImages.isEmpty()) {
+        options
+            .getLogger()
+            .log(
+                SentryLevel.WARNING,
+                "No debug images found for any of the %d addresses.",
+                addresses.size());
+        return null;
+      }
+
+      return referencedImages;
+    }
+  }
+
+  /**
+   * Finds all debug image containing the given addresses. Assumes that the images are sorted by
+   * address, which should always be true on Linux/Android and Windows platforms
+   *
+   * @return All matching debug images or null if none are found
+   */
+  private @NotNull Set<DebugImage> filterImagesByAddresses(
+      final @NotNull List<DebugImage> images, final @NotNull Set<String> addresses) {
+    final Set<DebugImage> result = new HashSet<>();
+
+    for (int i = 0; i < images.size(); i++) {
+      final @NotNull DebugImage image = images.get(i);
+      final @Nullable DebugImage nextDebugImage =
+          (i + 1) < images.size() ? images.get(i + 1) : null;
+      final @Nullable String nextDebugImageAddress =
+          nextDebugImage != null ? nextDebugImage.getImageAddr() : null;
+
+      for (final @NotNull String rawAddress : addresses) {
+        try {
+          final long address = Long.parseLong(rawAddress.replace("0x", ""), 16);
+
+          final @Nullable String imageAddress = image.getImageAddr();
+          if (imageAddress != null) {
+            try {
+              final long imageStart = Long.parseLong(imageAddress.replace("0x", ""), 16);
+              final long imageEnd;
+
+              final @Nullable Long imageSize = image.getImageSize();
+              if (imageSize != null) {
+                imageEnd = imageStart + imageSize;
+              } else if (nextDebugImageAddress != null) {
+                imageEnd = Long.parseLong(nextDebugImageAddress.replace("0x", ""), 16);
+              } else {
+                imageEnd = Long.MAX_VALUE;
+              }
+              if (address >= imageStart && address < imageEnd) {
+                result.add(image);
+                // once image is added we can skip the remaining addresses and go straight to the
+                // next
+                // image
+                break;
+              }
+            } catch (NumberFormatException e) {
+              // ignored, invalid debug image address
+            }
+          }
+        } catch (NumberFormatException e) {
+          // ignored, invalid address supplied
+        }
+      }
+    }
+    return result;
+  }
+
   @Override
   public void clearDebugImages() {
     synchronized (debugImagesLock) {
--- sentry-android-ndk/src/test/java/io/sentry/android/ndk/DebugImagesLoaderTest.kt
+++ sentry-android-ndk/src/test/java/io/sentry/android/ndk/DebugImagesLoaderTest.kt
@@ -5,8 +5,8 @@ import io.sentry.protocol.DebugImage
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
-import java.lang.RuntimeException
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
@@ -17,11 +17,13 @@ class DebugImagesLoaderTest {
         val options = SentryAndroidOptions()
 
         fun getSut(): DebugImagesLoader {
-            return DebugImagesLoader(options, nativeLoader)
+            val loader = DebugImagesLoader(options, nativeLoader)
+            loader.clearDebugImages()
+            return loader
         }
     }
 
-    private val fixture = Fixture()
+    private var fixture = Fixture()
 
     @Test
     fun `get images returns image list`() {
@@ -77,5 +79,108 @@ class DebugImagesLoaderTest {
         sut.clearDebugImages()
 
         assertNull(sut.cachedDebugImages)
+    }
+
+    @Test
+    fun `find images by address`() {
+        val sut = fixture.getSut()
+
+        val image1 = DebugImage().apply {
+            imageAddr = "0x1000"
+            imageSize = 0x1000L
+        }
+
+        val image2 = DebugImage().apply {
+            imageAddr = "0x2000"
+            imageSize = 0x1000L
+        }
+
+        val image3 = DebugImage().apply {
+            imageAddr = "0x3000"
+            imageSize = 0x1000L
+        }
+
+        whenever(fixture.nativeLoader.loadModuleList()).thenReturn(arrayOf(image1, image2, image3))
+
+        val result = sut.loadDebugImagesForAddresses(
+            setOf("0x1500", "0x2500")
+        )
+
+        assertNotNull(result)
+        assertEquals(2, result.size)
+        assertTrue(result.any { it.imageAddr == image1.imageAddr })
+        assertTrue(result.any { it.imageAddr == image2.imageAddr })
+    }
+
+    @Test
+    fun `find images with invalid addresses are not added to the result`() {
+        val sut = fixture.getSut()
+
+        val image1 = DebugImage().apply {
+            imageAddr = "0x1000"
+            imageSize = 0x1000L
+        }
+
+        val image2 = DebugImage().apply {
+            imageAddr = "0x2000"
+            imageSize = 0x1000L
+        }
+
+        whenever(fixture.nativeLoader.loadModuleList()).thenReturn(arrayOf(image1, image2))
+
+        val hexAddresses = setOf("0xINVALID", "0x1500")
+        val result = sut.loadDebugImagesForAddresses(hexAddresses)
+
+        assertEquals(1, result!!.size)
+    }
+
+    @Test
+    fun `find images by address returns null if result is empty`() {
+        val sut = fixture.getSut()
+
+        val image1 = DebugImage().apply {
+            imageAddr = "0x1000"
+            imageSize = 0x1000L
+        }
+
+        val image2 = DebugImage().apply {
+            imageAddr = "0x2000"
+            imageSize = 0x1000L
+        }
+
+        whenever(fixture.nativeLoader.loadModuleList()).thenReturn(arrayOf(image1, image2))
+
+        val hexAddresses = setOf("0x100", "0x10500")
+        val result = sut.loadDebugImagesForAddresses(hexAddresses)
+
+        assertNull(result)
+    }
+
+    @Test
+    fun `invalid image addresses are ignored for loadDebugImagesForAddresses`() {
+        val sut = fixture.getSut()
+
+        val image1 = DebugImage().apply {
+            imageAddr = "0xNotANumber"
+            imageSize = 0x1000L
+        }
+
+        val image2 = DebugImage().apply {
+            imageAddr = "0x2000"
+            imageSize = null
+        }
+
+        val image3 = DebugImage().apply {
+            imageAddr = "0x5000"
+            imageSize = 0x1000L
+        }
+
+        whenever(fixture.nativeLoader.loadModuleList()).thenReturn(arrayOf(image1, image2, image3))
+
+        val hexAddresses = setOf("0x100", "0x2000", "0x2000", "0x5000")
+        val result = sut.loadDebugImagesForAddresses(hexAddresses)
+
+        assertNotNull(result)
+        assertEquals(2, result.size)
     }
 }
