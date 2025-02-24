de1d908a3274a9a681d840d64d12084dadbeb663
lcian <lorenzo.cian@sentry.io>
2025-02-24 11:28:42 +0100

    refactor: use `java.net.URI` for parsing in `UrlUtils`

--- sentry/src/test/java/io/sentry/util/UrlUtilsTest.kt
+++ sentry/src/test/java/io/sentry/util/UrlUtilsTest.kt
@@ -1,5 +1,6 @@
 package io.sentry.util
 
+import java.net.URL
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
@@ -9,6 +10,32 @@ class UrlUtilsTest {
     @Test
     fun `returns null for null`() {
         assertNull(UrlUtils.parseNullable(null))
+    }
+
+    @Test
+    fun `i null for null`() {
+        val url = URL("https://www.example.com/search?q=hello%20world%21")
+        var uri = url.toURI()
+    }
+
+    @Test
+    fun `filters auth`() {
+        val urlDetails = UrlUtils.parseNullable(
+            "https://user:password@sentry.io?q=1&s=2&token=secret"
+        )!!
+        assertEquals("https://[Filtered]:[Filtered]@sentry.io", urlDetails.url)
+        assertEquals("q=1&s=2&token=secret", urlDetails.query)
+        assertNull(urlDetails.fragment)
+    }
+
+    @Test
+    fun `filters auth with fragment`() {
+        val urlDetails = UrlUtils.parseNullable(
+            "https://user:password@sentry.io?q=1&s=2&token=secret#top"
+        )!!
+        assertEquals("https://[Filtered]:[Filtered]@sentry.io", urlDetails.url)
+        assertEquals("q=1&s=2&token=secret", urlDetails.query)
+        assertEquals("top", urlDetails.fragment)
     }
 
     @Test
@@ -161,20 +188,62 @@ class UrlUtilsTest {
         assertEquals("top", urlDetails.fragment)
     }
 
+    // Fragment is allowed to contain '?' according to RFC 3986
+    // See https://datatracker.ietf.org/doc/html/rfc3986#section-3.5
     @Test
-    fun `no details extracted with query after fragment`() {
+    fun `extracts details with question mark after fragment`() {
         val urlDetails = UrlUtils.parse(
             "https://user:password@sentry.io#fragment?q=1&s=2&token=secret"
         )
-        assertNull(urlDetails.url)
+        assertEquals("https://[Filtered]:[Filtered]@sentry.io", urlDetails.url)
         assertNull(urlDetails.query)
-        assertNull(urlDetails.fragment)
+        assertEquals("fragment?q=1&s=2&token=secret", urlDetails.fragment)
     }
 
     @Test
-    fun `no details extracted with query after fragment without authority`() {
+    fun `extracts details with question mark after fragment without user info`() {
         val urlDetails = UrlUtils.parse(
             "https://sentry.io#fragment?q=1&s=2&token=secret"
+        )
+        assertEquals("https://sentry.io", urlDetails.url)
+        assertNull(urlDetails.query)
+        assertEquals("fragment?q=1&s=2&token=secret", urlDetails.fragment)
+    }
+
+    @Test
+    fun `filters empty user info`() {
+        val urlDetails = UrlUtils.parse(
+            "https://@sentry.io?query=a#fragment?q=1&s=2&token=secret"
+        )
+        assertEquals("https://[Filtered]@sentry.io", urlDetails.url)
+        assertEquals("query=a", urlDetails.query)
+        assertEquals("fragment?q=1&s=2&token=secret", urlDetails.fragment)
+    }
+
+    @Test
+    fun `extracts details from relative url with leading @ symbol`() {
+        val urlDetails = UrlUtils.parse(
+            "@@sentry.io/pages/10?query=a#fragment?q=1&s=2&token=secret"
+        )
+        assertEquals("@@sentry.io/pages/10", urlDetails.url)
+        assertEquals("query=a", urlDetails.query)
+        assertEquals("fragment?q=1&s=2&token=secret", urlDetails.fragment)
+    }
+
+    @Test
+    fun `extracts details from relative url with leading question mark`() {
+        val urlDetails = UrlUtils.parse(
+            "?query=a#fragment?q=1&s=2&token=secret"
+        )
+        assertEquals("", urlDetails.url)
+        assertEquals("query=a", urlDetails.query)
+        assertEquals("fragment?q=1&s=2&token=secret", urlDetails.fragment)
+    }
+
+    @Test
+    fun `no details extracted from malformed url due to # symbol in fragment`() {
+        val urlDetails = UrlUtils.parse(
+            "https://example.com#hello#fragment"
         )
         assertNull(urlDetails.url)
         assertNull(urlDetails.query)
@@ -182,12 +251,100 @@ class UrlUtilsTest {
     }
 
     @Test
-    fun `no details extracted from malformed url`() {
+    fun `no details extracted from malformed url due to invalid protocol`() {
         val urlDetails = UrlUtils.parse(
             "htps://user@sentry.io#fragment?q=1&s=2&token=secret"
         )
         assertNull(urlDetails.url)
         assertNull(urlDetails.query)
         assertNull(urlDetails.fragment)
+    }
+
+    @Test
+    fun `does not filter email address in path`() {
+        val urlDetails = UrlUtils.parseNullable(
+            "https://staging.server.com/api/v4/auth/password/reset/email@example.com"
+        )!!
+        assertEquals("https://staging.server.com/api/v4/auth/password/reset/email@example.com", urlDetails.url)
+        assertNull(urlDetails.query)
+        assertNull(urlDetails.fragment)
+    }
+
+    @Test
+    fun `does not filter email address in path with fragment`() {
+        val urlDetails = UrlUtils.parseNullable(
+            "https://staging.server.com/api/v4/auth/password/reset/email@example.com#top"
+        )!!
+        assertEquals("https://staging.server.com/api/v4/auth/password/reset/email@example.com", urlDetails.url)
+        assertNull(urlDetails.query)
+        assertEquals("top", urlDetails.fragment)
+    }
+
+    @Test
+    fun `does not filter email address in path with query and fragment`() {
+        val urlDetails = UrlUtils.parseNullable(
+            "https://staging.server.com/api/v4/auth/password/reset/email@example.com?a=b&c=d#top"
+        )!!
+        assertEquals("https://staging.server.com/api/v4/auth/password/reset/email@example.com", urlDetails.url)
+        assertEquals("a=b&c=d", urlDetails.query)
+        assertEquals("top", urlDetails.fragment)
+    }
+
+    @Test
+    fun `does not filter email address in query`() {
+        val urlDetails = UrlUtils.parseNullable(
+            "https://staging.server.com/?email=someone@example.com"
+        )!!
+        assertEquals("https://staging.server.com/", urlDetails.url)
+        assertEquals("email=someone@example.com", urlDetails.query)
+    }
+
+    @Test
+    fun `does not filter email address in fragment`() {
+        val urlDetails = UrlUtils.parseNullable(
+            "https://staging.server.com#email=someone@example.com"
+        )!!
+        assertEquals("https://staging.server.com", urlDetails.url)
+        assertEquals("email=someone@example.com", urlDetails.fragment)
+    }
+
+    @Test
+    fun `does not filter email address in fragment with query`() {
+        val urlDetails = UrlUtils.parseNullable(
+            "https://staging.server.com?q=a&b=c#email=someone@example.com"
+        )!!
+        assertEquals("https://staging.server.com", urlDetails.url)
+        assertEquals("q=a&b=c", urlDetails.query)
+        assertEquals("email=someone@example.com", urlDetails.fragment)
+    }
+
+    @Test
+    fun `extracts details from relative url with email in path`() {
+        val urlDetails = UrlUtils.parse(
+            "/emails/user@sentry.io?query=a&b=c#fragment?q=1&s=2&token=secret"
+        )
+        assertEquals("/emails/user@sentry.io", urlDetails.url)
+        assertEquals("query=a&b=c", urlDetails.query)
+        assertEquals("fragment?q=1&s=2&token=secret", urlDetails.fragment)
+    }
+
+    @Test
+    fun `extracts details from relative url with email in query`() {
+        val urlDetails = UrlUtils.parse(
+            "users/10?email=user@sentry.io&b=c#fragment?q=1&s=2&token=secret"
+        )
+        assertEquals("users/10", urlDetails.url)
+        assertEquals("email=user@sentry.io&b=c", urlDetails.query)
+        assertEquals("fragment?q=1&s=2&token=secret", urlDetails.fragment)
+    }
+
+    @Test
+    fun `extracts details from relative url with email in fragment`() {
+        val urlDetails = UrlUtils.parse(
+            "users/10?email=user@sentry.io&b=c#fragment?q=1&s=2&email=user@sentry.io"
+        )
+        assertEquals("users/10", urlDetails.url)
+        assertEquals("email=user@sentry.io&b=c", urlDetails.query)
+        assertEquals("fragment?q=1&s=2&email=user@sentry.io", urlDetails.fragment)
     }
 }
