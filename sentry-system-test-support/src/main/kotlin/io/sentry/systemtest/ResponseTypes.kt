5eb3279befbe47857eef30b7d32e54cbf36e1d70
Alexander Dinauer <adinauer@users.noreply.github.com>
2025-03-11 13:25:51 +0100

    Replace RestTemplate with OkHttp for system tests (#4239)
    * Also use port when checking if a request is made to Sentry DSN
    
    * changelog
    
    * Add a param to control whether the test script should rebuild before running the tested server
    
    * Add system tests for distributed tracing
    
    * reuse util classes for system tests
    
    * add schema
    
    * Add distributed tracing tests to more modules
    
    * use mono.just for post body
    
    * Replace RestTemplate with OkHttp in system tests
    
    * Format code
    
    * format + api
    
    * Update buildSrc/src/main/java/Config.kt
    
    ---------
    
    Co-authored-by: Sentry Github Bot <bot+github-bot@sentry.io>

--- sentry-system-test-support/src/main/kotlin/io/sentry/systemtest/ResponseTypes.kt
+++ sentry-system-test-support/src/main/kotlin/io/sentry/systemtest/ResponseTypes.kt
@@ -1,6 +1,6 @@
 package io.sentry.systemtest
 
-data class Todo(val id: Long, val title: String, val isCompleted: Boolean)
+data class Todo(val id: Long, val title: String, val completed: Boolean)
 
 data class Person(val firstName: String, val lastName: String) {
 
