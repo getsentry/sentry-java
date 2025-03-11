251127d02e91c1c51c28e2a2c94d2d6485bafadb
Alexander Dinauer <adinauer@users.noreply.github.com>
2025-03-11 10:56:14 +0100

    Reuse util classes for System tests (#4236)
    * Also use port when checking if a request is made to Sentry DSN
    
    * changelog
    
    * Add a param to control whether the test script should rebuild before running the tested server
    
    * Add system tests for distributed tracing
    
    * reuse util classes for system tests
    
    * add schema

--- sentry-samples/sentry-samples-spring-boot-opentelemetry-noagent/build.gradle.kts
+++ sentry-samples/sentry-samples-spring-boot-opentelemetry-noagent/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     id(Config.BuildPlugins.springDependencyManagement) version Config.BuildPlugins.springDependencyManagementVersion
     kotlin("jvm")
     kotlin("plugin.spring") version Config.kotlinVersion
-    id("com.apollographql.apollo3") version "3.8.2"
 }
 
 group = "io.sentry.sample.spring-boot"
@@ -57,6 +56,8 @@ dependencies {
     // database query tracing
     implementation(projects.sentryJdbc)
     runtimeOnly(Config.TestLibs.hsqldb)
+
+    testImplementation(projects.sentrySystemTestSupport)
     testImplementation(Config.Libs.springBootStarterTest) {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
@@ -102,15 +103,5 @@ tasks.named("test").configure {
 
     filter {
         excludeTestsMatching("io.sentry.systemtest.*")
-    }
-}
-
-apollo {
-    service("service") {
-        srcDir("src/test/graphql")
-        packageName.set("io.sentry.samples.graphql")
-        outputDirConnection {
-            connectToKotlinSourceSet("test")
-        }
     }
 }
