7def3ea808ad952c7fc65ad7478b2bd173b56822
Alexander Dinauer <alexander.dinauer@sentry.io>
2025-03-27 11:15:46 +0100

    Add tests for checking mixed versions

---
+++ sentry/src/test/java/io/sentry/SentryIntegrationPackageStorageTest.kt
@@ -0,0 +1,69 @@
+package io.sentry
+
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class SentryIntegrationPackageStorageTest {
+
+    @Test
+    fun `same package version is OK`() {
+        val storage = SentryIntegrationPackageStorage.getInstance()
+        storage.clearStorage()
+
+        storage.addPackage("maven:io.sentry:sentry", BuildConfig.VERSION_NAME)
+        storage.addPackage("maven:io.sentry:sentry-logback", BuildConfig.VERSION_NAME)
+
+        assertFalse(storage.checkForMixedVersions(SystemOutLogger()))
+    }
+
+    @Test
+    fun `checking twice works`() {
+        val storage = SentryIntegrationPackageStorage.getInstance()
+        storage.clearStorage()
+
+        storage.addPackage("maven:io.sentry:sentry", BuildConfig.VERSION_NAME)
+        storage.addPackage("maven:io.sentry:sentry-logback", BuildConfig.VERSION_NAME)
+
+        assertFalse(storage.checkForMixedVersions(SystemOutLogger()))
+        assertFalse(storage.checkForMixedVersions(SystemOutLogger()))
+    }
+
+    @Test
+    fun `checking twice with changes works`() {
+        val storage = SentryIntegrationPackageStorage.getInstance()
+        storage.clearStorage()
+
+        storage.addPackage("maven:io.sentry:sentry", BuildConfig.VERSION_NAME)
+        storage.addPackage("maven:io.sentry:sentry-logback", BuildConfig.VERSION_NAME)
+
+        assertFalse(storage.checkForMixedVersions(SystemOutLogger()))
+
+        storage.addPackage("maven:io.sentry:sentry-spring", "8.0.0")
+        assertTrue(storage.checkForMixedVersions(SystemOutLogger()))
+    }
+
+    @Test
+    fun `mixed package version is not OK`() {
+        val storage = SentryIntegrationPackageStorage.getInstance()
+        storage.clearStorage()
+
+        storage.addPackage("maven:io.sentry:sentry", BuildConfig.VERSION_NAME)
+        storage.addPackage("maven:io.sentry:sentry-logback", "8.0.0")
+
+        assertTrue(storage.checkForMixedVersions(SystemOutLogger()))
+    }
+
+    @Test
+    fun `only java sdk modules are checked`() {
+        val storage = SentryIntegrationPackageStorage.getInstance()
+        storage.clearStorage()
+
+        storage.addPackage("maven:io.sentry:sentry", BuildConfig.VERSION_NAME)
+        storage.addPackage("maven:io.sentry:sentry-logback", BuildConfig.VERSION_NAME)
+        storage.addPackage("maven:io.sentry.other:sentry-other", "1.0.0")
+        storage.addPackage("maven:io.opentelemetry.javaagent:opentelemetry-javaagent", "2.0.0")
+
+        assertFalse(storage.checkForMixedVersions(SystemOutLogger()))
+    }
+}
