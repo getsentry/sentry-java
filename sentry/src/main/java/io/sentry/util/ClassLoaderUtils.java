20f0c8f8c543be43485c81e024078044e8b88303
Aleksei Sazonov <acmi831@gmail.com>
2025-04-07 21:09:57 +0300

    Use thread context classloader when available

--- sentry/src/main/java/io/sentry/util/ClassLoaderUtils.java
+++ sentry/src/main/java/io/sentry/util/ClassLoaderUtils.java
@@ -8,6 +8,12 @@ public final class ClassLoaderUtils {
   public static @NotNull ClassLoader classLoaderOrDefault(final @Nullable ClassLoader classLoader) {
     // bootstrap classloader is represented as null, so using system classloader instead
     if (classLoader == null) {
+      // try thread context classloader
+      final @Nullable ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+      if (contextClassLoader != null) {
+        return contextClassLoader;
+      }
+      // fallback to system classloader
       return ClassLoader.getSystemClassLoader();
     } else {
       return classLoader;
