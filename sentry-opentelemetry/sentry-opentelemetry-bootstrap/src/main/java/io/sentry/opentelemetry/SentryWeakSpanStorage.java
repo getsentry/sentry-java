46170d46acd769819b60e17f12d0f8beb26a1e47
Alexander Dinauer <alexander.dinauer@sentry.io>
2025-04-23 13:55:05 +0200

    add tests

--- sentry-opentelemetry/sentry-opentelemetry-bootstrap/src/main/java/io/sentry/opentelemetry/SentryWeakSpanStorage.java
+++ sentry-opentelemetry/sentry-opentelemetry-bootstrap/src/main/java/io/sentry/opentelemetry/SentryWeakSpanStorage.java
@@ -63,5 +63,6 @@ public final class SentryWeakSpanStorage {
   @TestOnly
   public void clear() {
     sentrySpans.clear();
+    lastKnownRootSpan.clear();
   }
 }
--- sentry-opentelemetry/sentry-opentelemetry-core/build.gradle.kts
+++ sentry-opentelemetry/sentry-opentelemetry-core/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     testImplementation(kotlin(Config.kotlinStdLib))
     testImplementation(Config.TestLibs.kotlinTestJunit)
     testImplementation(Config.TestLibs.mockitoKotlin)
+    testImplementation(Config.TestLibs.mockitoInline)
     testImplementation(Config.TestLibs.awaitility)
 
     testImplementation(Config.Libs.OpenTelemetry.otelSdk)
