443f631c4dc56831c24d33e5fbf548a4b29894b2
Alexander Dinauer <alexander.dinauer@sentry.io>
2025-04-25 10:57:58 +0200

    Use SPI to search for other ContextStorageProviders and wrap them instead of using SentryOtelThreadLocalStorage

--- sentry-opentelemetry/sentry-opentelemetry-bootstrap/src/main/java/io/sentry/opentelemetry/SentryContextStorageProvider.java
+++ sentry-opentelemetry/sentry-opentelemetry-bootstrap/src/main/java/io/sentry/opentelemetry/SentryContextStorageProvider.java
@@ -1,11 +1,34 @@
 package io.sentry.opentelemetry;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
 import io.opentelemetry.context.ContextStorage;
 import io.opentelemetry.context.ContextStorageProvider;
 
 public final class SentryContextStorageProvider implements ContextStorageProvider {
   @Override
   public ContextStorage get() {
-    return new SentryContextStorage(new SentryOtelThreadLocalStorage());
+    return new SentryContextStorage(findStorageToWrap());
+  }
+
+  private @NotNull ContextStorage findStorageToWrap() {
+    try {
+      ServiceLoader<ContextStorageProvider> serviceLoader = ServiceLoader.load(ContextStorageProvider.class);
+      Iterator<ContextStorageProvider> iterator = serviceLoader.iterator();
+      while (iterator.hasNext()) {
+        ContextStorageProvider contextStorageProvider = iterator.next();
+        if (!(contextStorageProvider instanceof SentryContextStorageProvider)) {
+          return contextStorageProvider.get();
+        }
+      }
+    } catch (Throwable t) {
+      // ignore and use fallback
+    }
+
+    // using default / fallback storage
+    return new SentryOtelThreadLocalStorage();
   }
 }
