3572c9fa1836c49bccbbe6fd736bd4ef5e367480
stefanosiano <stefano.siano@sentry.io>
2025-06-12 16:47:13 +0200

    SentryLogcatAdapter now forwards output to Sentry Logs, if enabled

--- sentry-android-core/src/main/java/io/sentry/android/core/SentryLogcatAdapter.java
+++ sentry-android-core/src/main/java/io/sentry/android/core/SentryLogcatAdapter.java
@@ -2,8 +2,12 @@ package io.sentry.android.core;
 
 import android.util.Log;
 import io.sentry.Breadcrumb;
+import io.sentry.ScopesAdapter;
 import io.sentry.Sentry;
 import io.sentry.SentryLevel;
+import io.sentry.SentryLogLevel;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -44,73 +48,104 @@ public final class SentryLogcatAdapter {
     Sentry.addBreadcrumb(breadcrumb);
   }
 
+  private static void addAsLog(
+      @NotNull final SentryLogLevel level,
+      @Nullable final String msg,
+      @Nullable final Throwable tr) {
+    final @NotNull ScopesAdapter scopes = ScopesAdapter.getInstance();
+    if (tr == null) {
+      scopes.logger().log(level, msg);
+    } else {
+      StringWriter sw = new StringWriter(256);
+      PrintWriter pw = new PrintWriter(sw, false);
+      tr.printStackTrace(pw);
+      pw.flush();
+      scopes.logger().log(level, msg != null ? (msg + "\n" + sw.toString()) : sw.toString());
+      pw.close();
+    }
+  }
+
   public static int v(@Nullable String tag, @Nullable String msg) {
     addAsBreadcrumb(tag, SentryLevel.DEBUG, msg);
+    addAsLog(SentryLogLevel.TRACE, msg, null);
     return Log.v(tag, msg);
   }
 
   public static int v(@Nullable String tag, @Nullable String msg, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.DEBUG, msg, tr);
+    addAsLog(SentryLogLevel.TRACE, msg, tr);
     return Log.v(tag, msg, tr);
   }
 
   public static int d(@Nullable String tag, @Nullable String msg) {
     addAsBreadcrumb(tag, SentryLevel.DEBUG, msg);
+    addAsLog(SentryLogLevel.DEBUG, msg, null);
     return Log.d(tag, msg);
   }
 
   public static int d(@Nullable String tag, @Nullable String msg, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.DEBUG, msg, tr);
+    addAsLog(SentryLogLevel.DEBUG, msg, tr);
     return Log.d(tag, msg, tr);
   }
 
   public static int i(@Nullable String tag, @Nullable String msg) {
     addAsBreadcrumb(tag, SentryLevel.INFO, msg);
+    addAsLog(SentryLogLevel.INFO, msg, null);
     return Log.i(tag, msg);
   }
 
   public static int i(@Nullable String tag, @Nullable String msg, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.INFO, msg, tr);
+    addAsLog(SentryLogLevel.INFO, msg, tr);
     return Log.i(tag, msg, tr);
   }
 
   public static int w(@Nullable String tag, @Nullable String msg) {
     addAsBreadcrumb(tag, SentryLevel.WARNING, msg);
+    addAsLog(SentryLogLevel.WARN, msg, null);
     return Log.w(tag, msg);
   }
 
   public static int w(@Nullable String tag, @Nullable String msg, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.WARNING, msg, tr);
+    addAsLog(SentryLogLevel.WARN, msg, tr);
     return Log.w(tag, msg, tr);
   }
 
   public static int w(@Nullable String tag, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.WARNING, tr);
+    addAsLog(SentryLogLevel.WARN, null, tr);
     return Log.w(tag, tr);
   }
 
   public static int e(@Nullable String tag, @Nullable String msg) {
     addAsBreadcrumb(tag, SentryLevel.ERROR, msg);
+    addAsLog(SentryLogLevel.ERROR, msg, null);
     return Log.e(tag, msg);
   }
 
   public static int e(@Nullable String tag, @Nullable String msg, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.ERROR, msg, tr);
+    addAsLog(SentryLogLevel.ERROR, msg, tr);
     return Log.e(tag, msg, tr);
   }
 
   public static int wtf(@Nullable String tag, @Nullable String msg) {
     addAsBreadcrumb(tag, SentryLevel.ERROR, msg);
+    addAsLog(SentryLogLevel.FATAL, msg, null);
     return Log.wtf(tag, msg);
   }
 
   public static int wtf(@Nullable String tag, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.ERROR, tr);
+    addAsLog(SentryLogLevel.FATAL, null, tr);
     return Log.wtf(tag, tr);
   }
 
   public static int wtf(@Nullable String tag, @Nullable String msg, @Nullable Throwable tr) {
     addAsBreadcrumb(tag, SentryLevel.ERROR, msg, tr);
+    addAsLog(SentryLogLevel.FATAL, msg, tr);
     return Log.wtf(tag, msg, tr);
   }
 }
