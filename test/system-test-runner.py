96301df603ebe271a98dc6a723a3dacb2f0dbe70
Alexander Dinauer <adinauer@users.noreply.github.com>
2025-08-13 09:58:03 +0200

    Add E2E tests for Log4j2 (#4600)
    * Add E2E tests for Log4j2
    
    * Format code
    
    * codereview fixes
    
    * add to CI matrix
    
    ---------
    
    Co-authored-by: Sentry Github Bot <bot+github-bot@sentry.io>

--- test/system-test-runner.py
+++ test/system-test-runner.py
@@ -615,6 +615,7 @@ class SystemTestRunner:
             ModuleConfig("sentry-samples-console", "false", "true", "false"),
             ModuleConfig("sentry-samples-console-opentelemetry-noagent", "false", "true", "false"),
             ModuleConfig("sentry-samples-logback", "false", "true", "false"),
+            ModuleConfig("sentry-samples-log4j2", "false", "true", "false"),
         ]
 
     def _find_module_number(self, module_name: str, agent: str, auto_init: str) -> int:
