b5fa77253745c9342c6180be5f38c658bf768bb6
Lorenzo Cian <lorenzo.cian@sentry.io>
2025-08-27 08:57:09 +0200

    restore

--- sentry/src/main/java/io/sentry/util/Random.java
+++ sentry/src/main/java/io/sentry/util/Random.java
@@ -8,7 +8,6 @@
  * particular file as subject to the "Classpath" exception as provided
  * by Oracle in the LICENSE file that accompanied this code.
  *
- *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
