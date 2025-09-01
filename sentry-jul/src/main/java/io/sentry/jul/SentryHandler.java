540d9e75102dcae65bac3c9056f2a12127b48a6b
lcian <lorenzo.cian@sentry.io>
2025-09-01 18:11:24 +0200

    test JUL

--- sentry-jul/src/main/java/io/sentry/jul/SentryHandler.java
+++ sentry-jul/src/main/java/io/sentry/jul/SentryHandler.java
@@ -154,9 +154,16 @@ public class SentryHandler extends Handler {
       message = loggingEvent.getResourceBundle().getString(loggingEvent.getMessage());
     }
 
-    final @NotNull String formattedMessage = maybeFormatted(arguments, message);
+    String formattedMessage = null;
+    boolean formattingFailed = false;
+    try {
+      formattedMessage = maybeFormatted(arguments, message);
+    } catch (RuntimeException e) {
+      formattedMessage = message;
+      formattingFailed = true;
+    }
 
-    if (!formattedMessage.equals(message)) {
+    if (formattingFailed || !formattedMessage.equals(message)) {
       attributes.add(SentryAttribute.stringAttribute("sentry.message.template", message));
     }
 
@@ -173,6 +180,7 @@ public class SentryHandler extends Handler {
         return formatMessage(message, arguments);
       } catch (RuntimeException e) {
         // local formatting failed, sending raw message instead of formatted message
+        throw e;
       }
     }
 
