acdd35e1645c4e26616dc4a8e7044ae2c018f4d9
Alexander Dinauer <alexander.dinauer@sentry.io>
2025-09-11 15:31:05 +0200

    set source compat version to jdk 8 as well

--- sentry-system-test-support/build.gradle.kts
+++ sentry-system-test-support/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 configure<JavaPluginExtension> {
-  sourceCompatibility = JavaVersion.VERSION_17
+  sourceCompatibility = JavaVersion.VERSION_1_8
   targetCompatibility = JavaVersion.VERSION_1_8
 }
 
