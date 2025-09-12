59d386654c2c34d92806dc6c682b2b97930a60ea
Stefano <stefano.siano@sentry.io>
2025-09-12 17:05:59 +0200

    updated test to check for new options

--- sentry-android-integration-tests/sentry-uitest-android/src/androidTest/java/io/sentry/uitest/android/SdkInitTests.kt
+++ sentry-android-integration-tests/sentry-uitest-android/src/androidTest/java/io/sentry/uitest/android/SdkInitTests.kt
@@ -112,11 +112,7 @@ class SdkInitTests : BaseUiTest() {
 
   @Test
   fun doubleInitDoesNotWait() {
-    initSentry()
-
-    // We want the SDK to start sending the event. If we don't wait, it's possible we don't send
-    // anything before the SDK is restarted
-    waitUntilIdle()
+    initSentry(true)
 
     val beforeRestart = System.currentTimeMillis()
     // We restart the SDK. This shouldn't block the main thread, but new options (e.g. profiling)
@@ -128,9 +124,23 @@ class SdkInitTests : BaseUiTest() {
     val afterRestart = System.currentTimeMillis()
     val restartMs = afterRestart - beforeRestart
 
+    relayIdlingResource.increment()
+    Sentry.startTransaction("afterRestart", "emptyTransaction").finish()
+
     // We assert for less than 1 second just to account for slow devices in saucelabs or headless
     // emulator
     assertTrue(restartMs < 1000, "Expected less than 1000 ms for SDK restart. Got $restartMs ms")
+
+    relay.assert {
+      findEnvelope { assertEnvelopeTransaction(it.items.toList()).transaction == "afterRestart" }
+        .assert {
+          it.assertTransaction()
+          // There is a profiling item, as in the second init it was enabled
+          it.assertProfile()
+          it.assertNoOtherItems()
+        }
+      assertNoOtherEnvelopes()
+    }
   }
 
   @Test
