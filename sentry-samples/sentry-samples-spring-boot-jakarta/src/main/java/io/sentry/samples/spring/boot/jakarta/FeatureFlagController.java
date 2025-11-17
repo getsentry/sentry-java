92753c7538cb854a61f68755ae5e76f011351d24
Alexander Dinauer <alexander.dinauer@sentry.io>
2025-11-17 12:33:09 +0100

    use evaluation context

--- sentry-samples/sentry-samples-spring-boot-jakarta/src/main/java/io/sentry/samples/spring/boot/jakarta/FeatureFlagController.java
+++ sentry-samples/sentry-samples-spring-boot-jakarta/src/main/java/io/sentry/samples/spring/boot/jakarta/FeatureFlagController.java
@@ -1,13 +1,20 @@
 package io.sentry.samples.spring.boot.jakarta;
 
 import dev.openfeature.sdk.Client;
+import dev.openfeature.sdk.EvaluationContext;
+import dev.openfeature.sdk.ImmutableContext;
 import dev.openfeature.sdk.OpenFeatureAPI;
+import dev.openfeature.sdk.Value;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/feature-flag/")
@@ -25,7 +32,7 @@ public class  FeatureFlagController {
 
     // Evaluate boolean feature flag
     // This will trigger the SentryOpenFeatureHook which tracks the evaluation
-    boolean flagValue = client.getBooleanValue(flagKey, false);
+    boolean flagValue = client.getBooleanValue(flagKey, false, new ImmutableContext("example-context-key"));
 
     LOGGER.info("Feature flag '{}' evaluated to: {}", flagKey, flagValue);
 
@@ -38,7 +45,7 @@ public class  FeatureFlagController {
 
     // Evaluate feature flag before throwing error
     // The feature flag will be included in the Sentry event
-    boolean flagValue = client.getBooleanValue(flagKey, false);
+    boolean flagValue = client.getBooleanValue(flagKey, false, new ImmutableContext("example-context-key"));
 
     LOGGER.info("Feature flag '{}' evaluated to: {} before error", flagKey, flagValue);
 
