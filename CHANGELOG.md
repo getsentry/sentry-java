8107c75765607721501fe775d5c83ff2a9b36203
Alexander Dinauer <adinauer@users.noreply.github.com>
2026-01-20 14:55:35 +0100

    fix(logs): Check `sendDefaultPii` before attaching user attributes (#5036)
    * Check sendDefaultPii before attaching user attributes to logs
    
    * changelog
    
    * move changelog entry to fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@
 
 - Added `io.sentry.ndk.sdk-name` Android manifest option to configure the native SDK's name ([#5027](https://github.com/getsentry/sentry-java/pull/5027))
 
+### Fixes
+
+- Only attach user attributes to logs if `sendDefaultPii` is enabled ([#5036](https://github.com/getsentry/sentry-java/pull/5036))
+
 ### Dependencies
 
 - Bump Native SDK from v0.12.2 to v0.12.3 ([#5012](https://github.com/getsentry/sentry-java/pull/5012))
--- sentry/src/test/java/io/sentry/ScopesTest.kt
+++ sentry/src/test/java/io/sentry/ScopesTest.kt
@@ -2913,11 +2913,12 @@ class ScopesTest {
   }
 
   @Test
-  fun `adds user fields to log attributes`() {
+  fun `adds user fields to log attributes if sendDefaultPii is true`() {
     val (sut, mockClient) =
       getEnabledScopes {
         it.logs.isEnabled = true
         it.distinctId = "distinctId"
+        it.isSendDefaultPii = true
       }
 
     sut.configureScope { scope ->
@@ -2946,6 +2947,37 @@ class ScopesTest {
           val userEmail = it.attributes?.get("user.email")!!
           assertEquals("user@sentry.io", userEmail.value)
           assertEquals("string", userEmail.type)
+        },
+        anyOrNull(),
+      )
+  }
+
+  @Test
+  fun `does not add user fields to log attributes by default`() {
+    val (sut, mockClient) =
+      getEnabledScopes {
+        it.logs.isEnabled = true
+        it.distinctId = "distinctId"
+      }
+
+    sut.configureScope { scope ->
+      scope.user =
+        User().also {
+          it.id = "usrid"
+          it.username = "usrname"
+          it.email = "user@sentry.io"
+        }
+    }
+    sut.logger().log(SentryLogLevel.WARN, "log message")
+
+    verify(mockClient)
+      .captureLog(
+        check {
+          assertEquals("log message", it.body)
+
+          assertNull(it.attributes?.get("user.id"))
+          assertNull(it.attributes?.get("user.name"))
+          assertNull(it.attributes?.get("user.email"))
         },
         anyOrNull(),
       )
