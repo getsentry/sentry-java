25104b79522ae52b9bdad306cc75534afb1a952c
Alexander Dinauer <alexander.dinauer@sentry.io>
2026-02-06 11:38:24 +0100

    No longer fork Sentry Scopes for reactor-kafka consumer poll Runnable

--- sentry-spring/src/main/java/io/sentry/spring/webflux/SentryScheduleHook.java
+++ sentry-spring/src/main/java/io/sentry/spring/webflux/SentryScheduleHook.java
@@ -13,8 +13,27 @@ import org.jetbrains.annotations.NotNull;
  */
 @ApiStatus.Experimental
 public final class SentryScheduleHook implements Function<Runnable, Runnable> {
+
+  /**
+   * Runnable class names that should be excluded from scope forking. These are typically internal
+   * scheduler loops that reschedule themselves indefinitely, which would cause memory issues due to
+   * scope parent chain buildup.
+   *
+   * @see <a href="https://github.com/getsentry/sentry-java/issues/5051">GitHub Issue #5051</a>
+   */
+  private static final String[] EXCLUDED_RUNNABLE_PREFIXES = {
+    "reactor.kafka.receiver.internals.ConsumerEventLoop"
+  };
+
   @Override
   public Runnable apply(final @NotNull Runnable runnable) {
+    final String runnableClassName = runnable.getClass().getName();
+    for (final String excludedPrefix : EXCLUDED_RUNNABLE_PREFIXES) {
+      if (runnableClassName.startsWith(excludedPrefix)) {
+        return runnable;
+      }
+    }
+
     final IScopes newScopes = Sentry.getCurrentScopes().forkedCurrentScope("spring.scheduleHook");
 
     return () -> {
